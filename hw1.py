9d8cfb593ffb476ae184d9bfba2c5f47775a6f0a
akiko23 <usksoako@gmail.com>
2023-09-30 14:41:47 +0300

    rewrite code with flake8 standards

--- hw1.py
+++ hw1.py
@@ -1,35 +1,43 @@
-from typing import Optional, Dict, Generator, Iterable, Any
+"""Module that includes the implemented function from Vasilenko's first task."""
+
+
+from typing import Any, Dict, Generator, Iterable, Optional
 
 
 def salary_stats(
-    limit: Optional[str] = None,
-    **departments: Dict[str, Dict[str, float]]
+    limit: Optional[int] = None,
+    **departments: Dict[str, Dict[str, float]],
 ) -> Generator[Iterable | str, Any, Any]:
-    """
-    A function that returns a generator containing statistics about employee salaries
+    """Create a function that returns a generator containing statistics about employee salaries.
 
     Args:
-        limit: the value that represents max salary that will be considered
-        departments: dict of the keyword arguments where the keys
-                     are the names of the departments and the values
-                     are the dicts of the employees where the keys
-                     are the names of the employees and values are their salaries
-    Returns:
-        generator object which consists of the tuple of the three lowest salaries
+        limit (int): the maximum salary value to be considered.
+        departments (dict): dict of the keyword arguments where the keys \
+            are the names of the departments and the values \
+            are the dicts of the employees where the keys \
+            are the names of the employees and values are their salaries.
+
+    Yields:
+        tuple of the three lowest salaries
         and percent of its sum to all salaries
     """
+    if not limit:
+        limit = float('inf')
 
-    limit = float("inf") if not limit else limit
     all_salaries = []
-    for _, department in departments.items():
-        salaries = department.values()
-        all_salaries.extend(filter(lambda x: x <= limit, salaries))
+    for _, employees_data in departments.items():
+        salaries = employees_data.values()
+        for salary in salaries:
+            if salary <= limit:
+                all_salaries.append(salary)
 
     three_lowest_salaries = sorted(all_salaries)[:3]
     yield three_lowest_salaries
 
     total_payed = sum(all_salaries)
     if total_payed == 0:
-        yield "100%"
+        yield '100%'
         return
-    yield f"{round(sum(three_lowest_salaries) * 100 / total_payed, 2)}%"
+
+    lowests_percent_of_all_salaries = round(sum(three_lowest_salaries) * 100 / total_payed, 2)
+    yield f'{lowests_percent_of_all_salaries}%'
