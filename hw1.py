a1b0123a4b163cdb55924e2a47ff599da6432e46
Artem09076 <grigoranartem592@yandex.ru>
2023-10-01 12:27:20 +0300

    Add files hw1.py and testhw1.py

---
+++ hw1.py
@@ -0,0 +1,40 @@
+"""This modile include function get_statistic."""
+
+
+from typing import Optional
+
+
+def get_statistic(departamen_data: dict, limit: Optional[int] = None) -> tuple:
+    """Take a dictionary with salary as input and return statistics.
+
+    Args:
+        limit: A numerical limit above which salaries need not be taken
+        departamen_data: dictionary with the names departments as keys,\
+            the dictionary with employee names as keys\
+            and salaries as values
+
+    Returns:
+        The function returns statistics: average, maximum and median salary
+    """
+    salary_people = []
+    for names_departamens in departamen_data.keys():
+        for _, salary in dict(departamen_data[names_departamens]).items():
+            salary_people.append(salary)
+
+    salary_people = sorted(
+        filter(lambda se: limit is None or se <= limit, salary_people),
+        )
+    mal_salary = []
+    len_salary_people = len(salary_people)
+
+    if len(salary_people) % 2 == 1:
+        mal_salary.append(round(salary_people[len_salary_people//2], 2))
+    else:
+        mal_salary.append(round(
+            (salary_people[len_salary_people//2] +
+             salary_people[len_salary_people // 2 - 1]
+             ) // 2, 2,
+        ))
+    mal_salary.append(round(max(salary_people), 2))
+    mal_salary.append(round(sum(salary_people)/len(salary_people), 2))
+    return tuple(mal_salary)
---
+++ testhw1.py
@@ -0,0 +1,52 @@
+"""This module include test of function get_statistic."""
+
+import pytest
+
+from hw1 import get_statistic
+
+test_get_datas = (
+    (
+        None,
+        {
+            'Sales Departaments': {'Staffer1': 50000.0},
+        },
+        (50000.0, 50000.0, 50000.0),
+    ),
+    (
+        150000,
+        {
+            'Dep1': {'Staff1': 50000.0, 'Staff2': 60000.0, 'Staff3': 70000.0},
+        },
+        (60000.0, 70000.0, 60000.0),
+    ),
+    (
+        120000,
+        {
+            'Dep1': {'Staff3': 50000.0, 'Staff4': 60000.0, 'Staff5': 70000.0},
+            'Dep2': {'Staff6': 80000.0, 'Staff7': 90000.0},
+        },
+        (70000.0, 90000.0, 70000.0),
+    ),
+    (
+        80000,
+        {
+            'Dep1': {'Staff1': 50000.0, 'Staff2': 60000.0, 'Staff3': 70000.0},
+            'Dep2': {'Staffer4': 80000.0, 'Staff5': 90000.0},
+        },
+        (65000.0, 80000.0, 65000.0),
+    ),
+)
+
+
+@pytest.mark.parametrize('limit,departamens_data,expected', test_get_datas)
+def test_get_stat(limit: int, departamens_data: dict, expected: tuple) -> None:
+    """A function that tests get_statistic function.
+
+    Args:
+        limit: A numerical limit above which salaries need not be taken
+        departamens_data: dictionary with the names departments as keys,\
+            the dictionary with employee names as keys\
+            and salaries as values.
+        expected: expected function output.
+    """
+    assert get_statistic(departamens_data, limit) == expected
