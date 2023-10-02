0052d3144806109fca37566c6c643ac8e62a0d94
rTiRe <zientenin@mail.ru>
2023-10-02 19:56:04 +0300

    Add a docstring for function.

--- 1_homework.py
+++ 1_homework.py
@@ -4,6 +4,18 @@ from typing import Tuple
 
 
 def get_top_salaries(*args: Tuple[str, list], departments: Tuple[str] = None) -> tuple:
+    """Get the 3 highest salaries and the ratio of their sum to the sum of all salaries.
+
+    Args:
+        args: Tuple[str, list] - departments name and lists of salaries.
+        departments: Tuple[str] - names of departments in which salaries need to be calculated.
+
+    Returns:
+        tuple: A tuple containing two elements:
+        - A list of the top three salaries (sorted in descending order).
+        - The percentage that the top three salaries contribute to the total
+        salaries within the specified departments, rounded to two decimal places.
+    """
     top_salaries = []
     if departments:
         args = list(filter(lambda arg: arg[0] in departments, args))
