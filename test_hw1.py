4fe2ce394c90bbb5e4ec96eafa7055b4b18f5659
MomusWinner <areg06.06@mail.ru>
2023-10-02 20:16:25 +0300

    implemented test_hw1 modul

---
+++ test_hw1.py
@@ -0,0 +1,60 @@
+"""Unit tests for modul hw1."""
+
+from typing import Tuple
+
+import pytest
+
+from hw1 import get_best_salaries
+
+dept1 = {'a': 1, 'b': 2, 'c': 3}
+dept2 = {'d': 4, 'e': 5, 'f': 6}
+dept3 = {'g': 7, 'h': 8, 'i': 9}
+
+test_dict = {'dept1': dept1, 'dept2': dept2, 'dept3': dept3}
+test_data = (
+    (
+        None,
+        (
+            (
+                (
+                    'i',
+                    9,
+                ),
+                (
+                    'h',
+                    8,
+                ),
+                (
+                    'g',
+                    7,
+                ),
+            ),
+            53.33,
+        ),
+        test_dict,
+    ),
+)
+
+
+@pytest.mark.parametrize('ignored_depts, expected, kwargs', test_data)
+def test_colculating_best_salaries(ignored_depts: Tuple, expected: Tuple, kwargs) -> None:
+    """Test get_best_salaries function.
+
+    Args:
+        ignored_depts (Tuple): depts ignored when calculating statistics.
+        kwargs: the key is the name of the department, the value is a dictionary.
+        expected (Tuple): test value.
+    """
+    assert get_best_salaries(ignored_depts, **kwargs) == expected
+
+
+#     (
+#         ('dept3', 'dept2'),
+#         ((('c', 3), ('b', 2), ('a', 1)), 100.0),
+#         test_dict,
+#     ),
+#     (
+#         ('dept3'),
+#         ((('f', 6), ('e', 5), ('d', 4)), 71.43),
+#         test_dict,
+#     ),
