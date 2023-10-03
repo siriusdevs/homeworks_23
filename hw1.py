980f43fd7bb1ab808d7278c4b06953851b14cfed
Marko <90ts@mail.ru>
2023-10-03 11:15:14 +0300

    change variable(our_salary -> top3_salary) and change type of incident(list -> tuple)

--- hw1.py
+++ hw1.py
@@ -15,10 +15,10 @@ def salary_info(
         tuple[list, float] - top 3 salaries and the ratio for company.
     """
     salaries = []
-    incident = excluded if excluded else []
+    incident = excluded if excluded else ()
     for department in departments:
         if department[0] not in incident:
             salaries += department[1].values()
-    our_salary = sorted(salaries)[:3]
-    salary_percent = sum(our_salary) / (sum(salaries) or 1) * 100
-    return [round(salary, 2) for salary in our_salary], round(salary_percent, 2)
+    top3_salary = sorted(salaries)[:3]
+    salary_percent = sum(top3_salary) / (sum(salaries) or 1) * 100
+    return [round(salary, 2) for salary in top3_salary], round(salary_percent, 2)
