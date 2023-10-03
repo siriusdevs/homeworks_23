d9a59bbe062184dcb56587f5cebcdc65a6668043
bliinmaker <kir05062005@gmail.com>
2023-10-03 13:39:33 +0300

    added a check for the presence of the second element

--- hw1.py
+++ hw1.py
@@ -1,7 +1,7 @@
 """Module for calculation company salaries statistics."""
 
 
-def work_statistics(company_part: dict, ex_company_part: tuple = None) -> tuple:
+def work_statistics(company_part: dict, ex_company_part: tuple | None = None) -> tuple:
     """Calculate the statistics of employees salaries in a company.
 
     Args:
@@ -12,16 +12,16 @@ def work_statistics(company_part: dict, ex_company_part: tuple = None) -> tuple:
         tuple: top 3 of the lowest salaries of all departments and their percent of all salaries.
     """
     all_salaries = []
+    ex_company_part = () if ex_company_part is None else ex_company_part
 
     for part, employees in company_part.items():
         if part not in ex_company_part:
             for salary in employees.values():
-                all_salaries.append(salary)
+                all_salaries.append(round(salary, 2))
 
     lowest_top = sorted(all_salaries)[:3]
+    sum_salaries = sum(all_salaries)
 
-    percent_of_lowest = 0
-    if sum(all_salaries) != 0:
-        percent_of_lowest = round(sum(lowest_top) / sum(all_salaries) * 100, 2)
+    percent_of_lowest = 0 if sum_salaries == 0 else sum(lowest_top) / sum_salaries * 100
 
-    return lowest_top, percent_of_lowest
+    return lowest_top, round(percent_of_lowest, 2)
