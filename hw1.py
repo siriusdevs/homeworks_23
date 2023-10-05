acd6d151ed0a3e0d58dfd51e8af41dc773fedf82
Артём Тюрин <zientenin@mail.ru>
2023-10-06 00:17:05 +0300

    Sorting sheet assembly is now more readable

--- hw1.py
+++ hw1.py
@@ -14,22 +14,25 @@ def get_top_salaries(*departments: tuple[str, list], required_deps: tuple[str] =
         - The percentage that the top three salaries contribute to the total
         salaries within the specified departments, rounded to two decimal places.
     """
-    top_salaries = []
+    sorted_salaries = []
     if required_deps:
         departments = filter(lambda arg: arg, departments)
         departments = filter(lambda arg: arg[0] in required_deps, departments)
         departments = tuple(departments)
-    required_salaries = [salaries[1] for salaries in departments if len(salaries) > 1]
-    for salaries in required_salaries:
-        top_salaries.extend(salaries)
-    if any(salary < 0 for salary in top_salaries):
+    for salaries in departments:
+        if len(salaries) > 1:
+            sorted_salaries.extend(salaries[1])
+    if any(salary < 0 for salary in sorted_salaries):
         return 'Sorry, salary cannot be less than zero.'
-    top_salaries.sort(reverse=True)
+    sorted_salaries.sort(reverse=True)
     try:
-        percent = sum(top_salaries[:3]) / sum(top_salaries) * 100
+        percent = sum(sorted_salaries[:3]) / sum(sorted_salaries) * 100
     except ZeroDivisionError:
-        if top_salaries:
+        if sorted_salaries:
             percent = float(100)
         else:
             percent = float(0)
-    return top_salaries[:3], round(percent, 2)
+    return sorted_salaries[:3], round(percent, 2)
+
+
+print(get_top_salaries(('Develop', [20, 20, 20, 80, 20]), ('Marketing', [20, 20, 20, 80, 20]), ('Sales', [20, 20, 20, 80, 20])))
