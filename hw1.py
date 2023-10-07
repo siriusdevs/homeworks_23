1f0eb8ab3144f8d24aba85e0e81035e3967d858d
MomusWinner <areg06.06@mail.ru>
2023-10-07 23:15:58 +0300

    dictionary converted to a list, remove get_employee_salary functio and add vaildation for zero employees

--- hw1.py
+++ hw1.py
@@ -9,31 +9,22 @@ def get_best_salaries(ignored_depts: tuple = None, **dept_employees) -> tuple:
         dept_employees: the key is the name of the department, the value is a dictionary.
 
     Returns:
-        tuple: the first element is a tuple of 3 employees,
+        tuple: the first element is a list of 3 salaries,
         and the second element is a float number.
     """
-    employees = {}
+    employees = []
     for dept, employees_salaries in dept_employees.items():
         if ignored_depts is None or dept not in ignored_depts:
-            for key in employees_salaries.keys():
-                employees_salaries[key] = round(employees_salaries[key], 2)
-            employees.update(employees_salaries)
-    best_salaried_employees = sorted(employees.items(), key=get_employee_salary, reverse=True)[:3]
-    best_salaries_sum = sum(employee[1] for employee in best_salaried_employees)
-    salaries_sum = sum(salary for salary in employees.values())
+            for salary in employees_salaries.values():
+                employees.append(round(salary, 2))
+
+    if len(employees) == 0:
+        return [], 0
+
+    best_salaried_employees = sorted(employees, reverse=True)[:3]
+    best_salaries_sum = sum(best_salaried_employees)
+    salaries_sum = sum(employees)
 
     percent = round((best_salaries_sum / salaries_sum) * 100, 2)
 
     return best_salaried_employees, percent
-
-
-def get_employee_salary(employee: tuple):
-    """Get employee salary.
-
-    Args:
-        employee (tuple): employee with his salary.
-
-    Returns:
-        int: employee salary.
-    """
-    return employee[1]
