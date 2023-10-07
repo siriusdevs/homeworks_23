92e748cd61698762e0b8f338c57846e95e6a37fb
I-AM-LAMER <vadimtatarnikov300605@gmail.com>
2023-10-07 14:48:59 +0300

    fixed allll

--- hw1.py
+++ hw1.py
@@ -1,11 +1,13 @@
 """Module for calculating salary stats."""
 
+from typing import Optional
 
-def get_salaries(*company: tuple, dept_except: tuple[str] = None) -> tuple[list, float]:
+
+def get_salaries(*company: tuple, dept_except: Optional[str] = None) -> tuple[list, float]:
     """Count top 3 salaries and those part in all salaries.
 
     Args:
-        company (tuple): salaries in departments.
+        company (tuple): salaries in departments for each employee.
         dept_except (tuple[str], optional): Excluded departments. Defaults to None.
 
     Returns:
@@ -15,13 +17,14 @@ def get_salaries(*company: tuple, dept_except: tuple[str] = None) -> tuple[list,
 
     for department in company:
         if dept_except is None or (department[0] not in dept_except):
-            salaries += list(department[1].values())
+            salaries += department[1].values()
 
     top_three = sorted(salaries, reverse=True)[:3]
+    sum_salaries = sum(salaries)
 
-    if sum(salaries) == 0:
-        in_percentage = 0
+    if sum_salaries:
+        in_percentage = (sum(top_three) / sum_salaries) * 100
     else:
-        in_percentage = (sum(top_three) / sum(salaries)) * 100
+        in_percentage = 0
 
     return [round(salary, 2) for salary in top_three], round(in_percentage, 2)
