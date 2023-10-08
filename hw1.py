ca9da6f144a6548461c36d0fd394c390d532ff96
MomusWinner <areg06.06@mail.ru>
2023-10-08 18:08:43 +0300

    update validation of zero salaries

--- hw1.py
+++ hw1.py
@@ -21,12 +21,12 @@ def get_best_salaries(ignored_depts: tuple = None, **dept_employees) -> tuple:
                 employees.append(round(salary, 2))
                 salaries_sum += employees[-1]
 
-    if not employees:
-        return [], 0
-
     best_salaried_employees = sorted(employees, reverse=True)[:3]
     best_salaries_sum = sum(best_salaried_employees)
 
-    percent = round((best_salaries_sum / salaries_sum) * 100, 2)
+    if salaries_sum == 0:
+        percent = 0
+    else:
+        percent = round((best_salaries_sum / salaries_sum) * 100, 2)
 
     return best_salaried_employees, percent
