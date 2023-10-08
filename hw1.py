c194d7edfbf3908a0416bcbeed55434eca253714
telegins125@gmail.com <telegins125@gmail.com>
2023-10-08 19:43:36 +0300

    fix bug

--- hw1.py
+++ hw1.py
@@ -1,31 +1,37 @@
 """Module, that has one function "best_wage", homework1."""
 
 
-def best_wage(departments: list, exclude_depart: tuple = None) -> list[float | str]:
+def best_wage(departments: list, exclude_deps: tuple = None) -> list[float | int]:
     """Find 3 best salaries and their percentage to total amount of payments.
 
     Args:
-        exclude_depart: Tuple[str] - tuple of excluded department's names.
+        exclude_deps: Tuple[str] - tuple of excluded department's names.
         departments: List[tuple] - tuple of departments and dictionary of names and salaries.
 
     Returns:
         list[float | str]: 3 best salaries and their percentage to total amount of payments.
     """
+    quantity_deps = len(departments)
+    if quantity_deps == 0:
+        return ['Недостаточно данных для использования функции']
     total_payments = []
-    permitted_salaries = []
+    permitted_wages = []
 
     for dep in departments:
         for _, salary in dep[1].items():
             total_payments += [salary]
-            if exclude_depart is None or dep[0] not in exclude_depart:
-                permitted_salaries += [salary]
+            if exclude_deps is None or dep[0] not in exclude_deps:
+                permitted_wages += [salary]
 
-    permitted_salaries = sorted(permitted_salaries, reverse=True)
-    num_top = 3
-    best_salaries = []
+    permitted_wages = sorted(permitted_wages, reverse=True)
+    possible_len = len(permitted_wages)
 
-    best_salaries += [round(permitted_salaries[top], 2) for top in range(num_top)]
-    percent_best_salary = round(sum(best_salaries) / sum(total_payments) * 100, 2)
-    best_salaries += [f'{percent_best_salary}%']
+    if possible_len >= 3:
+        permitted_wages = [round(permitted_wages[top], 2) for top in range(3)]
+    else:
+        return ['Недостаточно не исключенных отделов для использования функции']
 
-    return best_salaries
+    percent_best_salary = round(sum(permitted_wages) / sum(total_payments) * 100, 2)
+    permitted_wages += [f'{percent_best_salary}%']
+
+    return permitted_wages
--- test_hw1.py
+++ test_hw1.py
@@ -10,32 +10,66 @@ test_data = (
             ('Smartphones', {'Anegin': 20000, 'Homosov': 30000}),
             ('Laptops', {'Lozhechkov': 15000, 'Genaninov': 18000}),
             ('Radio', {'Vilkov': 35000, 'Ternov': 37000}),
-            ('Headsets', {'Terrabled': 50000, 'Monkeykingov': 52250}),
+            ('Headsets', {'Terrabledov': 50000, 'Monkeykingov': 52250}),
         ),
         ('Laptops', 'Clothes'),
         ([52250, 50000, 37000, '53.1%']),
     ),
     (
         (
-            ('Clothes', {'Telegin': 2000, 'Sachkov': 3000}),
-            ('Smartphones', {'Anegin': 20000, 'Homosov': 30000}),
-            ('Laptops', {'Lozhechkov': 15000, 'Genaninov': 18000}),
-            ('Radio', {'Vilkov': 35000, 'Ternov': 37000}),
-            ('Headsets', {'Terrabled': 50000, 'Monkeykingov': 52250}),
+            ('Glasses', {'Ochkova': 2000, 'Morkovova': 3000}),
+            ('Bags', {'Portfelev': 20000, 'Sumochkina': 30000}),
+            ('Pencils', {'Karandashov': 15000, 'Krasnikova': 18000}),
+            ('Pens', {'Ruchkina': 35000, 'Nozhov': 37000}),
+            ('Markers', {'Enigmov': 50000, 'Shadowfiendov': 52250}),
         ),
-        ('Radio'),
+        ('Pens'),
         ([52250, 50000, 30000, '50.43%']),
     ),
     (
         (
-            ('Jeans', {'Telegin': 2576, 'Sachkov': 3567}),
-            ('Hats', {'Anegin': 23678, 'Homosov': 38467}),
-            ('Monitors', {'Lozhechkov': 35365.345, 'Genaninov': 18123.456}),
-            ('Bottles', {'Vilkov': 35678, 'Ternov': 37964}),
-            ('Tables', {'Terrabled': 55368, 'Monkeykingov': 53439}),
+            ('Jeans', {'Shortova': 2576, 'Sinikova': 3567}),
+            ('Hats', {'Kepkina': 23678, 'Nikov': 38467}),
+            ('Monitors', {'Acerov': 35365.345, 'Hyperova': 18123.456}),
+            ('Bottles', {'Bytilkov': 35678, 'Sobytilnik': 37964}),
+            ('Tables', {'Pudgov': 55368, 'Tinkerov': 53439}),
         ),
         ('Tables', 'Bottles'),
         ([38467, 35365.35, 23678, '32.05%']),
+    ),
+    (
+        (
+            ("Test's devices", {'Testovich': 2576, 'Testivina': 3567}),
+            ('Windows', {'Oknovin': 1360, 'Steklovina': 2450}),
+        ),
+        ('Pans'),
+        ([3567, 2576, 2450, '86.34%']),
+    ),
+    (
+        (),
+        (),
+        (['Недостаточно данных для использования функции']),
+    ),
+    (
+        (),
+        ('Library', 'Beer'),
+        (['Недостаточно данных для использования функции']),
+    ),
+    (
+        (
+            ('Books', {'Esenina': 35000, 'Bylgakov': 44000}),
+            ('Cakes', {'Tortova': 42000, 'Pirozhenkov': 20000}),
+        ),
+        ('Books', 'Cakes'),
+        (['Недостаточно не исключенных отделов для использования функции']),
+    ),
+    (
+        (
+            ('Milk', {'Yogurtova': 15500, 'Bylgakov': 23450}),
+            ('Chips', {'Laysova': 24680, 'Kartoshkova': 25340}),
+        ),
+        ('Milk'),
+        (['Недостаточно не исключенных отделов для использования функции']),
     ),
 )
 
