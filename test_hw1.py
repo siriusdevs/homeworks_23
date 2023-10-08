a07c3e5c30e4d5787f00c6b22c0e772555ba9eb2
MomusWinner <areg06.06@mail.ru>
2023-10-08 17:36:19 +0300

    made the constant a variable in uppercase

--- test_hw1.py
+++ test_hw1.py
@@ -1,39 +1,41 @@
 """Unit tests for modul hw1."""
 
+from types import MappingProxyType
+
 import pytest
 
 from hw1 import get_best_salaries
 
-dept1 = {'a': 1, 'b': 2, 'c': 3}
-dept2 = {'d': 4, 'e': 5, 'f': 6}
-dept3 = {'g': 7, 'h': 8, 'i': 9}
+DEPT1 = MappingProxyType({'a': 1, 'b': 2, 'c': 3})
+DEPT2 = MappingProxyType({'d': 4, 'e': 5, 'f': 6})
+DEPT3 = MappingProxyType({'g': 7, 'h': 8, 'i': 9})
 
-test_dict = {'dept1': dept1, 'dept2': dept2, 'dept3': dept3}
-test_data = (
+TEST_DICT = MappingProxyType({'dept1': DEPT1, 'dept2': DEPT2, 'dept3': DEPT3})
+TEST_DATA = (
     (
         ('dept1',),
         ([9, 8, 7], 61.54),
-        test_dict,
+        TEST_DICT,
     ),
     (
         ('dept3', 'dept2'),
         ([3, 2, 1], 100.0),
-        test_dict,
+        TEST_DICT,
     ),
     (
         (None, ),
         ([9, 8, 7], 53.33),
-        test_dict,
+        TEST_DICT,
     ),
     (
         ('dept1', 'dept2', 'dept3'),
         ([], 0),
-        test_dict,
+        TEST_DICT,
     ),
 )
 
 
-@pytest.mark.parametrize('ignored_depts, expected, dept_employees', test_data)
+@pytest.mark.parametrize('ignored_depts, expected, dept_employees', TEST_DATA)
 def test_colculating_best_salaries(ignored_depts: tuple, expected: tuple, dept_employees) -> None:
     """Test get_best_salaries function.
 
