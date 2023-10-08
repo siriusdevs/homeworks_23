ff44483ac81d75a85233ef891dc7f5ceb8ff7f4a
tofickbrodaga <mshdtlv@gmail.com>
2023-10-08 16:30:39 +0300

    Fix linter and add round for top_salary

--- hw1.py
+++ hw1.py
@@ -1,27 +1,29 @@
-'''Salary stats and percent's'''
+"""Salary stats and percents."""
 
 
-def top_salary(*company: tuple[str, list[float]],
-               name_departments: [list[str]] = None) -> tuple[list[float], float]:
-    """
-    Find top 3 salaries in the company, the ratio of the amount of this top to the total amount of
-    payments in the company in percentages.
+def top_salary(
+    *company: tuple[str, list[float]],
+    name_departments: list[str] = None,
+) -> tuple[list[float], float]:
+    """Find top-3 salaries in the company and the ratio of their sum to the total amount of payments.
 
     Args:
-        *company: Tuple[str, List[float]] - company departments and their salaries.
-        name_departments: List[str] - the name departaments. Usually Optional[List[str]] = None.
+        company: Tuple[str, list[float]] - company departments and their salaries.
+        name_departments: list[str] - the names of departments to consider.
 
     Returns:
-        tuple[list[float], float] - top 3 salaries and the ratio  of the amount
-        of this top to the total amount of payments.
+        tuple[list[float], float] - top 3 salaries and the ratio
+        of their sum to the total amount of payments.
     """
     all_salaries = []
+
     for department_name, salaries in company:
         if name_departments is None or department_name in name_departments:
-            all_salaries.extend(salaries)
+            all_salaries.extend([round(salary, 2) for salary in salaries])
 
-    if not all_salaries or sum(all_salaries) == 0:
-        return [], 0.0
+
+    if sum(all_salaries) == 0 or sum(all_salaries) == 0:
+        return [], 0
 
     top_salaries = sorted(all_salaries, reverse=True)[:3]
 
