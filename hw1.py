3076e98db680915eca2901bf072d376ecc28b2f8
Artemka <Artyom08112006@gmail.com>
2023-10-09 12:02:00 +0300

    added error output

--- hw1.py
+++ hw1.py
@@ -2,6 +2,38 @@
 import statistics
 
 
+def check_arguments(departments: tuple) -> None:
+    """Функция для проверки на пригодность переданных аргументов.
+
+    Args:
+        departments: tuple - кортеж кортежей с информацией об отделах в компании
+
+    Raises:
+        AttributeError: Ошибка связанная с некорректно переданными в функцию аргументы
+    """
+    if not departments:
+        raise AttributeError(
+            'Недостаточно аргументов для анализа.'
+            +
+            '\nТребуется ввести информацию минимум об 1 отделе!',
+            )
+
+    for department in departments:
+        if len(department) != 2:
+            raise AttributeError(
+                'Некорректно передана информация об отделах'
+                +
+                '\nКортеж должен содержать 2 элемента: Название отдела и список с зарплатами!!!',
+            )
+
+        if not department[1]:
+            raise AttributeError(
+                f'Для отдела "{department[0]}" некорректно переданы зарплаты.'
+                +
+                '\nВ отделе должен числиться минимум 1 сотрудник!',
+                )
+
+
 def generete_report(*departments: tuple, exceptions: tuple = None) -> tuple:
     """Анализирует аргументы и возвращает топ 3 высокооплачиваемых и самых низкооплачиваемых отдела.
 
@@ -12,9 +44,12 @@ def generete_report(*departments: tuple, exceptions: tuple = None) -> tuple:
     Returns:
         tuple - кортеж из 2 списков: Топ 3 высокооплачиваемых отдела и топ 3 низкооплачиваемых
     """
+
     def sort_data(department: tuple, reverse: bool) -> list:
         updated_data = sorted(department, key=lambda dpt: statistics.mean(dpt[1]), reverse=reverse)
         return [dpt[0] for dpt in updated_data if dpt[0] not in exceptions]
+
+    check_arguments(departments)
 
     if not exceptions:
         exceptions = ()
