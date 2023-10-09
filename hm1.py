142576bda07f7c5625286b16a12d239a1f4b4622
Raitman17 <skarenyakin@bk.ru>
2023-10-09 13:20:09 +0300

    первый коммит

---
+++ hm1.py
@@ -0,0 +1,30 @@
+""" Модуль для расчёта статистики: топ-3 самых
+высокооплачиваемых отдела в компании и топ-3 самых
+низкооплачиваемых (по средней зарплате в отделе)"""
+
+
+def salary_statistics(exclude_department: tuple = None, **department: dict[str, float]) -> list:
+    """ Расчёт топ-3 высокооплачиваемых отделов и топ-3 низкооплачиваемых отделов в компании.
+
+    Args:
+        exclude_department (tuple): Кортеж названий отделов, которые нужно \
+            исключить из статистики. По умолчанию None.
+        department (dict[str, float]): словарь, где имена сотрудников отдела ключи(str), \
+            а зарплаты значение(float).
+    Returns:
+        list: статистика: топ-3 самых высокооплачиваемых отдела в компании \
+            и топ-3 самых низкооплачиваемых.
+    """
+    average_salaries = []
+
+    for dep in department.keys():
+        if exclude_department is None or dep not in exclude_department:
+            if len(department[dep]) > 0:
+                average_salary = round(sum(department[dep].values()) / len(department[dep]), 2)
+                average_salaries.append([dep, average_salary])
+
+    average_salaries = sorted(average_salaries, key=lambda x: x[1])
+    top_dep = [i[0] for i in average_salaries[-3:]]
+    worst_dep = [i[0] for i in average_salaries[:3]]
+
+    return top_dep, worst_dep
---
+++ test_hm1.py
@@ -0,0 +1,110 @@
+"""Модуль тест для hm.1"""
+
+
+import pytest
+
+from hm1 import salary_statistics
+
+
+test_data = (
+    (
+        {
+            'north': {
+                'Nikita': 50000,
+                'Sasha': 70000,
+                'Masha': 60000
+            },
+            'south': {
+                'Andrey': 125000,
+                'Georgiy': 210000,
+                'Anna': 540000
+            },
+            'west': {
+                'Misha': 4400,
+                'Natasha': 8800,
+                'Grisha': 3800
+            },
+            'east': {
+                'Kostya': 65000,
+                'Vanya': 87000,
+                'Kira': 71000
+            }
+        },
+        (['north', 'east', 'south'], ['west', 'north', 'east'])
+    ),
+    (
+        {
+            'exclude_department': ('north', 'south', 'west', 'east'),
+            'north': {
+                'Nikita': 58000,
+                'Sasha': 71000,
+                'Masha': 66000
+            },
+            'south': {
+                'Andrey': 112000,
+                'Georgiy': 21000,
+                'Anna': 54000
+            },
+            'west': {
+                'Misha': 44000,
+                'Natasha': 88000,
+                'Grisha': 38000
+            },
+            'east': {
+                'Kostya': 65000,
+                'Vanya': 47000,
+                'Kira': 71000
+            }
+        },
+        ([], [])
+    ),
+    (
+        {
+            'exclude_department': ('north', 'south'),
+            'north': {
+                'Nikita': 1111,
+                'Sasha': 72323,
+                'Masha': 3232.23
+            },
+            'south': {
+                'Andrey': 1123230,
+                'Georgiy': 21000,
+                'Anna': 0
+            },
+            'west': {
+                'Misha': 44000,
+            },
+            'east': {
+                'Kostya': 632300,
+                'Vanya': 130000
+            }
+        },
+        (['west', 'east'], ['west', 'east'])
+    ),
+    (
+        {
+            'south': {
+
+            },
+            'west': {
+                'Misha': 44000,
+            },
+            'east': {
+                'Kostya': 632300,
+                'Vanya': 130000
+            }
+        },
+        (['west', 'east'], ['west', 'east'])
+    )
+)
+
+
+@pytest.mark.parametrize('args, expected', test_data)
+def test_worst_best_department(args: dict[str, float], expected: list) -> None:
+    """Тест функции salary_statistics
+
+    Args:
+        args (dict[str, float]): тестовые данные
+        expected (list): результат
+    """
+    assert salary_statistics(**args) == expected
