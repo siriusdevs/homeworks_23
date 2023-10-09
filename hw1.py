d72103864fe197bb58b593bde6623daafab3a382
slavikyd <slavikyd@gmail.com>
2023-10-09 10:50:23 +0300

    renamed departments to employees in hw1.py

--- hw1.py
+++ hw1.py
@@ -17,8 +17,8 @@ def get_salary_stats(
 
     """
     salaries = []
-    for departments in company_salaries_data.values():
-        salaries_list = departments.values()
+    for employees in company_salaries_data.values():
+        salaries_list = employees.values()
         if limit:
             salaries_list = [salary for salary in salaries_list if salary <= limit]
         salaries += salaries_list
