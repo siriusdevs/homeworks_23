b99b4be15c821d9e54fec07504ba8fcd27c639d8
MomusWinner <areg06.06@mail.ru>
2023-10-13 20:12:20 +0300

    Add tests what tasting get_better_salaries function with default values.

--- test_hw1.py
+++ test_hw1.py
@@ -6,37 +6,56 @@ import pytest
 
 from hw1 import get_best_salaries
 
-DEPT1 = MappingProxyType({'a': 1, 'b': 2, 'c': 3})
-DEPT2 = MappingProxyType({'d': 4, 'e': 5, 'f': 6})
-DEPT3 = MappingProxyType({'g': 7, 'h': 8, 'i': 9})
+DEPT1 = 'dept1'
+DEPT2 = 'dept2'
+DEPT3 = 'dept3'
 
-TEST_DICT = MappingProxyType({'dept1': DEPT1, 'dept2': DEPT2, 'dept3': DEPT3})
+DEPT1_DIR = MappingProxyType({'a': 1, 'b': 2, 'c': 3})
+DEPT2_DIR = MappingProxyType({'d': 4, 'e': 5, 'f': 6})
+DEPT3_DIR = MappingProxyType({'g': 7, 'h': 8, 'i': 9})
+
+TEST_DICT_THREE_DEPTS = MappingProxyType(
+    {
+        DEPT1: DEPT1_DIR, DEPT2: DEPT2_DIR, DEPT3: DEPT3_DIR,
+    },
+)
+TEST_DICT_ONE_DEPTS = MappingProxyType({DEPT1: DEPT1_DIR})
+
 TEST_DATA = (
     (
-        ('dept1',),
+        (DEPT1,),
         ([9, 8, 7], 61.54),
-        TEST_DICT,
+        TEST_DICT_THREE_DEPTS,
     ),
     (
-        ('dept3', 'dept2'),
+        (DEPT3, DEPT2),
         ([3, 2, 1], 100.0),
-        TEST_DICT,
+        TEST_DICT_THREE_DEPTS,
     ),
     (
-        (None, ),
-        ([9, 8, 7], 53.33),
-        TEST_DICT,
-    ),
-    (
-        ('dept1', 'dept2', 'dept3'),
+        (DEPT1, DEPT2, DEPT3),
         ([], 0),
-        TEST_DICT,
+        TEST_DICT_THREE_DEPTS,
+    ),
+)
+
+# Test get_better_salaries with default arguments.
+TEST_DATA_DEFAULT = (
+    (
+        ([3, 2, 1], 100.0),
+        TEST_DICT_ONE_DEPTS,
+    ),
+    (
+        ([9, 8, 7], 53.33),
+        TEST_DICT_THREE_DEPTS,
     ),
 )
 
 
 @pytest.mark.parametrize('ignored_depts, expected, dept_employees', TEST_DATA)
-def test_colculating_best_salaries(ignored_depts: tuple, expected: tuple, dept_employees) -> None:
+def test_colculating_best_salaries(
+    ignored_depts: tuple, expected: tuple, dept_employees: dict[str, dict[str, int]],
+) -> None:
     """Test get_best_salaries function.
 
     Args:
@@ -45,3 +64,16 @@ def test_colculating_best_salaries(ignored_depts: tuple, expected: tuple, dept_e
         expected (tuple): test value.
     """
     assert get_best_salaries(ignored_depts, **dept_employees) == expected
+
+
+@pytest.mark.parametrize('expected, dept_employees', TEST_DATA_DEFAULT)
+def test_calculating_best_salaries_default(
+    expected: tuple, dept_employees: dict[str, dict[str, int]],
+) -> None:
+    """Test get_best_salaries function without ignore_depts argument.
+
+    Args:
+        dept_employees: the key is the name of the department, the value is a dictionary.
+        expected (tuple): test value.
+    """
+    assert get_best_salaries(**dept_employees) == expected
