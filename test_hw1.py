3900a921a5693a81d5d80a3f0dd5136ef7a81209
roganofff <matematic1337@gmail.com>
2023-10-13 10:10:36 +0300

    Added more test cases & Splited tests in two parts

--- test_hw1.py
+++ test_hw1.py
@@ -4,22 +4,7 @@ import pytest
 
 from hw1 import get_salary_statistics
 
-TEST_DATA = (
-    (
-        None,
-        (42, 75, 30),
-        {
-            'valve': {
-                'Gabe Newell': 60,
-                'Mike Morasky': 30,
-                'Marc Laidlaw': 20,
-            },
-            'gsc': {
-                'Sergay Grigorovich': 75,
-                'Dushinskyy Petro': 25,
-            },
-        },
-    ),
+TEST_DATA_WITH_ARG = (
     (
         300,
         (657, 1000, 725),
@@ -41,6 +26,23 @@ TEST_DATA = (
         },
     ),
     (
+        100,
+        (204, 300, 180),
+        {
+            'lg': {
+                'Koo In-hwoi': 225,
+                'Koo Cha-kyung': 180,
+            },
+            'dyson': {
+                'James Dyson': 300,
+                'Roland Krueger': 145,
+            },
+            'bosch': {
+                'Robert Bosch': 170,
+            },
+        },
+    ),
+    (
         250,
         (0, 0, 0),
         {
@@ -54,20 +56,11 @@ TEST_DATA = (
             },
         },
     ),
-    (
-        None,
-        (88, 88, 88),
-        {
-            'kfc': {
-                'Colonel Sanders': 88,
-            },
-        },
-    ),
 )
 
 
-@pytest.mark.parametrize('minimal_salary, expected, departments', TEST_DATA)
-def test_get_salary_statistics(
+@pytest.mark.parametrize('minimal_salary, expected, departments', TEST_DATA_WITH_ARG)
+def test_function_with_optional_arg(
     minimal_salary: int | float,
     expected: tuple,
     departments: dict[str, dict[str, float]],
@@ -80,3 +73,57 @@ def test_get_salary_statistics(
         departments: dict - key is department, value is dict - str is name, float is salary.
     """
     assert get_salary_statistics(minimal_salary, **departments) == expected
+
+
+TEST_DATA_WITHOUT_ARG = (
+    (
+        (42, 75, 30),
+        {
+            'valve': {
+                'Gabe Newell': 60,
+                'Mike Morasky': 30,
+                'Marc Laidlaw': 20,
+            },
+            'gsc': {
+                'Sergay Grigorovich': 75,
+                'Dushinskyy Petro': 25,
+            },
+        },
+    ),
+    (
+        (88, 88, 88),
+        {
+            'kfc': {
+                'Colonel Sanders': 88,
+            },
+        },
+    ),
+    (
+        (0, 0, 0),
+        {
+            'xiaomi': {
+                'Lei Jun': 0,
+            },
+            'realme': {
+                'Sky Li': 0,
+            },
+            'oneplus': {
+                'Pete Lau': 0,
+            },
+        },
+    ),
+)
+
+
+@pytest.mark.parametrize('expected, departments', TEST_DATA_WITHOUT_ARG)
+def test_function_without_optional_arg(
+    expected: tuple,
+    departments: dict[str, dict[str, float]],
+) -> None:
+    """Test main module function get_salary_statistics.
+
+    Args:
+        expected: tuple - contains expected test outputs,
+        departments: dict - key is department, value is dict - str is name, float is salary.
+    """
+    assert get_salary_statistics(**departments) == expected
