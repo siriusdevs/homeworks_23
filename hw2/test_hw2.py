b2cea826320839513374b4d89e333f5bda872f69
Artem09076 <grigoranartem592@yandex.ru>
2023-10-21 22:57:40 +0300

    modified file  test_hw2.py

--- hw2/test_hw2.py
+++ hw2/test_hw2.py
@@ -1,24 +1,38 @@
 """This module include test of function process_data."""
 
 import json
+import pathlib
 
 import pytest
 
 from hw2 import process_data
 
+
+def make_path(file_name):
+    """Take a filename and returns path to filename.
+
+    Args:
+        file_name: name of file
+
+    Returns:
+        Returns full path to filename
+    """
+    return pathlib.Path(file_name)
+
+
 TEST_PROCESS_DATA = (
-    ('/home/runner/work/homeworks_23/hw2/data_hw2.json',
-     '/home/runner/work/homeworks_23/hw2/datas_output.json',
+    (make_path('data_hw2.json'),
+     make_path('datas_output.json'),
      {'2012-12-24': 50.0, '2022-10-30': 50.0, 'gmail.com': 50.0, 'yandex.ru': 50.0},
      ),
-    ('/home/runner/work/homeworks_23/hw2/data_hw1_2.json',
-     '/home/runner/work/homeworks_23/hw2/datas_test_output2.json',
+    (make_path('data_hw1_2.json'),
+     make_path('datas_test_output2.json'),
      {'2012-12-24': 33.33, '2019-10-30': 33.33, '2022-10-30': 33.33,
       'gmail.com': 33.33, 'mail.ru': 33.33, 'yandex.ru': 33.33,
       },
      ),
-    ('/home/runner/work/homeworks_23/hw2/data_hw2_3.json',
-     '/home/runner/work/homeworks_23/hw2/datas_output3.json', {},
+    (make_path('data_hw2_3.json'),
+     make_path('datas_output3.json'), {},
      ),
 )
 
