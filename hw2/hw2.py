35133188b552f387d59de9cf8038c36672fc2c87
akiko23 <usksoaoko@gmail.com>
2023-10-23 00:25:31 +0300

    satisfy linter

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -3,19 +3,28 @@ import json
 from datetime import datetime, timedelta
 from enum import Enum
 
-DEFAULT_DATE_FORMAT = '%Y-%m-%d'
-
 NOW = datetime.now()
 
 
 class Period(Enum):
-    TWO_DAYS = 2
-    WEEK = 7
-    MONTH = 31
-    HALF_OF_YEAR = 365 // 2
+    """Class-enum representing all the necessary periods."""
+
+    two_days = 2
+    week = 7
+    month = 31
+    half_of_year = 182
 
 
 def find_insertion_index(nums: list[int], target: int) -> int:
+    """Create function that finds the index for insertion using binary search algorithm and returns it.
+
+    Args:
+        nums (list[int]): the list where we want to insert the target number
+        target (int): the value we want to insert into the list
+
+    Returns:
+        Index to insert
+    """
     left, right = 0, len(nums) - 1
     while left <= right:
         mid = left + (right - left) // 2
@@ -26,29 +35,55 @@ def find_insertion_index(nums: list[int], target: int) -> int:
     return left
 
 
-def user_was_offline_lt_target_num_of_days(user_item, target_period) -> bool:
-    last_login_date = datetime.strptime(user_item['last_login'], DEFAULT_DATE_FORMAT)
+def user_was_offline_lt_target_num_of_days(user_item: dict, target_period: Period) -> bool:
+    """Create a function that returns whether the user is offline for less then target period or not.
+
+    Args:
+        user_item (dict): the dict that includes all the user params such as age, last_login, etc.
+        target_period (Period): the value that represents the target period in days
+
+    Returns:
+        Bool value whether the user is offline for less then target period or not
+    """
+    last_login_date = datetime.fromisoformat(user_item['last_login'])
     target_date = timedelta(days=target_period.value)
 
     return NOW - last_login_date < target_date
 
 
-def get_lt_certain_period_offline_users_average_age(users_data, target_period) -> int:
+def get_lt_period_offline_users_avg_age(users_data: dict, target_period) -> int:
+    """Create a function that returns whether the user is offline for lt target period or not.
+
+    Args:
+        users_data (dict): the dict that includes data about all users
+        target_period (Period): the value that represents the target period in days
+
+    Returns:
+        The avg age of the filtered by 'num of offline days lt target period' condition users
+    """
     filtered_by_offline_period = list(filter(
         lambda user_item: user_was_offline_lt_target_num_of_days(user_item, target_period),
-        users_data.values()
+        users_data.values(),
     ))
     number_of_users = len(filtered_by_offline_period)
     if not number_of_users:
         return 0
-    return sum(map(lambda user: user['age'], filtered_by_offline_period)) // number_of_users
+    return sum(user['age'] for user in filtered_by_offline_period) // number_of_users
 
 
 def get_mt_half_of_year_offline_users_avg_age(users_data) -> int:
+    """Create a function that returns whether the user is offline for more then half of year or not.
+
+    Args:
+        users_data (dict): the dict that includes data about all users
+
+    Returns:
+        The avg age of the filtered by 'num of offline days more then half of year' condition users
+    """
     mt_half_year_offline_users = []
     for user_item in users_data.values():
-        last_login_date = datetime.strptime(user_item['last_login'], DEFAULT_DATE_FORMAT)
-        target_date = timedelta(days=Period.HALF_OF_YEAR.value)
+        last_login_date = datetime.fromisoformat(user_item['last_login'])
+        target_date = timedelta(days=Period.half_of_year.value)
 
         if NOW - last_login_date > target_date:
             mt_half_year_offline_users.append(user_item['age'])
@@ -60,61 +95,56 @@ def get_mt_half_of_year_offline_users_avg_age(users_data) -> int:
 
 
 def process_data(data_file_path: str, output_file_path: str) -> None:
+    """Create the main function that reads users data from data_file \
+        and writes the necessary statistics by task into output_file.
+
+    Args:
+        data_file_path (str): the path to file with users data
+        output_file_path (str): the path to file in which we save the necessary statistics
+    """
     with open(data_file_path, 'r') as data_file:
         users_data: dict = json.load(data_file)
 
     all_ages = []
-    min_age, max_age = float('inf'), 0
     for user in users_data.values():
         user_age = user['age']
-        if user_age > max_age:
-            max_age = user_age
-        if user_age < min_age:
-            min_age = user_age
-        insertion_index = find_insertion_index(all_ages, user_age)
-        all_ages.insert(insertion_index, user_age)
+        all_ages.insert(find_insertion_index(all_ages, user_age), user_age)
 
     number_of_users = len(all_ages)
 
-    avg_age = sum(all_ages) // number_of_users
     median_age = all_ages[number_of_users // 2]
     if number_of_users % 2 == 0:
         median_age = (all_ages[number_of_users // 2] + all_ages[number_of_users // 2 - 1]) // 2
 
-    lt_two_days_offline_users_average_age = get_lt_certain_period_offline_users_average_age(
-        users_data=users_data,
-        target_period=Period.TWO_DAYS,
-    )
-    lt_week_offline_users_average_age = get_lt_certain_period_offline_users_average_age(
-        users_data=users_data,
-        target_period=Period.WEEK,
-    )
-    lt_month_offline_users_average_age = get_lt_certain_period_offline_users_average_age(
-        users_data=users_data,
-        target_period=Period.MONTH,
-    )
-    lt_half_of_year_offline_users_average_age = get_lt_certain_period_offline_users_average_age(
-        users_data=users_data,
-        target_period=Period.HALF_OF_YEAR,
-    )
-    mt_half_of_year_offline_users_average_age = get_mt_half_of_year_offline_users_avg_age(
-        users_data=users_data,
-    )
-
     with open(output_file_path, 'w') as output_file:
         json.dump(
             obj={
-                'max_age': max_age,
-                'min_age': min_age,
-                'avg_age': avg_age,
+                'max_age': all_ages[-1],
+                'min_age': all_ages[0],
+                'avg_age': sum(all_ages) // number_of_users,
                 'median_age': median_age,
-                'lt_two_days_offline_users_average_age': lt_two_days_offline_users_average_age,
-                'lt_week_offline_users_average_age': lt_week_offline_users_average_age,
-                'lt_month_offline_users_average_age': lt_month_offline_users_average_age,
-                'lt_half_of_year_offline_users_average_age': lt_half_of_year_offline_users_average_age,
-                'mt_half_of_year_offline_users_average_age': mt_half_of_year_offline_users_average_age,
+                'lt_two_days_offline_users_average_age': get_lt_period_offline_users_avg_age(
+                    users_data=users_data,
+                    target_period=Period.two_days,
+                ),
+                'lt_week_offline_users_average_age': get_lt_period_offline_users_avg_age(
+                    users_data=users_data,
+                    target_period=Period.week,
+                ),
+                'lt_month_offline_users_average_age': get_lt_period_offline_users_avg_age(
+                    users_data=users_data,
+                    target_period=Period.month,
+                ),
+                'lt_half_of_year_offline_users_average_age': get_lt_period_offline_users_avg_age(
+                    users_data=users_data,
+                    target_period=Period.half_of_year,
+                ),
+                'mt_half_of_year_offline_users_average_age':
+                    get_mt_half_of_year_offline_users_avg_age(
+                        users_data=users_data,
+                    ),
             },
             fp=output_file,
             indent=4,
-            ensure_ascii=False
+            ensure_ascii=False,
         )
--- hw2/test_hw2.py
+++ hw2/test_hw2.py
@@ -1,3 +1,4 @@
+"""Test module for function process_data from hw2.py."""
 import json
 
 import pytest
@@ -8,18 +9,30 @@ INPUT_FILES_DIR_PATH = 'hw2/test_files/input'
 OUTPUT_FILES_DIR_PATH = 'hw2/test_files/output'
 EXPECTED_FILES_DIR_PATH = 'hw2/test_files/expected_output'
 
+NUMBER_OF_FILES = 10
+
 
 def initialise_test_data():
-    result = []
-    for i in range(1, 11):
-        input_file_path = f'{INPUT_FILES_DIR_PATH}/{i}.json'
-        output_file_path = f'{OUTPUT_FILES_DIR_PATH}/{i}.json'
+    """Create a function that initialises test_data and returns it.
 
-        with open(f'{EXPECTED_FILES_DIR_PATH}/{i}.json') as expected_output_file:
+    Returns:
+        The test_data containing the tuples of the following format: \
+            ( \
+                input_file_path: path to file with users data, \
+                output_file_path: path file where we save necessary stats, \
+                expected_output_data: dict with correct statistics \
+            )
+    """
+    test_data = []
+    for filename in range(1, NUMBER_OF_FILES + 1):
+        input_file_path = f'{INPUT_FILES_DIR_PATH}/{filename}.json'
+        output_file_path = f'{OUTPUT_FILES_DIR_PATH}/{filename}.json'
+
+        with open(f'{EXPECTED_FILES_DIR_PATH}/{filename}.json') as expected_output_file:
             expected_output_data = json.load(expected_output_file)
 
-        result.append((input_file_path, output_file_path, expected_output_data))
-    return tuple(result)
+        test_data.append((input_file_path, output_file_path, expected_output_data))
+    return tuple(test_data)
 
 
 TEST_DATA = initialise_test_data()
@@ -27,6 +40,13 @@ TEST_DATA = initialise_test_data()
 
 @pytest.mark.parametrize('input_file_path, output_file_path, expected_output_data', TEST_DATA)
 def test_process_data(input_file_path, output_file_path, expected_output_data):
+    """Create function (extended by pytest decorator) that tests process_data function.
+
+    Args:
+        input_file_path: path to file with users data
+        output_file_path: path file where we save necessary stats
+        expected_output_data: dict with correct statistics
+    """
     process_data(input_file_path, output_file_path)
     with open(output_file_path) as output_file:
         output_data = json.load(output_file)
