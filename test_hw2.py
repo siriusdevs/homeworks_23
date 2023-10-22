89c3a2f07fdcb76ec638e01c923bcdf91bfd5d58
Artem09076 <grigoranartem592@yandex.ru>
2023-10-22 22:28:53 +0300

    workflow file and add files for test

--- test_hw2.py
+++ test_hw2.py
@@ -1,41 +1,27 @@
 """This module include test of function process_data."""
 
 import json
-import pathlib
 
 import pytest
 
 from hw2 import process_data
 
 
-def make_path(file_name):
-    """Take a filename and returns path to filename.
+def make_tests():
+    res=[]
+    for i in range(1,4):
+        input_file=f'/hw2/input/{i}.json'
+        output_file=f'/hw2/output/{i}.json'
 
-    Args:
-        file_name: name of file
-
-    Returns:
-        Returns full path to filename
-    """
-    return str(pathlib.Path(file_name))
+        with open(f'/hw2/expected/{i}.json') as file_excepted:
+            excepted=json.load(file_excepted)
+        res.append(input_file)
+        res.append(output_file)
+        res.append(excepted)
 
 
-TEST_PROCESS_DATA = (
-    (make_path('data_hw2.json'),
-     make_path('datas_output.json'),
-     {'2012-12-24': 50.0, '2022-10-30': 50.0, 'gmail.com': 50.0, 'yandex.ru': 50.0},
-     ),
-    (make_path('data_hw1_2.json'),
-     make_path('datas_test_output2.json'),
-     {'2012-12-24': 33.33, '2019-10-30': 33.33, '2022-10-30': 33.33,
-      'gmail.com': 33.33, 'mail.ru': 33.33, 'yandex.ru': 33.33,
-      },
-     ),
-    (make_path('data_hw2_3.json'),
-     make_path('datas_output3.json'), {},
-     ),
-)
 
+TEST_PROCESS_DATA = make_tests()
 TEST_ERROR_PROCESS_DATA = (
     ('/home/runner/work/homeworks_23/hw2/data.json',
      '/home/runner/work/homeworks_23/hw2/datas_output.json', 'Is not a file path',
