152bb09f5f911ab52b9c272097d0c4a780962a3e
Екатерина Зорина <katya@MacBook-Air-Ekaterina.local>
2023-10-23 19:06:00 +0300

    add test_hw1.py and fix docstring in hw1.py

--- hw1.py
+++ hw1.py
@@ -1,34 +1,37 @@
-"""A module that includes the function of calculating salaries"""
-
-SALARY_LIMIT = 20
+"""A module that includes the function of calculating salaries."""
 
 
-def get_stats(*departments: tuple[str, dict[str, float]], limit: float = None) -> tuple[float, float, float]:
-    """
-    Get the statistics for salaries in a company.
+def get_stats(
+    *departments: tuple[str, dict[str, float]],
+    limit: float = None,
+) -> tuple[float, float, float]:
+    """Get the statistics for salaries in a company.
 
     Args:
-        departments: tuple[str, dict[str, float]] - Tuples,
-        that include the names of company departments,
-        dictionary of the surnames of employees and their salaries.
-        limit: float - An optional parameter to filter out salaries below a certain limit.
+        departments:
+            tuple[str, dict[str, float]] - Tuples,
+            that include the names of company departments,
+            dictionary of the surnames of employees and their salaries.
+        limit:
+            float - An optional parameter to filter out salaries below a certain limit.
 
     Returns:
         A tuple containing the maximum salary, average salary, and median salary.
     """
-    all_salaries = []
+    all_sls = []
     for department in departments:
         for _, salary in department[1].items():
             if limit is None or salary >= limit:
-                all_salaries.append(salary)
-    if not all_salaries:
-        return {'average_salary': 0, 'max_salary': 0, 'median_salary': 0}
-    average_salary = round(sum(all_salaries) / len(all_salaries), 2)
-    max_salary = round(max(all_salaries), 2)
-    if len(all_salaries) % 2 == 1:
-        median_salary = round(sorted(all_salaries)[len(all_salaries) // 2], 2)
+                all_sls.append(salary)
+    if not all_sls:
+        return (0, 0, 0)
+    count_salaries = len(all_sls)
+    average_salary = round(sum(all_sls) / count_salaries, 2)
+    max_salary = round(max(all_sls), 2)
+    if count_salaries % 2 == 1:
+        median_salary = round(sorted(all_sls)[count_salaries // 2], 2)
     else:
-        lower_middle = sorted(all_salaries)[len(all_salaries) // 2 - 1]
-        upper_middle = sorted(all_salaries)[len(all_salaries) // 2]
-        median_salary = round((lower_middle + upper_middle) / 2, 2)
+        median_elements = sorted(all_sls)[count_salaries // 2 - 1]
+        median_elements += sorted(all_sls)[count_salaries // 2]
+        median_salary = round(median_elements / 2, 2)
     return max_salary, average_salary, median_salary
---
+++ test_hw1.py
@@ -0,0 +1,52 @@
+"""A module for testing hw1 get_stats function."""
+
+from typing import Tuple
+
+import pytest
+
+from hw1 import get_stats
+
+test_data = [
+    ((), None, (0, 0, 0)),
+    (
+        (
+            ('Meat processing plant', {'Zubarev': 1000, 'Sterlyagov': 80}),
+            ('Bakery', {'Grigoryan': 60, 'Kardashian': 90}),
+        ),
+        None,
+        (1000, 307.5, 85.0),
+    ),
+    (
+        (
+            ('Confectionery', {'Hadid': 100, 'Polovinkin': 80}),
+            ('Agrocomplex', {'Bronin': 60, 'Miheeva': 90}),
+        ),
+        85,
+        (100, 95.0, 95.0),
+    ),
+    (
+        (
+            ('Safety Department', {'Ivanova': 90, 'Noskov': 70, 'Pushkin': 80}),
+            ('Reception', {'Petrova': 60, 'Smirnov': 100, 'Lebedev': 30}),
+            ('Accounting', {'Tokareva': 50, 'Novosyolova': 10, 'Lobastov': 20}),
+        ),
+        20,
+        (100, 62.5, 65.0),
+    ),
+]
+
+
+@pytest.mark.parametrize('departments, limit, expected', test_data)
+def test_get_stats(departments: Tuple, limit, expected):
+    """Test get_stats function with test_data.
+
+    Args:
+        departments: departments with salaries to be passed to get_stats.
+        limit: the salary limit below which will not be taken into account.
+        expected: an actual expected get_stats result.
+
+    Asserts:
+        True if get_stats returns expected answers.
+    """
+    result_from_get_stats = get_stats(*departments, limit=limit)
+    assert result_from_get_stats == expected
