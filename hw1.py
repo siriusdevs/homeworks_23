fdb416aac66227b5738d27edcd528d70b4aa04cc
Sam Komarov <sam@skomarov.com>
2023-10-24 15:14:03 +0300

    satisfy flake8

--- hw1.py
+++ hw1.py
@@ -1,14 +1,37 @@
+"""Provides types and functions for solving task_1.
+
+Напишите функцию, которая принимает любые ключевые аргументы, но ожидает,
+что название аргумента будет названием отдела компании (str),
+а значение для аргумента - словарем, где имена сотрудников (str) служат в качестве ключей,
+а зарплаты (float) - в качестве значений. Функция должна возвращать статистику: средняя,
+максимальная и медианная зарплата в компании.
+Функция также должна принимать опциональный аргумент,
+который по умолчанию равен None. Этот аргумент - кортеж названий отделов,
+которые нужно включить в статистику (их и только их). В
+се числа в выводе функции округлять до второго знака после запятой.
+"""
 from dataclasses import dataclass
-from typing import Optional, Tuple
+from typing import Optional
 
 
 @dataclass
 class SalaryStats:
-    average: float = 0.0
-    median: float = 0.0
-    maximum: float = 0.0
+    """A dataclass for denoting basic salary statistics."""
+
+    average: float = 0
+    median: float = 0
+    maximum: float = 0
 
 
-def get_salary_stats(units: Optional[Tuple[str]] = None, **kwargs) -> SalaryStats:
-    stats = SalaryStats()
-    return stats
+def get_salary_stats(units: Optional[tuple[str]] = None, **kwargs) -> SalaryStats:
+    """Compute salary statistics for a given company, optionally including only the specified units.
+
+    Args:
+        units: the only units to include in stats. If omitted, defaults to all units
+        kwargs: dictionary of unit names to dictionaries of employee names to their salaries
+
+    Returns:
+        Statistics for provided company's salaries.
+        If units list (first argument) is specified, the result only includes these units.
+    """
+    return SalaryStats()
--- test_hw1.py
+++ test_hw1.py
@@ -1,12 +1,20 @@
-"""Tests for hw1.py"""
+"""Tests for hw1.py."""
 import pytest
+
 from hw1 import SalaryStats, get_salary_stats
 
 test_data = (
-    (tuple(), {}, SalaryStats(maximum=0, average=0, median=0)),
+    ((), {}, SalaryStats(maximum=0, average=0, median=0)),
 )
 
 
 @pytest.mark.parametrize('args, kwargs, expected', test_data)
-def test_get_salary_stats(args, kwargs, expected):
+def test_get_salary_stats(args: tuple, kwargs: dict, expected: SalaryStats):
+    """Asserts that calling get_salary_stats with args and kwargs returns expected.
+
+    Args:
+        args: arguments to forward to get_salary_stats
+        kwargs: keyword arguments to forward to get_salary_stats
+        expected: expected result of the get_salary_stats call
+    """
     assert get_salary_stats(*args, **kwargs) == expected
