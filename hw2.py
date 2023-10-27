325ef0ba5311497b35fc49020a9b713baf8cb956
Artem09076 <grigoranartem592@yandex.ru>
2023-10-27 22:01:30 +0300

    modified tests files and hw2.py

--- hw2.py
+++ hw2.py
@@ -1,11 +1,14 @@
-"""This module include function process_data."""
+"""This module include function process_data,which writes statistics to a file."""
 
 
 import json
 import os
 
+mail = 'email'
+registr = 'registered'
 
-def is_file(in_path: str, out_path: str) -> bool:
+
+def is_file(in_path: str, out_path: str) -> None:
     """Check if the arguments are files.
 
     Args:
@@ -19,27 +22,29 @@ def is_file(in_path: str, out_path: str) -> bool:
         raise FileNotFoundError('Is not a file path')
 
 
-def dict_path(set_names: set, registr: list, mail: list) -> dict:
+def dict_path(count_dct: dict) -> dict:
     """Take a set and two lists and returns a dictionary with statistics.
 
     Args:
-        set_names:set of mail and registrar list joins
-        registr: list with data of registration
-        mail: list with mails host
+        count_dct:dict with counts
 
     Returns:
-        Returns a dictionary with statistics.
+        A dictionary with statistics.
     """
-    res_dict = {}
-    for name in set_names:
-        if name in registr:
-            res_dict[name] = round((registr.count(name)/len(registr))*100, 2)
-        else:
-            res_dict[name] = round((mail.count(name)/len(mail))*100, 2)
+    res_dict = {mail: {}, registr: {}}
+    regist_len = len(count_dct[registr])
+    email_len = len(count_dct['email'])
+
+    for register, email in zip(count_dct[registr], count_dct[mail]):
+        res_dict[registr][register] = round((
+            count_dct[registr][register]/regist_len
+        )*100, 2,
+        )
+        res_dict[mail][email] = round((count_dct[mail][email]/email_len)*100, 2)
     return res_dict
 
 
-def process_data(input_filepath: str, output_filepath: str) -> None | str:
+def process_data(input_filepath: str, output_filepath: str) -> None:
     """Take json file and returns emailing and registration statiatic.
 
     Args:
@@ -48,15 +53,16 @@ def process_data(input_filepath: str, output_filepath: str) -> None | str:
 
     """
     is_file(input_filepath, output_filepath)
-    with open(input_filepath, 'r') as file1:
-        registrated = []
-        email = []
-        for user in json.load(file1).values():
+    with open(input_filepath, 'r') as input_file:
+        res_dict = {mail: {}, registr: {}}
+
+        for user in json.load(input_file).values():
             for key in user.keys():
-                if key == 'registered':
-                    registrated.append(user[key])
-                elif key == 'email':
-                    email.append(user[key].split('@')[1])
-    res_dict = dict_path(sorted((set(registrated+email))), registrated, email)
-    with open(output_filepath, 'w') as file2:
-        json.dump(res_dict, file2)
+                if key == registr:
+                    res_dict[registr][user[key]] = res_dict.get(user[key], 0)+1
+                elif key == mail:
+                    mail_host = user[key][user[key].find('@')+1:]
+                    res_dict[mail][mail_host] = res_dict.get(mail_host, 0)+1
+        res_dict = dict_path(res_dict)
+    with open(output_filepath, 'w') as output_file:
+        json.dump(res_dict, output_file)
