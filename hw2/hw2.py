cb257154efe971f72a0d6268217ef2d99aba610d
akiko23 <usksoako@gmail.com>
2023-10-31 18:28:01 +0300

    replace None with NoReturn typehint

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -1,10 +1,11 @@
 """Module that includes the implemented function from Vasilenko's second task."""
 import json
+from typing import NoReturn
 
 from hw2.utils import UserStatsUtils, find_insertion_index
 
 
-def process_data(data_file_path: str, output_file_path: str) -> None:
+def process_data(data_file_path: str, output_file_path: str) -> NoReturn:
     """Create the main function that reads users data from data_file \
         and writes the necessary statistics by task into output_file.
 
@@ -15,8 +16,8 @@ def process_data(data_file_path: str, output_file_path: str) -> None:
     user_stats_utils: UserStatsUtils = UserStatsUtils(data_file_path, output_file_path)
 
     all_ages: list[int] = []
-    for user_data in user_stats_utils.users_data.values():
-        user_age: int = user_data['age']
+    for user in user_stats_utils.users:
+        user_age: int = user.age
         all_ages.insert(find_insertion_index(all_ages, user_age), user_age)
 
     user_stats_utils.all_ages = all_ages
