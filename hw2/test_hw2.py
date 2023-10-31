8b93b7596c1ef4c8dd7bb1f0701ccbda1fe2335d
akiko23 <usksoako@gmail.com>
2023-10-31 18:30:11 +0300

    add automatic test correction according to the certain date && add class-helper for test generation

--- hw2/test_hw2.py
+++ hw2/test_hw2.py
@@ -1,43 +1,15 @@
 """Test module for function process_data from hw2.py."""
 import json
+from contextlib import suppress
 
 import pytest
 from msgspec import ValidationError
 
 from hw2.hw2 import process_data
 from hw2.typesdev import Error, Users
+from hw2.utils.test_data_initialiser import TestDataGenerator
 
-INPUT_FILES_DIR_PATH = 'hw2/test_files/input'
-OUTPUT_FILES_DIR_PATH = 'hw2/test_files/output'
-EXPECTED_FILES_DIR_PATH = 'hw2/test_files/expected_output'
-
-NUMBER_OF_FILES = 10
-
-
-def initialise_test_data():
-    """Create a function that initialises test_data and returns it.
-
-    Returns:
-        The test_data containing the tuples of the following format: \
-            ( \
-                input_file_path: path to file with users data, \
-                output_file_path: path file where we save necessary stats, \
-                expected_output_data: dict with correct statistics \
-            )
-    """
-    test_data = []
-    for filename in range(1, NUMBER_OF_FILES + 1):
-        input_file_path = f'{INPUT_FILES_DIR_PATH}/{filename}.json'
-        output_file_path = f'{OUTPUT_FILES_DIR_PATH}/{filename}.json'
-
-        with open(f'{EXPECTED_FILES_DIR_PATH}/{filename}.json') as expected_output_file:
-            expected_output_data = json.load(expected_output_file)
-
-        test_data.append((input_file_path, output_file_path, expected_output_data))
-    return tuple(test_data)
-
-
-TEST_DATA = initialise_test_data()
+TEST_DATA = TestDataGenerator()
 
 
 @pytest.mark.parametrize('input_file_path, output_file_path, expected_output_data', TEST_DATA)
@@ -53,14 +25,9 @@ def test_process_data(
         output_file_path: path file where we save necessary stats
         expected_output_data: dict with correct statistics
     """
-    try:
+    # Ignore errors since they will be checked anyway.
+    with suppress(ValidationError, json.JSONDecodeError):
         process_data(input_file_path, output_file_path)
-    except ValidationError:
-        # Invalid data in file 2.json ('age': 'lol')
-        assert input_file_path == f'{INPUT_FILES_DIR_PATH}/2.json'
-    except json.JSONDecodeError:
-        # Invalid json syntax in 8.json (421: 57)
-        assert input_file_path == f'{INPUT_FILES_DIR_PATH}/8.json'
 
     with open(output_file_path) as output_file:
         output_data = json.load(output_file)
---
+++ hw2/utils/test_data_initialiser.py
@@ -0,0 +1,123 @@
+"""Module with an implemented TestDataInitialiser class."""
+import json
+import os
+from datetime import datetime, timedelta
+from typing import Generator, NoReturn
+
+from hw2.typesdev import TestDataItem, Users
+
+
+class TestDataGenerator():
+    """Class-generator representing all test data."""
+
+    _number_of_files = 10
+    _input_files_dir_path = 'hw2/test_files/input/'
+    _output_files_dir_path = 'hw2/test_files/output/'
+    _expected_files_dir_path = 'hw2/test_files/expected_output/'
+    _last_modification_date_file_path = 'hw2/last_modification_date'
+
+    def initialize_test_data(self) -> tuple[TestDataItem, ...]:
+        """Create a function that initialises test_data and returns it.
+
+        Returns:
+            The field containing the tuples of the following format: \
+                ( \
+                    input_file_path: path to file with users data, \
+                    output_file_path: path file where we save necessary stats, \
+                    expected_output_data: dict with correct statistics \
+                )
+        """
+        self._modify_test_data()
+
+        test_data = []
+        for filename in self._generate_filenames():
+            input_file_path = f'{self._input_files_dir_path}{filename}'
+            output_file_path = f'{self._output_files_dir_path}{filename}'
+
+            with open(f'{self._expected_files_dir_path}{filename}') as expected_output_file:
+                expected_output_data = json.load(expected_output_file)
+
+            test_data.append((input_file_path, output_file_path, expected_output_data))
+
+        return tuple(test_data)
+
+    def __iter__(self) -> Generator[tuple[TestDataItem, ...], None, None]:
+        """Create a magic method that returns generator containing all the test items.
+
+        Yields:
+            All the test items.
+        """
+        yield from self.initialize_test_data()
+
+    def _generate_filenames(self) -> Generator[str, None, None]:
+        """Create a function that returns a generator containing file names.
+
+        Yields:
+            The name for the file from 1.json to 10.json
+        """
+        yield from (f'{file_num}.json' for file_num in range(1, self._number_of_files + 1))
+
+    def _modify_test_data(self) -> NoReturn:
+        """Create a function that modifies the last_login of all users according to \
+        the current date so that the tests work with the already existing expected_data."""
+        if not os.path.exists(self._last_modification_date_file_path):
+            self._update_last_modification_date()
+            return
+
+        # load the last modification date and determine the difference with the current date
+        with open(self._last_modification_date_file_path, 'r') as last_modification_date_file:
+            last_modification_date = datetime.fromisoformat(
+                last_modification_date_file.read(),
+            )
+        date_diff: timedelta = datetime.now() - last_modification_date
+
+        for filename in self._generate_filenames():
+            input_file_path = f'{self._input_files_dir_path}{filename}'
+            with open(input_file_path, 'r') as input_file:
+                # skip files with invalid JSON syntax
+                try:
+                    input_data = json.load(input_file)
+                except json.JSONDecodeError:
+                    continue
+
+            updated_data: Users = self._update_input_data(input_data, date_diff)
+            self._save_updated_data(input_file_path, updated_data)
+
+        # update last_modification_date
+        self._update_last_modification_date()
+
+    def _update_last_modification_date(self) -> NoReturn:
+        """Create a function that updates the date in the last_modification_date \
+        file to the current date."""
+        with open(self._last_modification_date_file_path, 'w') as last_modification_date_file:
+            last_modification_date_file.write(datetime.now().strftime('%Y-%m-%d'))
+
+    def _update_input_data(self, input_user_data: Users, date_diff: timedelta) -> Users:
+        """Create a function that updates last_login field in received input_user_data \
+        and returns it.
+
+        Args:
+            input_user_data (Users): user data we need to update.
+            date_diff (timedelta): the difference between current date and last_modification_date.
+
+        Returns:
+            An updated input_user_data.
+        """
+        for user in input_user_data.values():
+            # skip files with invalid user model
+            try:
+                old_last_login_date = datetime.fromisoformat(user['last_login'])
+            except KeyError:
+                continue
+            user['last_login'] = (old_last_login_date + date_diff).strftime('%Y-%m-%d')
+        return input_user_data
+
+    def _save_updated_data(self, input_file_path: str, updated_data: Users) -> NoReturn:
+        """Create a function that writes an updated data to input_file.
+
+        Args:
+            input_file_path (str): path to the input file that we need to update.
+            updated_data (Users): data that we need to write to input file.
+        """
+        with open(input_file_path, 'w') as input_file:
+            json.dump(updated_data, input_file, indent=4, ensure_ascii=False)
