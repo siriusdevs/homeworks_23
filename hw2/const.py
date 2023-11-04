69cadf5268dd925af9b33a2c0b7255c6b2bc8566
Sam Komarov <sam@skomarov.com>
2023-11-04 23:03:34 +0300

    clairfy output fields' names

--- hw2/const.py
+++ hw2/const.py
@@ -11,22 +11,23 @@ AGE_AVERAGE = 'age_average'
 AGE_MEDIAN = 'age_median'
 
 
-LESS_TWO_DAYS = 'less_two_days'
-LESS_WEEK = 'less_week'
-LESS_MONTH = 'less_month'
-LESS_HALFYEAR = 'less_halfyear'
+LESS_TWO_DAYS = 'active_in_last_two_days_average_age'
+LESS_WEEK = 'active_in_last_week_average_age'
+LESS_MONTH = 'active_in_last_month_average_age'
+LESS_HALFYEAR = 'active_in_last_halfyear_average_age'
 
-GREATER_HALFYEAR = 'greater_halfyear'
+GREATER_HALFYEAR = 'not_active_in_last_halfyear_average_age'
 
 TimeFilterType = str
 LESS: TimeFilterType = 'LESS'
 GREATER: TimeFilterType = 'GREATER'
 
 YEAR_DAYS = 365
+MONTH_DAYS = 30
 TIMEDELTAS: tuple[TimeFilterType, str, timedelta] = (
     (LESS, LESS_TWO_DAYS, timedelta(days=2)),
     (LESS, LESS_WEEK, timedelta(weeks=1)),
-    (LESS, LESS_MONTH, timedelta(weeks=4)),
+    (LESS, LESS_MONTH, timedelta(days=MONTH_DAYS)),
     (LESS, LESS_HALFYEAR, timedelta(days=YEAR_DAYS/2)),
     (GREATER, GREATER_HALFYEAR, timedelta(days=YEAR_DAYS/2)),
 )
