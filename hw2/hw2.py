57d694709b6c9817c19ef85131e1b86176ca0fa1
Sam Komarov <sam@skomarov.com>
2023-11-04 16:04:39 +0300

    add test for an empty input

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -8,9 +8,40 @@ Provides types and functions for solving task_2.
 клиентов, которые были онлайн менее двух дней, недели, месяца, полугода, и более полугода назад.
 """
 
+import dataclasses
+import datetime
+
+
+@dataclasses.dataclass
+class AgeStats:
+    """Basic stats for a list of ages."""
+
+    min: float = 0
+    max: float = 0
+    mean: float = 0
+    median: float = 0
+
+
+YEAR_DAYS = 365
+TIMEDELTAS_LESS = (
+    ('less_2_days', datetime.timedelta(days=2)),
+    ('less_week', datetime.timedelta(weeks=1)),
+    ('less_month', datetime.timedelta(weeks=4)),
+    ('less_halfyear', datetime.timedelta(days=YEAR_DAYS/2)),
+)
+
+
+TIMEDELTAS_GREATER = (
+    ('greater_halfyear', datetime.timedelta(days=YEAR_DAYS/2)),
+)
+
 
 def aggregate_users_stats(input_file: str, output_file: str) -> None:
     """Read user stats from input_file, aggregate them and write to output_file.
+
+    Schema of the output file is a json object with fields corresponding to keys of
+    TIMEDELTAS_LESS and TIMEDELTAS_GREATER and values corresponding to AgeStats of users
+    who have been online according to these timedeltas.
 
     Args:
         input_file: path to a json file containing user stats
--- hw2/test_hw2.py
+++ hw2/test_hw2.py
@@ -1,13 +1,22 @@
 """pytest tests for hw2 module."""
+import dataclasses
 import json
 import tempfile
 from typing import Any
 
 import pytest
 
-from hw2 import aggregate_users_stats
+from hw2 import AgeStats, aggregate_users_stats
 
-TESTS_TABLE = ()
+TESTS_TABLE = (
+    ('test_data/empty.json', {
+        'less_2_days': dataclasses.asdict(AgeStats()),
+        'less_week': dataclasses.asdict(AgeStats()),
+        'less_month': dataclasses.asdict(AgeStats()),
+        'less_halfyear': dataclasses.asdict(AgeStats()),
+        'greater_halfyear': dataclasses.asdict(AgeStats()),
+    }),
+)
 
 
 @pytest.mark.parametrize('input_file, expected', TESTS_TABLE)
@@ -20,5 +29,5 @@ def test_aggregate_users_stats(input_file: str, expected: dict[str, Any]):
     """
     with tempfile.NamedTemporaryFile() as output:
         aggregate_users_stats(input_file, output.name)
-        got = json.read(output)
+        got = json.load(output)
         assert sorted(got.items()) == sorted(expected.items())
