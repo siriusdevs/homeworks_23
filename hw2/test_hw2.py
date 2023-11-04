ba4246f743afa162e355b32d32af7d3db95da597
Sam Komarov <sam@skomarov.com>
2023-11-04 16:43:30 +0300

    add test for asserting file creation

--- hw2/test_hw2.py
+++ hw2/test_hw2.py
@@ -1,6 +1,7 @@
 """pytest tests for hw2 module."""
 import dataclasses
 import json
+import os
 import tempfile
 from typing import Any
 
@@ -9,8 +10,10 @@ import pytest
 
 from hw2 import AgeStats, aggregate_users_stats
 
+EMPTY_INPUT_FILE = 'test_data/empty.json'
+
 TESTS_TABLE = (
-    ('test_data/empty.json', {
+    (EMPTY_INPUT_FILE, {
         const.LESS_TWO_DAYS: dataclasses.asdict(AgeStats()),
         const.LESS_WEEK: dataclasses.asdict(AgeStats()),
         const.LESS_MONTH: dataclasses.asdict(AgeStats()),
@@ -32,3 +35,14 @@ def test_aggregate_users_stats(input_file: str, expected: dict[str, Any]):
         aggregate_users_stats(input_file, output.name)
         got = json.load(output)
         assert sorted(got.items()) == sorted(expected.items())
+
+
+def test_aggregate_users_stats_file_creation():
+    """Asserts that aggregate_users_stats() creates the output file if it doesn't exist."""
+    tmp_output = tempfile.TemporaryFile()
+    output_path = str(tmp_output.name)
+    tmp_output.close()  # deletes this file, so that we can check file creation by tested function
+
+    aggregate_users_stats(EMPTY_INPUT_FILE, output_path)
+    assert os.path.exists(output_path)
+    os.remove(output_path)
