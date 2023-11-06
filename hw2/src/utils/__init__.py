3ee9eb9905a83fb57824da4822dc7202392aeec5
akiko23 <usksoaoko@gmail.com>
2023-11-06 19:05:46 +0300

    fix package docstring

--- hw2/src/utils/__init__.py
+++ hw2/src/utils/__init__.py
@@ -1,4 +1,4 @@
-"""Contain modules with classes and functions for process_data."""
+"""Contain modules with classes and funcs for hw2.process_data and test_hw2.test_process_data."""
 
 from .common import find_insertion_index, get_valid_dict_to_str
 from .test_data_initializer import TestDataGenerator
