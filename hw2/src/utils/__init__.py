47acee94f5f35f73478481dd35774952954935f2
akiko23 <usksoaoko@gmail.com>
2023-11-11 19:00:26 +0300

    add and leave only necessary funcs and classes

--- hw2/src/utils/__init__.py
+++ hw2/src/utils/__init__.py
@@ -1,5 +1,5 @@
 """Contain modules with classes and funcs for hw2.process_data and test_hw2.test_process_data."""
 
-from .common import find_insertion_index, get_valid_dict_to_str
-from .test_data_initializer import TestDataGenerator
+from .common import log_error_and_write_to_output_file
+from .initialize_test_data import TestDataGenerator
 from .user_stats import UserStatsUtils
