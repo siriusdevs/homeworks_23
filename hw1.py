8a6ebb397334e22eaddaa6b0ef771524d28b50b3
agent-ilyusha <fingerstylena@gmail.com>
2023-11-12 23:30:22 -0900

    update files

--- hw1.py
+++ hw1.py
@@ -1,32 +1,39 @@
 # --encoding: utf-8
+from typing import Tuple, List
+
 """
 Search for three maximum salaries for employees.
 """
 
-from typing import Tuple, List
-
-
-def check_sum_solary(*args: Tuple[str, List[float]], salary_cap: None or float = None) ->\
+def check_sum_salary(*args: Tuple[str, List[float]], salary_cap: None or float = None) ->\
         Tuple[List[float], float] or int:
+
     """
     Function searches three max salaries and return their.
      if salary_cap is less than 0, then it will be equal to 0.
-    Args:
+
+    Parameters:
         args: Tuple[str, List[float]] - takes two arguments: name company and all salaries
-        salary_cap: Any - maximum salary
+        salary_cap: None | float - maximum salary
+
     Returns:
         Tuple[List[float], float] - top three salaries in company and percentage of salary
     """
+
     if salary_cap is not None and salary_cap < 0:
         salary_cap = 0
+
     list_all_salaries: List[float] = list()
     list_three_max_salaries: List[float] = list()
 
     for i in range(len(args)):
+
+        if not isinstance(args[i][0], str):
+            raise TypeError("Неверный тип")
+
         list_all_salaries.extend(args[i][1])
 
     list_all_salaries.sort(reverse=True)
-
     list_all_salaries = [salary for salary in list_all_salaries if salary >= 0]
 
     if salary_cap is not None:
@@ -38,6 +45,7 @@ def check_sum_solary(*args: Tuple[str, List[float]], salary_cap: None or float =
             if len(list_three_max_salaries) == 3:
                 list_all_salaries = list_all_salaries[-list_all_salaries.index(salary)-1:]
                 break
+
     else:
         list_three_max_salaries.extend(list_all_salaries[:3])
 
--- test_hw1.py
+++ test_hw1.py
@@ -1,20 +1,18 @@
 # --encoding: utf-8
+import pytest
+from typing import Tuple, List
+from hw1 import check_sum_salary
 
 """
 Function for test main file.
 """
 
-import pytest
-from typing import Tuple, List
-from hw1 import check_sum_solary
-
-
 test_argument = [
-    ((('a', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]),), 9, ([8, 7, 6], 58.33)),
-    ((('a', [1, 0.1, 3]), ('b', [0, 5, 1.1])), None, ([5, 3, 1.1], 89.22)),
-    ((('a', [1, 0.2, 3]), ('b', [0, 5, 1.2]), ('c', [10, 11.1, 14.1])), None,
+    ((('v', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]),), 9, ([8, 7, 6], 58.33)),
+    ((('b', [1, 0.1, 3]), ('n', [0, 5, 1.1])), None, ([5, 3, 1.1], 89.22)),
+    ((('z', [1, 0.2, 3]), ('t', [0, 5, 1.2]), ('ba', [10, 11.1, 14.1])), None,
      ([14.1, 11.1, 10], 77.19)),
-    ((('a', [-1, -2, -3]), ('b', [0, -7, -1.1])), None, 0)
+    ((('a', [-1, -2, -3]), ('u', [0, -7, -1.1])), None, 0)
 ]
 
 
@@ -25,10 +23,9 @@ def test_func(
         answer: Tuple[List[float], float]):
     """
     Checks answers.
-    Args:
+    Parameters:
         args: Tuple[str, List[float]] - accepts department name and their salaries
         answer: Tuple[List[float], float] - top three salaries in company and percentage of salary
         salary_cap: None or float - maximum salary
-    Returns:
     """
-    assert check_sum_solary(*args, salary_cap=salary_cap) == answer
+    assert check_sum_salary(*args, salary_cap=salary_cap) == answer
