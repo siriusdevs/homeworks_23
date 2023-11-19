baff49cb27faef051bd2c3942b4b181fbce5f9af
Артём Тюрин <zientenin@mail.ru>
2023-11-19 20:43:41 +0300

    Took out the counter list from the function

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -1,15 +1,18 @@
 import json
 from datetime import datetime
 
+
+online_status_count = {
+    "less_than_2_days": 0,
+    "less_than_a_week": 0,
+    "less_than_a_month": 0,
+    "less_than_six_months": 0,
+    "more_than_six_months": 0
+}
+
+
 def process_data(input_path, output_path):
     hosts_count = dict()
-    online_status_count = {
-        "less_than_2_days": 0,
-        "less_than_a_week": 0,
-        "less_than_a_month": 0,
-        "less_than_six_months": 0,
-        "more_than_six_months": 0
-    }
     with open(input_path, 'r') as input_file:
         data = json.load(input_file)
     TOTAL_CLIENTS = len(data)
@@ -27,20 +30,24 @@ def process_data(input_path, output_path):
                 raise Exception(f'last_login field is empty for client {client}')
             date = datetime.strptime(last_login, '%Y-%m-%d')
             last_login_ago = (datetime.now() - date).days
-            if last_login_ago < 2:
-                online_status_count['less_than_2_days'] += 1
-            elif last_login_ago < 7:
-                online_status_count['less_than_a_week'] += 1
-            elif last_login_ago < 30:
-                online_status_count['less_than_a_month'] += 1
-            elif last_login_ago < (30 * 6):
-                online_status_count['less_than_six_months'] += 1
-            else:
-                online_status_count['more_than_six_months'] += 1
+            fill_online_status_count(last_login_ago)
         except KeyError:
             raise Exception(f'No last_login field for client {client}.')
     hosts_percentage = {host: (count / TOTAL_CLIENTS) * 100 for host, count in hosts_count.items()}
     print(online_status_count)
+
+
+def fill_online_status_count(last_login_ago: int) -> None:
+    if last_login_ago < 2:
+        online_status_count['less_than_2_days'] += 1
+    elif last_login_ago < 7:
+        online_status_count['less_than_a_week'] += 1
+    elif last_login_ago < 30:
+        online_status_count['less_than_a_month'] += 1
+    elif last_login_ago < (30 * 6):
+        online_status_count['less_than_six_months'] += 1
+    else:
+        online_status_count['more_than_six_months'] += 1
 
 
 process_data('/home/tire/Documents/Sirius_dev/homeworks_23/data_hw2.json', 'ululu')
