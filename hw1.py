9b01d8c9d91131818d108344b4fe1505a09d9901
viktoriatyapkova <tyapkova06@bk.ru>
2023-11-20 13:39:37 +0300

    removed the department overwrite, fixed the problem with raise Exception, return tuples

--- hw1.py
+++ hw1.py
@@ -1,7 +1,7 @@
 """Module for calculating top-3 high and low paid departments."""
 
 
-def top_departments(department: dict[str, dict], limit: float = None) -> dict:
+def top_departments(department: dict[str, dict], limit: float = None) -> tuple:
     """
     Calculate the average salary of departments.
 
@@ -10,23 +10,19 @@ def top_departments(department: dict[str, dict], limit: float = None) -> dict:
         limit: float - optional arg(default=None), above which salaries are not taken into account.
 
     Returns:
-        dict: the first - 3 highly paid, the second - 3 low-paid departments.
+        tuple:  first - 3 highly paid, second - 3 low-paid departments.
 
-    Raises:
-        Exception: if not salaries are found below the entered limit.
     """
     new = {}
     for depts, employee in department.items():
         if limit:
             filt = {employee: salary for employee, salary in employee.items() if salary < limit}
             if not filt:
-                raise Exception('Нет данных по введенному лимиту!')
-            department[depts] = filt
+                continue
             salary = filt.values()
         else:
             salary = employee.values()
         new[depts] = round(sum(salary) / len(salary), 2)
     high_paid_res = (sorted(new, key=new.get, reverse=True))[:3]
     low_paid_res = (sorted(new, key=new.get))[:3]
-    return f'Топ высокооплачиваемых отделов: {high_paid_res}', \
-        f'Топ низкооплачиваемых отделов: {low_paid_res}'
+    return high_paid_res, low_paid_res
