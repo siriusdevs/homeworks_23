efce908e0b6f5faf5ee2e133114d1a0df8b58bb1
agent-yandex <fox.online12@gmail.com>
2023-11-27 14:09:44 +0300

    Исправление ошибок flake8

--- hw1.py
+++ hw1.py
@@ -1,68 +1,67 @@
-"""Module, that has one function "check_arguments" and "top_salary", homework 1."""
-from itertools import chain
+"""Module, provides functions for solving task_1."""
+from typing import Tuple
 
 
-def check_arguments(departments: dict[str, ...], limit_salary: int | float):
-    """Check the validity of the arguments.
-
-    Args:
-        departments: A dictionary representing departments and their workers
-            where the keys are department names (str) and the values are dictionaries of workers
-            where the keys are worker names (str) and the values are their salaries (float).
-        limit_salary: An optional argument representing the salary limit.
-
-    Raises:
-        ValueError: If no arguments are provided in the departments dictionary.
-        TypeError: If the department or workers is not of the correct type.
-        TypeError: If the worker or salary is not of the correct type.
-        TypeError: If the optional argument limit_salary is not an int or float.
-    """
-    if not departments:
-        raise ValueError('No one argument in departments')
-
-    for department, workers in departments.items():
-        if not isinstance(department, str) or not isinstance(workers, dict):
-            raise TypeError('Wrong type of department or workers')
-
-        for worker, salary in workers.items():
-            if not isinstance(worker, str) or not isinstance(salary, float):
-                raise TypeError('Wrong type of worker or salary')
-
-    if limit_salary and not isinstance(limit_salary, (int, float)):
-        raise TypeError(f'Optional argument limit_salary must be int or float, '
-                        f'but got {type(limit_salary).__name__}')
-
-
-def top_salary(
-    limit_salary: int | float = None,
-    **departments: dict[str, dict],
-) -> (list[float], float):
-    """Calculates the top salaries and the percentage they represent.
+def get_all_salaries(departments: dict[str, dict]) -> list[float]:
+    """Get a sorted list of all salaries across departments.
 
     Args:
-        limit_salary: The salary limit. Default is None.
-        **departments: Variable keyword arguments representing departments
-            and their workers. The keys are department names (str) and the values are
-            dictionaries of workers where the keys are worker names (str) and the
-            values are their salaries (float).
+        departments (dict): A dictionary representing departments and their workers.
+            Keys are department names (str), and values are dictionaries of workers,
+            where keys are worker names (str), and values are their salaries (float).
 
     Returns:
-        tuple: A tuple containing a list of the top salaries (rounded to 2 decimal places) and
-            the percentage these salaries represent (rounded to 2 decimal places).
-
-    Raises:
-        ValueError: If there are no arguments in the departments.
+        list[float]: A sorted list of all salaries from all workers in the provided departments,
+            in descending order.
     """
-    check_arguments(departments, limit_salary)
+    all_salaries = [salary for workers in departments.values() for salary in workers.values()]
 
-    all_salaries = sorted(list(chain.from_iterable(workers.values() for workers in
-                                                   departments.values())), reverse=True)
+    return sorted(all_salaries, reverse=True)
 
+
+def filter_top_salaries(all_salaries: list[float], limit_salary: int | float) -> list[float]:
+    """Filter top salaries based on the provided salary limit.
+
+    Args:
+        all_salaries (list[float]): A list of all salaries to be filtered.
+        limit_salary (int | float): The salary limit. Salaries above this limit will be excluded.
+
+    Returns:
+        list[float]: A list containing the top salaries (up to 3) based on the provided limit.
+    """
     if limit_salary:
         top = list(filter(lambda salary: salary < limit_salary, all_salaries))[:3]
     else:
         top = all_salaries[:3]
 
+    return top
+
+
+def top_salary(
+    limit_salary: int | float = None,
+    **departments: dict[str, dict],
+) -> Tuple[list[float], float]:
+    """Calculate the top salaries and the percentage they represent.
+
+    Args:
+        limit_salary: The salary limit, default is None.
+        departments (dict): A dictionary representing departments and their workers.
+
+    Returns:
+        tuple: A tuple containing a list of the top salaries (rounded to 2 decimal
+        places) and the percentage these salaries represent (rounded to
+        2 decimal places).
+    """
+    all_salaries = get_all_salaries(departments)
+    top = filter_top_salaries(all_salaries, limit_salary)
     percent = round(sum(top) / sum(all_salaries) * 100, 2)
 
     return [round(salary, 2) for salary in top], percent
+
+
+data = {
+            'HR2': {'BobMArk': 4000.0, 'Mark': 4500.0, 'KAtya': 4200.0},
+            'Sales2': {'Juse': 5500.0, 'Pasito': 5200.0},
+        }
+
+print(top_salary(**data))
