c3739d2b6f6a20a5db18052ab71a24fd33acfe43
Rin2396 <kolkareva.dayana2016@gmail.com>
2023-12-04 10:53:21 +0300

    fix

--- hw2.py
+++ hw2.py
@@ -1,5 +1,6 @@
 """Function that calculates website usage statistics."""
 import json
+import os
 from datetime import datetime
 
 MONTH = 30
@@ -14,6 +15,29 @@ time_stats = {
 }
 
 
+def check_is_file(file_name: str) -> bool:
+    """Chekcs that file is a file."""
+    if not os.path.isfile(file_name):
+        raise FileNotFoundError(f'<{file_name}> is not a file or is not exist.')
+
+
+def check_by_time(user_info: dict) -> None:
+    """Dictributes age over time."""
+    age = user_info.get('age')
+    online = datetime.now() - datetime.strptime(user_info.get('last_login'), '%Y-%m-%d')
+
+    if online.days <= 2:
+        time_stats['less_than_2_days'].append(age)
+    elif online.days <= 7:
+        time_stats['less_than_1_week'].append(age)
+    elif online.days <= MONTH:
+        time_stats['less_then_1_month'].append(age)
+    elif online.days <= HALF_YEAR:
+        time_stats['less_then_6_months'].append(age)
+    else:
+        time_stats['more_then_6_months'].append(age)
+
+
 def process_data(input_data, output_data):
     """Calculate websites usage statistics.
 
@@ -22,8 +46,9 @@ def process_data(input_data, output_data):
         output_data: name of the output data file.
 
     """
-    with open(input_data) as in_fl:
-        information: dict[str, dict] = json.load(in_fl)
+    check_is_file(input_data)
+    with open(input_data) as input_file:
+        information: dict[str, dict] = json.load(input_file)
 
     for user_info in information.values():
         email: str = user_info.get('email')
@@ -34,33 +59,18 @@ def process_data(input_data, output_data):
             if len(email_parts) == 2:
                 mail_host[email_parts[1]] += 1
 
-        age = user_info.get('age')
-        online = datetime.now() - datetime.strptime(user_info.get('last_login'), '%Y-%m-%d')
+        check_by_time(user_info)
 
-        if online.days <= 2:
-            time_stats['less_than_2_days'].append(age)
-        elif online.days <= 7:
-            time_stats['less_than_1_week'].append(age)
-        elif online.days <= MONTH:
-            time_stats['less_then_1_month'].append(age)
-        elif online.days <= HALF_YEAR:
-            time_stats['less_then_6_months'].append(age)
-        else:
-            time_stats['more_then_6_months'].append(age)
-
-    for online_time, user_quantity in time_stats.values():
-        if user_quantity:
-            time_stats[online_time] = sum(user_quantity) / len(user_quantity)
-        else:
-            time_stats[online_time] = 0
+    for user_quantity in time_stats.values():
+        user_quantity = sum(user_quantity) / len(user_quantity) if user_quantity else 0
 
     stats: dict[str, dict[str, list]] = {
         'email_usage': mail_host,
         'time_stats': time_stats,
         }
 
-    with open(output_data, 'w') as out_fl:
-        json.dump(stats, out_fl, indent=4, ensure_ascii=False)
+    with open(output_data, 'w') as output_file:
+        json.dump(stats, output_file, indent=4, ensure_ascii=False)
 
 
 if __name__ == '__main__':
