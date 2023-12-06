b78acf158ecfd8f568bf75858412a0fb91a1a788
Maksim <74344870+Rubsun@users.noreply.github.com>
2023-12-06 19:04:55 +0300

    add parent directory for paths

--- hw2/test_hw2.py
+++ hw2/test_hw2.py
@@ -6,33 +6,33 @@ import pytest
 
 import hw2
 
-PATH = 'hw2/tests_folder/output.json'
-PATH_ERROR = 'hw2/error.json'
+PATH = 'tests_folder/output.json'
+PATH_ERROR = 'error.json'
 
 
 TEST_DIFFERENT_ARGUMENTS = (
-    ('tests_folder/test1.json', 'expected_folder/1.json'),
-    ('tests_folder/test2.json', 'expected_folder/2.json'),
+    ('hw2/tests_folder/test1.json', 'hw2/expected_folder/1.json'),
+    ('hw2/tests_folder/test2.json', 'hw2/expected_folder/2.json'),
 )
 
 
 TEST_WITHOUT_DIFFERENT_ARGUMENTS = (
-    ('tests_folder/test3.json', 'expected_folder/3.json'),
-    ('tests_folder/test4.json', 'expected_folder/4.json'),
-    ('tests_folder/test5.json', 'expected_folder/5.json'),
+    ('hw2/tests_folder/test3.json', 'hw2/expected_folder/3.json'),
+    ('hw2/tests_folder/test4.json', 'hw2/expected_folder/4.json'),
+    ('hw2/tests_folder/test5.json', 'hw2/expected_folder/5.json'),
 )
 
 
 TEST_WITH_WRONG_TYPE_VARIABLES = (
-    ('tests_folder/test6.json', 'expected_folder/6.json'),
+    ('hw2/tests_folder/test6.json', 'hw2/expected_folder/6.json'),
 )
 
 
 TEST_WITH_ERRORS = (
-    ('tests_folder/test7.json', 'expected_folder/7.json'),
-    ('tests_folder/test9.txt', 'expected_folder/9.json'),
-    ('tests_folder/test10.txt', 'expected_folder/10.json'),
-    ('tests_folder/tes11.json', 'expected_folder/11.json'),
+    ('hw2/tests_folder/test7.json', 'hw2/expected_folder/7.json'),
+    ('hw2/tests_folder/test9.txt', 'hw2/expected_folder/9.json'),
+    ('hw2/tests_folder/test10.txt', 'hw2/expected_folder/10.json'),
+    ('hw2/tests_folder/tes11.json', 'hw2/expected_folder/11.json'),
 )
 
 
