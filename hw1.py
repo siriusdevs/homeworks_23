cf2ffe38a79f13aaaff9217f299d44d864404f59
agent-yandex <fox.online12@gmail.com>
2023-12-12 13:10:20 +0300

    Исправление недочетов в исполняемом файле

--- hw1.py
+++ hw1.py
@@ -1,46 +1,38 @@
 """Module, provides functions for solving task_1."""
-from typing import Tuple
 
 
-def get_all_salaries(departments: dict[str, dict]) -> list[float]:
+def get_all_salaries(departments: dict[str, dict], limit_salary: int | float) -> list[float]:
     """Get a sorted list of all salaries across departments.
 
     Args:
         departments (dict): A dictionary representing departments and their workers.
-            Keys are department names (str), and values are dictionaries of workers,
-            where keys are worker names (str), and values are their salaries (float).
+        limit_salary (int | float): The salary limit. Salaries above this limit will be excluded.
 
     Returns:
         list[float]: A sorted list of all salaries from all workers in the provided departments,
             in descending order.
     """
-    all_salaries = [salary for workers in departments.values() for salary in workers.values()]
+    if limit_salary:
+        all_salaries = [
+            salary
+            for workers in departments.values()
+            for salary in workers.values()
+            if salary < limit_salary
+        ]
+    else:
+        all_salaries = [
+            salary
+            for workers in departments.values()
+            for salary in workers.values()
+        ]
 
     return sorted(all_salaries, reverse=True)
-
-
-def filter_top_salaries(all_salaries: list[float], limit_salary: int | float) -> list[float]:
-    """Filter top salaries based on the provided salary limit.
-
-    Args:
-        all_salaries (list[float]): A list of all salaries to be filtered.
-        limit_salary (int | float): The salary limit. Salaries above this limit will be excluded.
-
-    Returns:
-        list[float]: A list containing the top salaries (up to 3) based on the provided limit.
-    """
-    if limit_salary:
-        top = list(filter(lambda salary: salary < limit_salary, all_salaries))[:3]
-    else:
-        top = all_salaries[:3]
-
-    return top
 
 
 def top_salary(
     limit_salary: int | float = None,
     **departments: dict[str, dict],
-) -> Tuple[list[float], float]:
+) -> tuple[list[float], float]:
     """Calculate the top salaries and the percentage they represent.
 
     Args:
@@ -52,8 +44,8 @@ def top_salary(
         places) and the percentage these salaries represent (rounded to
         2 decimal places).
     """
-    all_salaries = get_all_salaries(departments)
-    top = filter_top_salaries(all_salaries, limit_salary)
+    all_salaries = get_all_salaries(departments, limit_salary)
+    top = all_salaries[:3]
 
     if sum(all_salaries) == 0:
         percent = 0
