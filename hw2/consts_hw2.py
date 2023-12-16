0d5390d2259ac20bdf9788a4287bf7f1ef56ac0e
Dimmension <vadimtio1221@mail.ru>
2023-12-16 23:46:29 +0300

    add hw2 and tests for hw2

---
+++ hw2/consts_hw2.py
@@ -0,0 +1,14 @@
+"""Provides constants for hw2."""
+
+AGE = 'age'
+AGE_MAX = 'age_max'
+AGE_MIN = 'age_min'
+AGE_AVERAGE = 'age_average'
+AGE_MEDIAN = 'age_median'
+
+LAST_LOGIN = 'last_login'
+LESS_TWO_DAYS = 'percent_active_users_last_two_days'
+LESS_WEEK = 'percent_active_users_last_week'
+LESS_MONTH = 'percent_active_users_last_month'
+LESS_HALFYEAR = 'percent_active_users_last_halfyear'
+MORE_HALFYEAR = 'percent_active_not_users_last_halfyear'
