cce8bf001d003786e809c52efc84ce2788a0b2c4
nnnhurt <nnnzagora625@mail.ru>
2023-12-17 19:57:18 +0300

    fixed problems

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -26,15 +26,15 @@ def check(new_value: Any, classes: tuple[type] | type[Any]):
 class Weapon:
     """Class for weapons."""
 
-    def __init__(self, name_weapon: str, damage: int | float) -> None:
+    def __init__(self, name: str, damage: int | float) -> None:
         """
         Name of weapon and its damage.
 
         Args:
-            name_weapon (str): name of weapon.
+            name (str): name of weapon.
             damage (float): damage of weapon.
         """
-        self.name_weapon = name_weapon
+        self.name = name
         self.damage = damage
 
     def __str__(self) -> str:
@@ -44,28 +44,33 @@ class Weapon:
         Returns:
             str: string with name of weapon.
         """
-        return self.name_weapon
+        return self.name
 
     @property
-    def name_weapon(self) -> str:
+    def name(self) -> str:
         """
         Getter for name of a weapon.
 
         Returns:
             str: name of a weapon.
         """
-        return self._name_weapon
+        return self._name
 
-    @name_weapon.setter
-    def name_weapon(self, new_name_weapon: str) -> None:
+    @name.setter
+    def name(self, new_name: str) -> None:
         """
         Setter for name of a weapon.
 
         Args:
-            new_name_weapon (str): new name of a weapon.
+            new_name (str): new name of a weapon.
+
+        Raises:
+            ValueError: error if name is empty.
         """
-        check(new_name_weapon, (str))
-        self._name_weapon = new_name_weapon
+        check(new_name, (str))
+        if new_name == '':
+            raise ValueError(f'{new_name} must have name!')
+        self._name = new_name
 
     @property
     def damage(self) -> float | int:
@@ -78,7 +83,7 @@ class Weapon:
         return self._damage
 
     @damage.setter
-    def damage(self, new_damage: str) -> None:
+    def damage(self, new_damage: float | int) -> None:
         """
         Setter for damage of a weapon.
 
@@ -90,21 +95,21 @@ class Weapon:
         """
         check(new_damage, (float | int))
         if new_damage < 0:
-            raise ValueError(f'Damage of {self.name_weapon} is less than zero')
+            raise ValueError(f'Damage of {self.name} is less than zero')
         self._damage = new_damage
 
 
 class Unit:
     """Class with units."""
 
-    def __init__(self, name: str, health: int | float, weapon: str) -> None:
+    def __init__(self, name: str, health: int | float, weapon: Weapon) -> None:
         """
         Name, health and weapon for unit.
 
         Args:
             name (str): name of unit.
             health (int | float): health of unit.
-            weapon (str): weapon of unit.
+            weapon (Weapon): weapon of unit.
         """
         self.name = name
         self.health = health
@@ -127,8 +132,13 @@ class Unit:
 
         Args:
             new_name (str): new name of unit.
+
+        Raises:
+            ValueError: error if name is empty.
         """
         check(new_name, (str))
+        if new_name == '':
+            raise ValueError(f'{new_name} must have name!')
         self._name = new_name
 
     @property
@@ -158,25 +168,25 @@ class Unit:
         self._health = float(new_health) if new_health is int else new_health
 
     @property
-    def name_weapon(self) -> str:
+    def weapon(self) -> Weapon:
         """
         Getter for name of weapon.
 
         Returns:
-            str: string with the name of weapon
+            Weapon: string with the name of weapon
         """
-        return self._name_weapon
+        return self._weapon
 
-    @name_weapon.setter
-    def name_weapon(self, new_name_weapon: str) -> None:
+    @weapon.setter
+    def weapon(self, new_weapon: Weapon) -> None:
         """
         Setter for name of weapon.
 
         Args:
-            new_name_weapon (str): string with new name of weapon.
+            new_weapon (Weapon): string with new name of weapon.
         """
-        check(new_name_weapon, (str))
-        self._name_weapon = new_name_weapon
+        check(new_weapon, (Weapon))
+        self._weapon = new_weapon
 
     def __str__(self) -> str:
         """
@@ -203,27 +213,18 @@ class Unit:
 class Player(Unit):
     """Class players."""
 
-    def __init__(self, name: str, health: int | float, weapon: str, level: int) -> None:
+    def __init__(self, name: str, health: int | float, weapon: Weapon, level: int) -> None:
         """
         Create name, health, weapon and level for player.
 
         Args:
             name (str): name of player.
             health (int | float): health of player.
-            weapon (str): weapon of player.
+            weapon (Weapon): weapon of player.
             level (int): level of player.
         """
         super().__init__(name, health, weapon)
         self.level = level
-
-    def __str__(self) -> str:
-        """
-        Display the characteristics of a player.
-
-        Returns:
-            str: string with name of a player.
-        """
-        return self.name
 
     @property
     def level(self) -> int:
@@ -254,15 +255,6 @@ class Player(Unit):
 
 class Enemy(Unit):
     """Class enemies."""
-
-    def __str__(self) -> str:
-        """
-        Display the characteristics of a enemy.
-
-        Returns:
-            str: string with name of a enemy.
-        """
-        return self.name
 
 
 class Game:
@@ -347,17 +339,7 @@ class Game:
         self.players.append(new_player)
 
     def _remove_player(self, old_player: Player) -> None:
-        for index, player in enumerate(self.__players):
-            correct_attrs = (
-                player.name == old_player.name
-                and player.health == old_player.health
-                and player.weapon == old_player.weapon
-                and player.level == old_player.level
-            )
-            if correct_attrs:
-                self.__players.pop(index)
-                return
-        raise ValueError(f'{old_player.name} not in players')
+        self.players.remove(old_player)
 
     @property
     def weapons(self) -> list[Weapon]:
@@ -375,15 +357,7 @@ class Game:
         self.weapons.append(new_wepon)
 
     def _remove_weapon(self, old_weapon: Weapon) -> None:
-        for index, weapon in enumerate(self.__weapons):
-            correct_attrs = (
-                weapon.name_weapon == old_weapon.name_weapon
-                and weapon.damage == old_weapon.damage
-            )
-            if correct_attrs:
-                self.__weapons.pop(index)
-                return
-        raise ValueError(f'{old_weapon.name} not in weapons')
+        self.weapons.remove(old_weapon)
 
     @property
     def enemies(self) -> list[Enemy]:
@@ -401,13 +375,4 @@ class Game:
         self.enemies.append(new_enemy)
 
     def _remove_enemy(self, old_enemy: Enemy) -> None:
-        for index, enemy in enumerate(self.__enemies):
-            correct_attrs = (
-                enemy.name == old_enemy.name
-                and enemy.health == old_enemy.health
-                and enemy.weapon == old_enemy.weapon
-            )
-            if correct_attrs:
-                self.__enemies.pop(index)
-                return
-        raise ValueError(f'{old_enemy.name} not in enemies')
+        self.enemies.remove(old_enemy)
--- hw3/test_hw3.py
+++ hw3/test_hw3.py
@@ -48,20 +48,20 @@ def test_weapon_fail(name_weapon: str, damage: int | float) -> None:
 
 
 TEST_PLAYER = (
-    ('Goblin', HEALTH_PLAYER, 'Stone', LEVEL),
-    ('Wizard', HEALTH_PLAYER, 'Magic wang', LEVEL),
+    ('Goblin', HEALTH_PLAYER, Weapon('Stone', DAMAGE), LEVEL),
+    ('Wizard', HEALTH_PLAYER, Weapon('Magic wang', DAMAGE), LEVEL),
 )
 
 
 @pytest.mark.parametrize('name, health, weapon, level', TEST_PLAYER)
-def test_player(name: str, health: int | float, weapon: str, level: int) -> None:
+def test_player(name: str, health: int | float, weapon: Weapon, level: int) -> None:
     """
     Test function for player.
 
     Args:
         name (str): name of player.
         health (int | float): health of player.
-        weapon (str): weapon of player.
+        weapon (Weapon): weapon of player.
         level (int): level of player.
     """
     assert Player(name, health, weapon, level)
@@ -74,14 +74,14 @@ TEST_PLAYER_FAIL = (
 
 
 @pytest.mark.parametrize('name, health, weapon, level', TEST_PLAYER_FAIL)
-def test_player_fail(name: str, health: int | float, weapon: str, level: int) -> None:
+def test_player_fail(name: str, health: int | float, weapon: Weapon, level: int) -> None:
     """
     Test function for wrong player.
 
     Args:
         name (str): name of player.
         health (int | float): health of player.
-        weapon (str): weapon of player.
+        weapon (Weapon): weapon of player.
         level (int): level of player.
     """
     with pytest.raises(TypeError):
@@ -89,20 +89,20 @@ def test_player_fail(name: str, health: int | float, weapon: str, level: int) ->
 
 
 TEST_ENEMY = (
-    ('Dragon', HEALTH_ENEMY, 'Teeth'),
-    ('Druid', HEALTH_ENEMY, 'Magic'),
+    ('Dragon', HEALTH_ENEMY, Weapon('Teeth', DAMAGE)),
+    ('Druid', HEALTH_ENEMY, Weapon('Magic', DAMAGE)),
 )
 
 
 @pytest.mark.parametrize('name, health, weapon', TEST_ENEMY)
-def test_enemy(name: str, health: int | float, weapon: str) -> None:
+def test_enemy(name: str, health: int | float, weapon: Weapon) -> None:
     """
     Test function for enemy.
 
     Args:
         name (str): name of enemy.
         health (int | float): health of enemy.
-        weapon (str): weapon of enemy.
+        weapon (Weapon): weapon of enemy.
     """
     assert Enemy(name, health, weapon)
 
@@ -114,14 +114,14 @@ TEST_ENEMY_FAIL = (
 
 
 @pytest.mark.parametrize('name, health, weapon', TEST_ENEMY_FAIL)
-def test_enemy_fail(name: str, health: int | float, weapon: str) -> None:
+def test_enemy_fail(name: str, health: int | float, weapon: Weapon) -> None:
     """
     Test function for wrong enemy.
 
     Args:
         name (str): name of enemy.
         health (int | float): health of enemy.
-        weapon (str): weapon of enemy
+        weapon (Weapon): weapon of enemy
     """
     with pytest.raises(TypeError):
         assert Enemy(name, health, weapon)
@@ -129,11 +129,14 @@ def test_enemy_fail(name: str, health: int | float, weapon: str) -> None:
 
 TEST_ADD_PLAYER = (
     [
-        Player('Monkey', HEALTH_PLAYER, 'Stick', LEVEL),
-        Player('Rabbit', HEALTH_PLAYER, 'Stone', LEVEL),
+        Player('Monkey', HEALTH_PLAYER, Weapon('Stick', DAMAGE), LEVEL),
+        Player('Rabbit', HEALTH_PLAYER, Weapon('Stone', DAMAGE), LEVEL),
     ],
     [Weapon('Stick', DAMAGE), Weapon('Wifi', DAMAGE)],
-    [Enemy('Wolf', HEALTH_ENEMY, 'Teeth'), Enemy('Tiger', HEALTH_ENEMY, 'Claws')],
+    [
+        Enemy('Wolf', HEALTH_ENEMY, Weapon('Teeth', DAMAGE)),
+        Enemy('Tiger', HEALTH_ENEMY, Weapon('Claws', DAMAGE)),
+    ],
 )
 
 
@@ -145,18 +148,21 @@ def test_add_player():
         True if new_player add.
     """
     game = Game(*TEST_ADD_PLAYER)
-    new_player = Player('Dog', HEALTH_PLAYER, 'Brain', LEVEL)
+    new_player = Player('Dog', HEALTH_PLAYER, Weapon('Brain', DAMAGE), LEVEL)
     game += new_player
     assert new_player in game.players
 
 
 TEST_REMOVE_PLAYER = (
     [
-        Player('Goblin', HEALTH_PLAYER, 'Lopata', LEVEL),
-        Player('Wizard', HEALTH_PLAYER, 'Magic wang', LEVEL),
+        Player('Goblin', HEALTH_PLAYER, Weapon('Lopata', DAMAGE), LEVEL),
+        Player('Wizard', HEALTH_PLAYER, Weapon('Magic wang', DAMAGE), LEVEL),
     ],
     [Weapon('Sword', DAMAGE), Weapon('Brute', DAMAGE)],
-    [Enemy('Dragon', HEALTH_ENEMY, 'Teeth'), Enemy('Druid', HEALTH_ENEMY, 'Magic')],
+    [
+        Enemy('Dragon', HEALTH_ENEMY, Weapon('Teeth', DAMAGE)),
+        Enemy('Druid', HEALTH_ENEMY, Weapon('Magic', DAMAGE)),
+    ],
 )
 
 
@@ -168,18 +174,22 @@ def test_remove_player():
         True if old_player remove.
     """
     game = Game(*TEST_REMOVE_PLAYER)
-    old_player = Player('Goblin', HEALTH_PLAYER, 'Lopata', LEVEL)
+    old_player = Player('Goblin', HEALTH_PLAYER, Weapon('Lopata', DAMAGE), LEVEL)
+    game += old_player
     game -= old_player
     assert old_player not in game.players
 
 
 TEST_ADD_WEAPON = (
     [
-        Player('Pug', HEALTH_PLAYER, 'Socket', LEVEL),
-        Player('Corgi', HEALTH_PLAYER, 'Bread', LEVEL),
+        Player('Pug', HEALTH_PLAYER, Weapon('Socket', DAMAGE), LEVEL),
+        Player('Corgi', HEALTH_PLAYER, Weapon('Bread', DAMAGE), LEVEL),
     ],
     [Weapon('Knife', DAMAGE), Weapon('Fork', DAMAGE)],
-    [Enemy('Chiheahua', HEALTH_ENEMY, 'Scream'), Enemy('Doberman', HEALTH_ENEMY, 'Evil face')],
+    [
+        Enemy('Chiheahua', HEALTH_ENEMY, Weapon('Scream', DAMAGE)),
+        Enemy('Doberman', HEALTH_ENEMY, Weapon('Evil face', DAMAGE)),
+    ],
 )
 
 
@@ -198,11 +208,14 @@ def test_add_weapon():
 
 TEST_REMOVE_WEAPON = (
     [
-        Player('Pug', HEALTH_PLAYER, 'Socket', LEVEL),
-        Player('Corgi', HEALTH_PLAYER, 'Bread', LEVEL),
+        Player('Pug', HEALTH_PLAYER, Weapon('Socket', DAMAGE), LEVEL),
+        Player('Corgi', HEALTH_PLAYER, Weapon('Bread', DAMAGE), LEVEL),
     ],
     [Weapon('Pan', DAMAGE), Weapon('Plate', DAMAGE)],
-    [Enemy('Chiheahua', HEALTH_ENEMY, 'Scream'), Enemy('Doberman', HEALTH_ENEMY, 'Evil face')],
+    [
+        Enemy('Chiheahua', HEALTH_ENEMY, Weapon('Scream', DAMAGE)),
+        Enemy('Doberman', HEALTH_ENEMY, Weapon('Evil face', DAMAGE)),
+    ],
 )
 
 
@@ -215,17 +228,21 @@ def test_remove_weapon():
     """
     game = Game(*TEST_REMOVE_WEAPON)
     old_weapon = Weapon('Pan', DAMAGE)
+    game += old_weapon
     game -= old_weapon
     assert old_weapon not in game.weapons
 
 
 TEST_ADD_ENEMY = (
     [
-        Player('Fedya', HEALTH_PLAYER, 'Katana', LEVEL),
-        Player('Matvey', HEALTH_PLAYER, 'Laptop', LEVEL),
+        Player('Fedya', HEALTH_PLAYER, Weapon('Katana', DAMAGE), LEVEL),
+        Player('Matvey', HEALTH_PLAYER, Weapon('Laptop', DAMAGE), LEVEL),
     ],
     [Weapon('Bottle', DAMAGE), Weapon('Laptop', DAMAGE)],
-    [Enemy('Schreck', HEALTH_ENEMY, 'Power'), Enemy('Skibidy', HEALTH_ENEMY, 'Song')],
+    [
+        Enemy('Schreck', HEALTH_ENEMY, Weapon('Power', DAMAGE)),
+        Enemy('Skibidy', HEALTH_ENEMY, Weapon('Song', DAMAGE)),
+    ],
 )
 
 
@@ -237,18 +254,21 @@ def test_add_enemy():
         True if new_enemy add.
     """
     game = Game(*TEST_ADD_ENEMY)
-    new_enemy = Enemy('Black man', HEALTH_ENEMY, 'Black magic')
+    new_enemy = Enemy('Black man', HEALTH_ENEMY, Weapon('Black magic', DAMAGE))
     game += new_enemy
     assert new_enemy in game.enemies
 
 
 TEST_REMOVE_ENEMY = (
     [
-        Player('Fedya', HEALTH_PLAYER, 'Katana', LEVEL),
-        Player('Matvey', HEALTH_PLAYER, 'Laptop', LEVEL),
+        Player('Fedya', HEALTH_PLAYER, Weapon('Katana', DAMAGE), LEVEL),
+        Player('Matvey', HEALTH_PLAYER, Weapon('Laptop', DAMAGE), LEVEL),
     ],
     [Weapon('Potato', DAMAGE), Weapon('Cucumber', DAMAGE)],
-    [Enemy('Skibidy', HEALTH_ENEMY, 'Song'), Enemy('Schreck', HEALTH_ENEMY, 'Power')],
+    [
+        Enemy('Skibidy', HEALTH_ENEMY, Weapon('Song', DAMAGE)),
+        Enemy('Schreck', HEALTH_ENEMY, Weapon('Power', DAMAGE)),
+    ],
 )
 
 
@@ -260,6 +280,7 @@ def test_remove_enemy():
         True if old_enemy remove.
     """
     game = Game(*TEST_REMOVE_ENEMY)
-    old_enemy = Enemy('Schreck', HEALTH_ENEMY, 'Power')
+    old_enemy = Enemy('Schreck', HEALTH_ENEMY, Weapon('Power', DAMAGE))
+    game += old_enemy
     game -= old_enemy
     assert old_enemy not in game.enemies
