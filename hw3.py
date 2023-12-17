1ea386727c4b0052616004a74745de7b4cb36425
BBEDITE-IMUA <artem.s.krivenko@yandex.ru>
2023-12-17 15:33:25 +0300

    Error correction_4

--- hw3.py
+++ hw3.py
@@ -1,142 +1,259 @@
-from typing import Any
+"""
+This module contains classes for managing a restaurant.
+"""
+
+from typing import Any, List
 
 
-def check_type(value: Any, type_: Any) -> bool:
-    """Check type of value."""
+def check_value_type(value: Any, type_: Any) -> bool:
+    """
+    Check type of value.
+
+    :param value: value to check
+    :param type_: expected type
+    :raises ValueError: if value is not of expected type
+    """
     if not isinstance(value, type_):
         raise ValueError(f'Value must be {type_}')
+    return True
 
 
 class Product:
+    """
+    Class representing a product.
+    """
+
     def __init__(self, name: str, price: float):
-        """Create product."""
+        """
+        Create product.
+
+        :param name: name of the product
+        :param price: price of the product
+        """
         self.name = name
         self.price = price
 
     @property
     def price(self) -> float:
-        """Return price."""
+        """
+        Return price.
+
+        :return: price of the product
+        """
         return self._price
 
     @price.setter
-    def price(self, value):
-        """Set price."""
-        check_type(value, float)
-        if value < 0:
+    def price(self, price_value: float):
+        """
+        Set price.
+
+        :param price_value: new price
+        :raises ValueError: if price is not positive
+        """
+        check_value_type(price_value, float)
+        if price_value < 0:
             raise ValueError('Price must be positive')
-        self._price = value
+        self._price = price_value
 
     @property
     def name(self) -> str:
-        """Return name."""
+        """
+        Return name.
+
+        :return: name of the product
+        """
         return self._name
 
     @name.setter
-    def name(self, value):
-        """Set name."""
-        check_type(value, str)
-        self._name = value
+    def name(self, name_value: str):
+        """
+        Set name.
+
+        :param name_value: new name
+        """
+        check_value_type(name_value, str)
+        self._name = name_value
 
 
 class Dish:
-    def __init__(self, name: str, ingredients: list[Product]):
-        """Create dish."""
+    """
+    Class representing a dish.
+    """
+
+    def __init__(self, name: str, ingredients: List[Product]):
+        """
+        Create dish.
+
+        :param name: name of the dish
+        :param ingredients: list of ingredients
+        """
         self.name = name
         self.ingredients = ingredients
 
     @property
     def name(self) -> str:
-        """Return name."""
+        """
+        Return name.
+
+        :return: name of the dish
+        """
         return self._name
 
     @name.setter
-    def name(self, value):
-        """Set name."""
-        check_type(value, str)
-        self._name = value
+    def name(self, name_value: str):
+        """
+        Set name.
+
+        :param name_value: new name
+        """
+        check_value_type(name_value, str)
+        self._name = name_value
 
     @property
-    def ingredients(self) -> list[Product]:
-        """Return ingredients."""
+    def ingredients(self) -> List[Product]:
+        """
+        Return ingredients.
+
+        :return: list of ingredients
+        """
         return self._ingredients
 
     @ingredients.setter
-    def ingredients(self, value):
-        """Set ingredients."""
-        check_type(value, list)
-        for item in value:
-            check_type(item, Product)
-        self._ingredients = value
+    def ingredients(self, ingredients_value: List[Product]):
+        """
+        Set ingredients.
+
+        :param ingredients_value: new list of ingredients
+        """
+        check_value_type(ingredients_value, list)
+        for ingredient in ingredients_value:
+            check_value_type(ingredient, Product)
+        self._ingredients = ingredients_value
 
     def add_ingredient(self, product: Product):
-        """Add ingredient."""
-        check_type(product, Product)
+        """
+        Add ingredient.
+
+        :param product: product to add
+        """
+        check_value_type(product, Product)
         self._ingredients.append(product)
 
     def remove_ingredient(self, product: Product):
-        """Delete ingredient."""
-        check_type(product, Product)
+        """
+        Delete ingredient.
+
+        :param product: product to remove
+        """
+        check_value_type(product, Product)
         self._ingredients.remove(product)
 
 
 class Restaurant:
-    def __init__(self, name: str, dishes: list[Dish], products: list[Product]):
-        """Create restaurant."""
+    """
+    Class representing a restaurant.
+    """
+
+    def __init__(self, name: str, dishes: List[Dish], products: List[Product]):
+        """
+        Create restaurant.
+
+        :param name: name of the restaurant
+        :param dishes: list of dishes
+        :param products: list of products
+        """
         self.name = name
         self.dishes = dishes
         self.products = products
 
     @property
     def name(self) -> str:
-        """Return name."""
+        """
+        Return name.
+
+        :return: name of the restaurant
+        """
         return self._name
 
     @name.setter
-    def name(self, value):
-        """Set name."""
-        check_type(value, str)
-        self._name = value
+    def name(self, name_value: str):
+        """
+        Set name.
+
+        :param name_value: new name
+        """
+        check_value_type(name_value, str)
+        self._name = name_value
 
     @property
-    def dishes(self) -> list[Dish]:
-        """Return dishes."""
+    def dishes(self) -> List[Dish]:
+        """
+        Return dishes.
+
+        :return: list of dishes
+        """
         return self._dishes
 
     @dishes.setter
-    def dishes(self, value):
-        """Set dishes."""
-        check_type(value, list)
-        for item in value:
-            check_type(item, Dish)
-        self._dishes = value
+    def dishes(self, dishes_value: List[Dish]):
+        """
+        Set dishes.
+
+        :param dishes_value: new list of dishes
+        """
+        check_value_type(dishes_value, list)
+        for dish in dishes_value:
+            check_value_type(dish, Dish)
+        self._dishes = dishes_value
 
     @property
-    def products(self) -> list[Product]:
-        """Return products."""
+    def products(self) -> List[Product]:
+        """
+        Return products.
+
+        :return: list of products
+        """
         return self._products
 
     @products.setter
-    def products(self, value):
-        """Set products."""
-        check_type(value, list)
-        for item in value:
-            check_type(item, Product)
-        self._products = value
+    def products(self, products_value: List[Product]):
+        """
+        Set products.
+
+        :param products_value: new list of products
+        """
+        check_value_type(products_value, list)
+        for product in products_value:
+            check_value_type(product, Product)
+        self._products = products_value
 
     def add_dish(self, dish: Dish):
-        """Add dish."""
-        check_type(dish, Dish)
+        """
+        Add dish.
+
+        :param dish: dish to add
+        """
+        check_value_type(dish, Dish)
         self._dishes.append(dish)
 
     def remove_dish(self, dish: Dish):
-        """Delete dish."""
-        check_type(dish, Dish)
+        """
+        Delete dish.
+
+        :param dish: dish to remove
+        """
+        check_value_type(dish, Dish)
         self._dishes.remove(dish)
 
     def order_dish(self, dish: Dish) -> bool:
-        """Order dish."""
-        check_type(dish, Dish)
+        """
+        Order dish.
+
+        :param dish: dish to order
+        :return: True if order is successful, False otherwise
+        """
+        check_value_type(dish, Dish)
         for ingredient in dish.ingredients:
             if ingredient not in self._products:
-                print(f'Not enough {ingredient.name}')
-        print(f'Order {dish.name}')
+                return False
+        return True
