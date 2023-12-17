bb4ca56176cd3c7654c136cf3107390278e9e7f7
BBEDITE-IMUA <artem.s.krivenko@yandex.ru>
2023-12-17 18:05:27 +0300

    Error correction_13

--- test_hw3.py
+++ test_hw3.py
@@ -1,11 +1,15 @@
+"""test module for hw3.py."""
 import pytest
+
 from hw3 import Product, Dish, Restaurant
 
+MIDDLE_PRICE = 5.0
+LOW_PRICE = 2.0
 def test_product():
     """Test Product class."""
-    product = Product('Burger', 5.0)
+    product = Product('Burger', MIDDLE_PRICE)
     assert product.name == 'Burger'
-    assert product.price == 5.0
+    assert product.price == MIDDLE_PRICE
     with pytest.raises(ValueError):
         product.price = -1.0
     with pytest.raises(ValueError):
@@ -13,21 +17,21 @@ def test_product():
 
 def test_dish():
     """Test Dish class."""
-    product1 = Product('Burger', 5.0)
-    product2 = Product('Fries', 2.0)
-    dish = Dish('Burger and Fries', [product1, product2])
-    assert dish.name == 'Burger and Fries'
+    product1 = Product('Cotlet', MIDDLE_PRICE)
+    product2 = Product('Fries', LOW_PRICE)
+    dish = Dish('Cotlet and Fries', [product1, product2])
+    assert dish.name == 'Cotlet and Fries'
     assert dish.ingredients == [product1, product2]
     with pytest.raises(ValueError):
         dish.name = 123
     with pytest.raises(ValueError):
-        dish.ingredients = ['Burger', 'Fries']
+        dish.ingredients = ['Cotlet', 'Fries']
 
 def test_restaurant():
     """Test Restaurant class."""
-    product1 = Product('Burger', 5.0)
-    product2 = Product('Fries', 2.0)
-    dish = Dish('Burger and Fries', [product1, product2])
+    product1 = Product('Salad', MIDDLE_PRICE)
+    product2 = Product('Eggs', LOW_PRICE)
+    dish = Dish('Salad and Eggs', [product1, product2])
     restaurant = Restaurant('Fast Food', [dish], [product1, product2])
     assert restaurant.name == 'Fast Food'
     assert restaurant.dishes == [dish]
@@ -35,9 +39,9 @@ def test_restaurant():
     with pytest.raises(ValueError):
         restaurant.name = 123
     with pytest.raises(ValueError):
-        restaurant.dishes = ['Burger and Fries']
+        restaurant.dishes = ['Salad and Eggs']
     with pytest.raises(ValueError):
-        restaurant.products = ['Burger', 'Fries']
+        restaurant.products = ['Salad', 'Eggs']
     assert restaurant.order_dish(dish) is not None
     restaurant.remove_dish(dish)
     assert restaurant.order_dish(dish) is False
