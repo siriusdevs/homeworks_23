121e6c74d25683d937f1628ceb8504294cbcf261
Sam Komarov <sam@skomarov.com>
2023-12-18 22:51:30 +0300

    implement Aviacompany getters and setters

---
+++ hw3/aviacompany.py
@@ -0,0 +1,107 @@
+"""Module that provides the Aviacompany class."""
+
+from . import hw3
+from .flight import Flight
+from .passenger import Passenger
+from .ticket import Ticket
+
+
+class Aviacompany:  # noqa: WPS214
+    """Represents an aviacompany that aggregates its flights, passengers and tickets."""
+
+    def __init__(
+        self,
+        name: str,
+        flights: list[Flight],
+        passengers: list[Passenger],
+        tickets: list[Ticket],
+    ) -> None:
+        """Initialize an aviacompany.
+
+        Args:
+            name: full name of the aviacompany, unrestricted
+            flights: list of flights that this aviacompany has
+            passengers: list of passengers that are served by this aviacompany
+            tickets: list of tickets that were bought
+        """
+        self.name = name
+        self.flights = flights
+        self.passengers = passengers
+        self.tickets = tickets
+
+    @property
+    def name(self) -> str:
+        """Getter for name.
+
+        Returns:
+            str: name
+        """
+        return self._name
+
+    @name.setter
+    def name(self, name: str) -> None:
+        """Setter for name.
+
+        Args:
+            name: new name
+        """
+        hw3.check_type(name, str)
+        self._name = name
+
+    @property
+    def flights(self) -> list[Flight]:
+        """Getter for flights.
+
+        Returns:
+            List[Flight]: list of flights this aviacompany currently has
+        """
+        return self._flights
+
+    @flights.setter
+    def flights(self, flights: list[Flight]) -> None:
+        """Setter for flights.
+
+        Args:
+            flights: new list of all flights this aviacompany has
+        """
+        hw3.check_type(flights, list)
+        hw3.check_elements_types(flights, Flight)
+        self._flights = flights
+
+    @property
+    def passengers(self) -> list[Passenger]:
+        """Getter for passengers.
+
+        Returns:
+            list[Passenger]: list of passengers this aviacompany serves
+        """
+        return self._passengers
+
+    @passengers.setter
+    def passengers(self, passengers: list[Passenger]) -> None:
+        """Setter for passengers.
+
+        Args:
+            passengers: new list of passengers this aviacompany serves
+        """
+        hw3.check_type(passengers, list)
+        hw3.check_elements_types(passengers, Passenger)
+        self._passengers = passengers
+
+    @property
+    def tickets(self) -> list[Ticket]:
+        """Getter for tickets.
+
+        Returns:
+            list[Ticket]: list of tickets that were bought from this company
+        """
+        return self._tickets
+
+    @tickets.setter
+    def tickets(self, tickets: list[Ticket]) -> None:
+        """Setter for tickets.
+
+        Args:
+            tickets: new list of tickets
+        """
+        self._tickets = tickets
---
+++ hw3/tests/test_aviacompany.py
@@ -0,0 +1,63 @@
+"""Module with tests for hw3.aviacompany."""
+
+import pytest
+
+from ..aviacompany import Aviacompany
+from ..flight import Flight
+from ..passenger import Passenger
+from ..ticket import Ticket
+
+_T_FLIGHTS = (Flight('1', 'A', 'B'), Flight('2', 'C', 'D'))
+_T_PASSENGERS = (Passenger('Alice', '123456'), Passenger('Bob', '555555'))
+_T_TICKETS = (
+    Ticket('123', _T_PASSENGERS[0], _T_FLIGHTS[0]),
+    Ticket('321', _T_PASSENGERS[1], _T_FLIGHTS[1]),
+)
+
+
+def test_aviacompany_getters_happy():
+    """Creates an aviacompany and asserts on its getters."""
+    sut = Aviacompany('Aeroflot', list(_T_FLIGHTS), list(_T_PASSENGERS), list(_T_TICKETS))
+    assert sut.name == 'Aeroflot'
+    assert sut.flights == list(_T_FLIGHTS)
+    assert sut.passengers == list(_T_PASSENGERS)
+    assert sut.tickets == list(_T_TICKETS)
+
+
+def test_aviacompany_setters_happy():
+    """Uses aviacompany's setters and asserts they truly modify properties."""
+    sut = Aviacompany('Aeroflot', list(_T_FLIGHTS), list(_T_PASSENGERS), list(_T_TICKETS))
+    sut.name = 'S7 Airlines'
+    sut.flights = list(reversed(_T_FLIGHTS))
+    sut.passengers = list(reversed(_T_PASSENGERS))
+    sut.tickets = list(reversed(_T_TICKETS))
+    assert sut.name == 'S7 Airlines'
+    assert sut.flights == list(reversed(_T_FLIGHTS))
+    assert sut.passengers == list(reversed(_T_PASSENGERS))
+    assert sut.tickets == list(reversed(_T_TICKETS))
+
+
+def test_aviacompany_setters_errors():
+    """Asserts that aviacompany constructors raise errors on inappriate types."""
+    sut = Aviacompany('Fly Emirates', list(_T_FLIGHTS), list(_T_PASSENGERS), list(_T_TICKETS))
+
+    with pytest.raises(TypeError):
+        sut.name = 1234
+    with pytest.raises(TypeError):
+        sut.flights = 'hello'
+    with pytest.raises(TypeError):
+        sut.passengers = 42.0
+    with pytest.raises(TypeError):
+        sut.tickets = Passenger()
+
+
+def test_aviacompany_list_setters_errors():
+    """Asserts that aviacompany constructors raise errors on inappriate types inside lists."""
+    sut = Aviacompany('Fly Emirates', list(_T_FLIGHTS), list(_T_PASSENGERS), list(_T_TICKETS))
+
+    with pytest.raises(TypeError):
+        sut.flights = list(_T_FLIGHTS[:1]) + [55]
+    with pytest.raises(TypeError):
+        sut.passengers = ['hello'] + list(_T_PASSENGERS[:1])
+    with pytest.raises(TypeError):
+        sut.tickets = [Flight()] + list(_T_TICKETS[:1])
