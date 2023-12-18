0267c8106e0a99b0f8e5d17fe8de69637278cf98
Sam Komarov <sam@skomarov.com>
2023-12-18 18:34:53 +0300

    add type assertion helpers

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -43,3 +43,30 @@
 Купить билет
 Отменить билет
 """
+
+from typing import Any, Iterable, Type
+
+
+def check_type(got: Any, want_type: Type) -> None:
+    """Assert that got is of type want_type.
+
+    Args:
+        got: value which must have want_type
+        want_type: type that you want to assert on
+
+    Raises:
+        TypeError: when got does not satisfy want_type
+    """
+    if not isinstance(got, want_type):
+        raise TypeError(f'expected {got} to be of type {want_type}')
+
+
+def check_elements_types(got: Iterable[Any], want_elem_type: Type) -> None:
+    """Execute check_type() for each element of the provided Iterable.
+
+    Args:
+        got: Iterable, which elements will be checked
+        want_elem_type: type that all provided elements must have
+    """
+    for got_elem in got:
+        check_type(got_elem, want_elem_type)
