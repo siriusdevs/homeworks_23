21986c7aefdbea4fe6b2a7c35b303c7b73deacc0
anonim767bn <anonim2462@yandex.ru>
2023-12-19 16:24:15 +0300

    make exclude - set

--- hw1.py
+++ hw1.py
@@ -17,11 +17,11 @@ def salary_stat(
     Returns:
         tuple[list[float], float]: top 3 salaries and their sum percentage.
     """
-    if exclude is None:
-        exclude = ()
+    if exclude is not None:
+        exclude = set(exclude)
     salaries = []
     for department, department_salaries in args:
-        if department not in exclude:
+        if exclude and department not in exclude:
             for salary in department_salaries:
                 check_type(salary, float, check_positive=True)
             salaries.extend(department_salaries)
