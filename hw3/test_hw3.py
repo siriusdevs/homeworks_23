c8f45f835bed5e5c6fae4739015e6e80d149adea
MomusWinner <areg06.06@mail.ru>
2023-12-21 02:32:13 +0300

    fix linter

--- hw3/test_hw3.py
+++ hw3/test_hw3.py
@@ -1,7 +1,12 @@
-import pytest
-from .hw3 import Product, Dish, Restaurant
+"""Test for hw3 modul."""
 
-NUMBERS = [1, 2, 3]
+import pytest
+
+from .hw3 import Dish, Product, Restaurant
+
+RAISES_TYPE_ERROR = pytest.raises(TypeError)
+RAISES_TYPE_VALUE = pytest.raises(ValueError)
+
 
 def _get_banana(): return Product('banana', 13)
 
@@ -12,68 +17,88 @@ def _get_apple(): return Product('apple', 10)
 def _get_cereal(): return Product('cereal', 5)
 
 
-def _get_honey(): return Product('honey', 30)
+def test_dish_property_error() -> None:
+    """Test dish property for an error call."""
+    numbers = [1, 2, 3]
+    porridge = Dish('porrige', [])
+    with RAISES_TYPE_ERROR:
+        porridge.products = _get_apple()
+    with RAISES_TYPE_ERROR:
+        porridge.products = numbers
+    with RAISES_TYPE_ERROR:
+        porridge.name = numbers
 
 
-def test_dish_property():
-    apple, banana, cereal, honey = _get_apple(), _get_banana(), _get_cereal(), _get_honey()
+def test_dish_method() -> None:
+    """Test add_product, remove_product for correct working."""
+    apple, banana, cereal = _get_apple(), _get_banana(), _get_cereal()
     porridge = Dish('porridge', [apple, banana, cereal])
     porridge.remove_product(apple)
+    honey = Product('honey', 30)
     porridge.add_product(honey)
     assert porridge.products == [banana, cereal, honey]
 
 
-def test_dish_error():
+def test_dish_method_error() -> None:
+    """Test dish add_product and remove_product method for an error call."""
     porridge = Dish('porrige', [])
-    with pytest.raises(TypeError):
-        return porridge.add_product('')
-    with pytest.raises(ValueError):
-        return porridge.remove_product(_get_apple())
-    with pytest.raises(TypeError):
-        return porridge.remove_product('')
-    with pytest.raises(TypeError):
-        porridge.name = 54321
-    with pytest.raises(TypeError):
-        porridge.products = _get_apple()
-    with pytest.raises(TypeError):
-        porridge.products = NUMBERS
+    with RAISES_TYPE_ERROR:
+        porridge.add_product('')
+    with RAISES_TYPE_VALUE:
+        porridge.remove_product(_get_apple())
+    with RAISES_TYPE_ERROR:
+        porridge.remove_product('')
 
 
-def test_restorant_property():
+def test_restorant_property_error() -> None:
+    """Test restorant property for an error call."""
+    apple_juice = Dish('apple juice', [_get_apple()])
+    restaurant = Restaurant('', [], [])
+    numbers = [1, 2, 3]
+    with RAISES_TYPE_ERROR:
+        restaurant.dishes = apple_juice
+    with RAISES_TYPE_ERROR:
+        restaurant.dishes = numbers
+    with RAISES_TYPE_ERROR:
+        restaurant.products = numbers
+    with RAISES_TYPE_ERROR:
+        restaurant.name = 5
+
+
+def test_restorant_methods_error() -> None:
+    """Test dish add_product and remove_product method for an error call."""
+    restaurant = Restaurant('', [], [])
+    with RAISES_TYPE_ERROR:
+        restaurant.add_dish('')
+    with RAISES_TYPE_ERROR:
+        restaurant.remove_dish(5)
+    with RAISES_TYPE_ERROR:
+        restaurant.add_product('')
+    with RAISES_TYPE_ERROR:
+        restaurant.remove_product('')
+
+
+def test_restorant_methods() -> None:
+    """Test add_product, add_dish, remove_product, remove_dish for correct working."""
     apple, banana, cereal = _get_apple(), _get_banana(), _get_cereal()
     apple_juice = Dish('apple juice', [apple])
     porridge = Dish('porridge', [cereal, banana])
-    restaurant = Restaurant('noname', [apple_juice], [apple])
+    restaurant = Restaurant('', [apple_juice], [apple])
     restaurant.add_product(banana)
     restaurant.add_dish(porridge)
-    restaurant.remove_dish(apple)
+    restaurant.remove_dish(apple_juice)
     restaurant.remove_product(apple)
-    restaurant.name 
-    assert
+    assert restaurant.products == [banana]
+    assert restaurant.dishes == [porridge]
 
 
-def test_restorant_error():
-    apple_juice = Dish('apple juice', [_get_apple()])
-    restaurant = Restaurant('noname', [], [])
-    with pytest.raises(TypeError):
-        restaurant.dishes = apple_juice
-    with pytest.raises(TypeError):
-        restaurant.dishes = NUMBERS
-    with pytest.raises(TypeError):
-        return restaurant.add_product('')
-    with pytest.raises(ValueError):
-        return restaurant.remove_product(_get_apple())
-    with pytest.raises(TypeError):
-        return restaurant.remove_product('')
-    with pytest.raises(TypeError):
-        restaurant.products = NUMBERS
-
-def test_order_dish():
+def test_order_dish() -> None:
+    """Test order_dish method."""
     banana, apple = _get_banana(), _get_apple()
     banana_porridge = Dish('banan_porridge', [banana])
-    restaurant = Restaurant('noname', [banana_porridge], [banana])
+    restaurant = Restaurant('', [banana_porridge], [banana])
     restaurant.add_product(apple)
-    result = restaurant.order_dish(banana_porridge.name)
-    assert result == banana_porridge
+    order_dish = restaurant.order_dish(banana_porridge.name)
+    assert order_dish == banana_porridge
     assert banana_porridge not in restaurant.dishes
     assert apple not in restaurant.dishes
