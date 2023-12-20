753f85c98e2305f3feb04d796c09782ab25e0c47
BBEDITE-IMUA <artem.s.krivenko@yandex.ru>
2023-12-20 20:49:06 +0300

    Error corection_4

--- test_hw1.py
+++ test_hw1.py
@@ -1,21 +1,21 @@
 """Test hw1.py module."""
 
-import pytest
-
 from hw1 import salary_stats
+
+LIMIT = 2000
+LIMIT2 = 6000
 
 
 def test_salary_stats():
     """Test salary_stats function."""
+    test1 = salary_stats(('Dept1', [1000, 2000, 3000, 4000, 5000]), limit=LIMIT)
+    assert test1 == ([2000, 3000, 4000], 64.29)
 
-    result = salary_stats(("Dept1", [1000, 2000, 3000, 4000, 5000]), limit=2000)
-    assert result == ([2000, 3000, 4000], 64.29)
+    test2 = salary_stats(('Dept2', [1000, 2000, 3000, 4000, 5000]), limit=LIMIT2)
+    assert test2 == ([], 0)
 
-    result = salary_stats(("Dept1", [1000, 2000, 3000, 4000, 5000]), limit=6000)
-    assert result == ([], 0)
+    test3 = salary_stats(('Dept3', [1000, 2000, 3000, 4000, 5000]))
+    assert test3 == ([1000, 2000, 3000], 40.0)
 
-    result = salary_stats(("Dept1", [1000, 2000, 3000, 4000, 5000]))
-    assert result == ([1000, 2000, 3000], 40.0)
-
-    result = salary_stats(("Dept1", [0, 0, 0, 0, 0]))
-    assert result == ([0, 0, 0], 0)
+    test4 = salary_stats(('Dept4', [0, 0, 0, 0, 0]))
+    assert test4 == ([0, 0, 0], 0)
