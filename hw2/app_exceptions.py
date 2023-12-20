5e574ce33a0c6a2bd02e15fa9417e685fbd042b9
developer-mixa <mixail_081177@mail.ru>
2023-12-20 10:10:28 +0300

    linter fix

--- hw2/app_exceptions.py
+++ hw2/app_exceptions.py
@@ -5,5 +5,5 @@ class NotFoundAgeException(Exception):
     """Exception for age if it is not found."""
 
     def __init__(self) -> None:
-        """Init an error that is called when the age parameter is not found."""
+        """Init an error that is called when the age parametr is not found."""
         super().__init__('The parametr <age> is not found!')
