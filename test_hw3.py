2a3ee211a663d38052e743e761c49c7a421d837d
fghh64 <fghh64.46hhgf@yandex.ru>
2023-12-20 20:19:52 +0300

    test 7 hw3

--- test_hw3.py
+++ test_hw3.py
@@ -4,27 +4,32 @@ import pytest
 
 from hw3 import Car, Owner, CarShowroom, Sale
 
+YEAR = 2020
+
 
 def test_car_creation():
-    car = Car('Toyota', 'Camry', 2020)
+    car = Car('Toyota', 'Camry', YEAR)
     assert car.brand == 'Toyota'
     assert car.model == 'Camry'
     assert car.year == 2020
 
+
 def test_owner_creation():
-    car = Car('Toyota', 'Camry', 2020)
+    car = Car('Toyota', 'Camry', YEAR)
     owner = Owner('John Doe', [car])
     assert owner.name == 'John Doe'
     assert owner.car_park == [car]
 
+
 def test_car_showroom_creation():
-    car = Car('Toyota', 'Camry', 2020)
+    car = Car('Toyota', 'Camry', YEAR)
     showroom = CarShowroom('Showroom 1', [car])
     assert showroom.name == 'Showroom 1'
     assert showroom.cars == [car]
 
+
 def test_sale_creation():
-    car = Car('Toyota', 'Camry', 2020)
+    car = Car('Toyota', 'Camry', YEAR)
     owner = Owner('John Doe', [])
     showroom = CarShowroom('Showroom 1', [car])
     sale = Sale(car, owner, showroom)
@@ -32,8 +37,9 @@ def test_sale_creation():
     assert sale.owner == owner
     assert sale.car_showroom == showroom
 
+
 def test_sell_car():
-    car = Car('Toyota', 'Camry', 2020)
+    car = Car('Toyota', 'Camry', YEAR)
     owner = Owner('John Doe', [])
     showroom = CarShowroom('Showroom 1', [car])
     sale = Sale(car, owner, showroom)
@@ -41,8 +47,9 @@ def test_sell_car():
     assert car not in showroom.cars
     assert car in owner.car_park
 
+
 def test_sell_car_not_in_showroom():
-    car = Car('Toyota', 'Camry', 2020)
+    car = Car('Toyota', 'Camry', YEAR)
     owner = Owner('John Doe', [])
     showroom = CarShowroom('Showroom 1', [])
     sale = Sale(car, owner, showroom)
