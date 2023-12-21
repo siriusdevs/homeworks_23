4344098acc369bc7fed569025ff901a92fe66c42
Artemka <Artyom08112006@gmail.com>
2023-12-21 18:01:01 +0300

    fixed error with tests, added folder cleaning after tests

--- hw2/test_hw2.py
+++ hw2/test_hw2.py
@@ -4,6 +4,20 @@ import os
 from testcases import positive_test_data, negative_test_data
 from test_answers import test_answers
 from hw2 import analyze_json
+import shutil
+
+POSITIVE_TEST_ID = (
+     (1, True),
+     (2, True),
+     (3, True),
+)
+
+NEGATIVE_TEST_ID = (
+     (4, True),
+     (5, True),
+     (6, True),
+     (7, True)
+)
 
 def create_file(subfolder: str, filename: str) -> str:
     file_path = os.path.join(subfolder, filename)
@@ -22,10 +36,10 @@ def prepare_data(test_id: int, positive: bool = True) -> tuple[str, str]:
             test_data = negative_test_data[test_id]
 
         to_json = test_data
-        path_to_json = create_file('./hw2/tests/', f'test_{test_id}.json')
+        path_to_json = create_file('./tests/', f'test_{test_id}.json')
         with open(path_to_json, 'w') as json_to_write:
             json_to_write.write(json.dumps(to_json))
-        path_to_result = create_file('./hw2/tests/res/', f'res_{test_id}.json')
+        path_to_result = create_file('./tests/res/', f'res_{test_id}.json')
         return path_to_json, path_to_result
 
 def get_result(path_to_result) -> str:
@@ -42,9 +56,21 @@ def positive_tests(test_id: int) -> bool:
 
 def negative_tests(test_id: int) -> bool:
     path_to_json, path_to_result = prepare_data(test_id, positive=False)
-    analyze_json(path_to_json, path_to_result)
-    is_correct = (get_result(path_to_result) == test_answers[test_id])
-    
+    try:
+        analyze_json(path_to_json, path_to_result)
+    except SystemExit:
+        is_correct = get_result(path_to_result) == test_answers[test_id]
+
     return is_correct
 
-print(negative_tests(5))    
+
+@pytest.mark.parametrize('test_id, expected', POSITIVE_TEST_ID)
+def test_positive_hw2(test_id: int, expected: bool) -> None:
+    assert positive_tests(test_id=test_id) == expected
+    shutil.rmtree('./tests')
+
+
+@pytest.mark.parametrize('test_id, expected', NEGATIVE_TEST_ID)
+def test_negative_hw2(test_id: int, expected: bool) -> None:
+    assert negative_tests(test_id=test_id) == expected
+    shutil.rmtree('./tests')
