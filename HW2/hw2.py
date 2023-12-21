51f68d3254335ab06183cab45f406fe1db4f057e
BBEDITE-IMUA <artem.s.krivenko@yandex.ru>
2023-12-22 00:55:12 +0300

    Error corection_10

--- HW2/hw2.py
+++ HW2/hw2.py
@@ -1,25 +1,37 @@
 """Contains functions for processing data from input file and saving results to output file."""
 
 import json
-from collections import defaultdict
 from datetime import datetime
 
 
-def process_data(input_file: str, output_file: str):
+def process_data(input_file: str, output_file: str) -> str:
     """
     Process data from input_file and save results to output_file.
 
     Args:
         input_file (str): Path to the input file.
         output_file (str): Path to the output file.
+
+    Returns:
+        str: A status message indicating the result of the data processing.
     """
-    with open(input_file, 'r') as input_filee:
-        dataa = json.load(input_filee)
+    try:
+        with open(input_file, 'r') as input_filee:
+            dataa = json.load(input_filee)
+    except FileNotFoundError:
+        return f'{input_file} not found'
+    except json.decoder.JSONDecodeError:
+        return f'{input_file} has not json'
 
     output_data = get_stat(dataa)
 
-    with open(output_file, 'w') as output_filee:
-        json.dump(output_data, output_filee)
+    try:
+        with open(output_file, 'w') as output_filee:
+            json.dump(output_data, output_filee)
+    except FileNotFoundError:
+        return f'invalid file path {output_file}'
+    except PermissionError:
+        return f'not permission to write to file {output_file}'
 
 
 def get_stat(dataa: dict) -> dict:
@@ -32,12 +44,14 @@ def get_stat(dataa: dict) -> dict:
     Returns:
         dict: Dict with stat.
     """
-    city_distrib = defaultdict(int)
-    year_distrib = defaultdict(int)
+    city_distrib = {}
+    year_distrib = {}
 
-    for user in dataa.values():
-        city_distrib[user['region']] += 1
-        year_distrib[datetime.strptime(user['registered'], '%Y-%m-%d').year] += 1
+    for user in dataa.keys():
+        city = dataa[user]['region']
+        city_distrib[city] = city_distrib.get(city, 0) + 1
+        year_of_regisrt = datetime.strptime(dataa[user]['registered'], '%Y-%m-%d').year
+        year_distrib[year_of_regisrt] = year_distrib.get(year_of_regisrt, 0) + 1
     total_users = len(dataa.keys())
     city_distrib = {city: count / total_users * 100 for city, count in city_distrib.items()}
     year_distrib = {year: count / total_users * 100 for year, count in year_distrib.items()}
--- HW2/test_hw2.py
+++ HW2/test_hw2.py
@@ -8,24 +8,22 @@ from hw2 import process_data
 
 def test_process_data():
     """Test process_data function."""
-    test_data = {
-        'user1': {'region': 'Moscow', 'registered': '2020-01-01'},
-        'user2': {'region': 'Moscow', 'registered': '2021-01-01'},
-        'user3': {'region': 'Saint Petersburg', 'registered': '2020-01-01'},
-    }
+    process_data('./HW2/data_test.json', './data_test_output.json')
 
-    with open('test_input.json', 'w') as input_file:
-        json.dump(test_data, input_file)
-
-    process_data('test_input.json', 'test_output.json')
-
-    with open('test_output.json', 'r') as output_file:
+    with open('./data_test_output.json', 'r') as output_file:
         output_data = json.load(output_file)
 
-    assert output_data['city_distribution'] == {'Moscow': 2/3*100, 'Saint Petersburg': 1/3*100}
-    assert output_data['year_distribution'] == {'2020': 2/3*100, '2021': 1/3*100}
-    os.remove('test_input.json')
-    os.remove('test_output.json')
+    assert output_data == {
+        'city_distribution': {
+            'Moscow': 2/3*100,
+            'Saint Petersburg': 1/3*100,
+        },
+        'year_distribution': {
+            '2020': 2/3*100,
+            '2021': 1/3*100,
+        },
+    }
+    os.remove('./data_test_output.json')
 
 
 def test_data():
