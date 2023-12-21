cab72e0ed636d353f237b75101e1522379c0e98a
anonim767bn <anonim2462@yandex.ru>
2023-12-21 15:25:39 +0300

    tests wf3

--- HW2/test_hw2.py
+++ HW2/test_hw2.py
@@ -9,14 +9,14 @@ def test_file_not_found():
 
 def test_file_not_json():
     """Test for json.decoder.JSONDecodeError."""
-    assert process_data('./input/decoderError.json', 'test_output.json') == 2
+    assert process_data('./HW2/input/decoderError.json', 'test_output.json') == 2
 
 
 def test_output_file_not_found():
     """Test for FileNotFoundError."""
-    assert process_data('./input/data_hw2.json', '/nesuchestvuyushaya_papka/1/1/1/1/1/1.json') == 3
+    assert process_data('./HW2/input/data_hw2.json', '/nesuchestvuyushaya_papka/1/1/1/1/1/1.json') == 3
 
 
 def test_output_file_not_writable():
     """Test for PermissionError."""
-    assert process_data('./input/data_hw2.json', './output/non_writable.json') == 4
+    assert process_data('./HW2/input/data_hw2.json', './HW2/output/non_writable.json') == 4
