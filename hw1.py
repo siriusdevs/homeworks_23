66f781d16cfd01962ef6d18550631d3213b59e42
fghh64 <fghh64.46hhgf@yandex.ru>
2023-12-21 15:52:40 +0300

    test 1 hw1

---
+++ hw1.py
@@ -0,0 +1,31 @@
+"""Calculate the top 3 salaries and their ratio to the total salary."""
+
+
+def get_salary_stats(limit: tuple = None, **kwargs: dict[str, dict[str, float]]):
+    """
+    Calculate the top 3 salaries and their ratio to the total salary.
+
+    Args:
+        limit: The departments to consider.
+        kwargs: The department and employee salary information.
+
+    Returns:
+        The top 3 salaries and their ratio to the total salary.
+    """
+    salaries = []
+    for department, employees in kwargs.items():
+        if limit is not None:
+            if department in limit:
+                salaries.extend(employees.values())
+        else:
+            salaries.extend(employees.values())
+
+    salaries.sort()
+    top_salaries = salaries[:3]
+    total_salary = sum(salaries)
+    try:
+        top_ratio = sum(top_salaries) / total_salary * 100
+    except ZeroDivisionError:
+        top_ratio = 0
+
+    return [round(salary, 2) for salary in top_salaries], round(top_ratio, 2)
---
+++ test_hw1.py
@@ -0,0 +1,25 @@
+"""Tests for hw1.py."""
+
+from hw1 import get_salary_stats
+
+
+def test_get_salary_stats_no_limit():
+    stats = get_salary_stats(
+        IT={"Alice": 1000.0, "Bob": 2000.0, "Charlie": 3000.0},
+        HR={"Dave": 4000.0, "Eve": 5000.0, "Frank": 6000.0},
+    )
+    assert stats == ([1000.0, 2000.0, 3000.0], 30.0)
+
+
+def test_get_salary_stats_with_limit():
+    stats = get_salary_stats(
+        ("IT",),
+        IT={"Alice": 1000.0, "Bob": 2000.0, "Charlie": 3000.0},
+        HR={"Dave": 4000.0, "Eve": 5000.0, "Frank": 6000.0},
+    )
+    assert stats == ([1000.0, 2000.0, 3000.0], 100.0)
+
+
+def test_get_salary_stats_no_salaries():
+    stats = get_salary_stats()
+    assert stats == ([], 0)
