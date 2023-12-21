79329c0228c8e8a15ba9025fad1de65f5b8922ed
Artemka <Artyom08112006@gmail.com>
2023-12-21 21:02:28 +0300

    added solution for task 3

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -1,31 +1,104 @@
-import json, os, sys
-from typing import Any
+"""Module, that provide analyzing of JSON."""
+import json
+import os
+import sys
 from datetime import datetime
 from statistics import mean
+from typing import Any
+
 
 class PathError(Exception):
-    def __init__(self, path: str) -> None:
-        super().__init__(f'File with path: "{path}" is not exist')
+    """
+    Exception raised for errors in the input path.
 
-class ExtentionError(Exception):
-    def __init__(self, extention: str, expected: str) -> None:
-        super().__init__(f'File must be {expected}, but got {extention}')
+    Attributes:
+        path (str): The path that caused the error.
+    """
+
+    def __init__(self, path: str) -> None:
+        """
+        Initialize the PathError exception with a specific file path.
+
+        Args:
+            path (str): The path of the file that does not exist.
+        """
+        super().__init__(f'File with path: {path} is not exist')
+
+
+class ExtensionError(Exception):
+    """
+    Exception raised for errors in the file extension.
+
+    Attributes:
+        extension (str): The actual extension of the file.
+        expected (str): The expected extension of the file.
+    """
+
+    def __init__(self, extension: str, expected: str) -> None:
+        """
+        Initialize the ExtensionError exception with the file extension and the expected extension.
+
+        Args:
+            extension (str): The actual extension of the file.
+            expected (str): The expected extension for the file.
+        """
+        super().__init__(f'File must be {expected}, but got {extension}')
+
 
 def check_path(path_to_file: str) -> None:
+    """
+    Check if the given file path points to an existing file.
+
+    Raises:
+        PathError: If the file at the given path does not exist.
+
+    Args:
+        path_to_file (str): Path to the file to check.
+    """
     if not os.path.isfile(path_to_file):
         raise PathError(path_to_file)
-    
-def check_extention(path_to_file: str, extention: str) -> None:
-    cur_extention = os.path.splitext(path_to_file)[1]
-    if not cur_extention == extention:
-        raise ExtentionError(cur_extention, extention)
-    
-def print_error(path_to_file: str, text: str):
-    with open(path_to_file, 'w') as f:
-        f.write(text)
+
+
+def check_extension(path_to_file: str, extension: str) -> None:
+    """
+    Check if the file at the given path has the specified extension.
+
+    Raises:
+        ExtensionError: If the file's extension does not match the expected extension.
+
+    Args:
+        path_to_file (str): Path to the file to check.
+        extension (str): Expected file extension.
+    """
+    current_extension = os.path.splitext(path_to_file)[1]
+    if current_extension != extension:
+        raise ExtensionError(current_extension, extension)
+
+
+def print_error(path_to_file: str, text: str) -> None:
+    """
+    Write an error message to a file and then exit the program.
+
+    Args:
+        path_to_file (str): Path to the file where the error message will be written.
+        text (str): Error message to be written.
+    """
+    with open(path_to_file, 'w') as file_to_write:
+        file_to_write.write(text)
     sys.exit()
-    
-def open_json(path_to_file: str, output_path: str) -> dict[str:dict[str:Any]]:
+
+
+def open_json(path_to_file: str, output_path: str) -> dict:
+    """
+    Open a JSON file and return its contents as a dictionary.
+
+    Args:
+        path_to_file (str): Path to the JSON file.
+        output_path (str): Path for the output file to write errors if any.
+
+    Returns:
+        dict: Contents of the JSON file.
+    """
     try:
         check_path(path_to_file)
     except PathError:
@@ -33,18 +106,29 @@ def open_json(path_to_file: str, output_path: str) -> dict[str:dict[str:Any]]:
         print_error(output_path, text)
 
     try:
-        check_extention(path_to_file, '.json')
-    except ExtentionError:
-        text = 'The input file should have a JSON extension, but another one is given. Have a nice day!'
+        check_extension(path_to_file, '.json')
+    except ExtensionError:
+        text = 'The input file should have a JSON extension, but another one is given. \
+            Have a nice day!'
         print_error(output_path, text)
 
-    
-    with open(path_to_file) as file:
-        result_of_reading = json.load(file)
+    with open(path_to_file) as file_to_read:
+        result_of_reading = json.load(file_to_read)
 
     return result_of_reading
 
-def count_email_domain(json_file: dict[str:dict[str:Any]], output_path: str) -> dict:
+
+def count_email_domain(json_file: dict, output_path: str) -> dict:
+    """
+    Count the occurrence of each email domain in the provided JSON file.
+
+    Args:
+        json_file (dict): A dictionary representing the JSON file.
+        output_path (str): Path for the output file to write errors if any.
+
+    Returns:
+        dict: A dictionary mapping each email domain to its occurrence percentage.
+    """
     users = json_file.keys()
     amount_of_users = len(users)
     email_statistics = {}
@@ -60,30 +144,33 @@ def count_email_domain(json_file: dict[str:dict[str:Any]], output_path: str) -> 
             text = 'Mail with an invalid email has been entered in the file. Have a nice day!'
             print_error(output_path, text)
 
-        if not email in email_statistics.keys():
-            email_statistics[email] = 1
-        else:
-            email_statistics[email] += 1
-    
+        email_statistics[email] = email_statistics.get(email, 0) + 1
+
     for mail in email_statistics.keys():
         percent = round(email_statistics[mail] / amount_of_users * 100, 2)
         email_statistics[mail] = f'{percent}%'
 
     return email_statistics
 
-def count_user_age(json_file: dict[str:dict[str:Any]], output_file) -> dict:
+def get_required_variables(json_file: dict[str : dict[str:Any]]
+                           ) -> tuple[datetime, list, dict, list]:
     # Вообще, в идеале, тут правда должна быть текущая дата, но тогда тесты, увы, работают
     # только сутки (((
     # current_date = datetime.now()
     current_date = datetime.strptime('2023-12-20', '%Y-%m-%d')
     users = json_file.keys()
-    time_steps = {'Less than 2 days': 2,
-                  'Less than a week': 7,
-                  'Less than a month': 31,
-                  'Less than half a year': 186}
-    
+    time_steps = {
+        'Less than 2 days': 2,
+        'Less than a week': 7,
+        'Less than a month': 31,
+        'Less than half a year': 186,
+    }
     average_age = {time_lapse: [] for time_lapse in time_steps.keys()}
     average_age['More than half a year'] = []
+    return current_date, users, time_steps, average_age
+
+def count_user_age(json_file: dict[str : dict[str:Any]], output_file) -> dict:
+    current_date, users, time_steps, average_age = get_required_variables(json_file)
 
     for user in users:
         age = json_file[user]['age']
@@ -103,10 +190,10 @@ def count_user_age(json_file: dict[str:dict[str:Any]], output_file) -> dict:
                 average_age[time_lapse].append(age)
                 added = True
                 break
-        
+
         if not added:
             average_age['More than half a year'].append(age)
-    
+
     for time_lapse in average_age.keys():
         if average_age[time_lapse]:
             mean_age = mean(average_age[time_lapse])
@@ -115,25 +202,26 @@ def count_user_age(json_file: dict[str:dict[str:Any]], output_file) -> dict:
             average_age[time_lapse] = 'NaN'
 
     return average_age
-    
-def write_result_to_json(path_to_file: str,
-                         email_statistics: dict[str:str],
-                         average_age: dict[str:float|str],
-                        ) -> None:
 
-    to_json = {'email statistics':email_statistics,
-               'average_age':average_age}
-    
+
+def write_result_to_json(
+    path_to_file: str,
+    email_statistics: dict[str:str],
+    average_age: dict[str : float | str],
+) -> None:
+    to_json = {'email statistics': email_statistics, 'average_age': average_age}
+
     with open(path_to_file, 'w') as file_to_write:
         file_to_write.write(json.dumps(to_json))
-  
+
 
 def analyze_json(path_to_json: str, path_to_result: str) -> None:
     json = open_json(path_to_json, path_to_result)
-    write_result_to_json(path_to_result,
-                         count_email_domain(json, path_to_result),
-                         count_user_age(json, path_to_result),
-                         )
+    write_result_to_json(
+        path_to_result,
+        count_email_domain(json, path_to_result),
+        count_user_age(json, path_to_result),
+    )
 
 
 if __name__ == '__main__':
