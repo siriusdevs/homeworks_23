fff62f37459c5172fd3fbcd5bb10ceb294921621
fghh64 <fghh64.46hhgf@yandex.ru>
2023-12-21 22:17:40 +0300

    test 13 hw1

--- hw1.py
+++ hw1.py
@@ -1,8 +1,7 @@
 """Calculate the top 3 salaries and their ratio to the total salary."""
 
 
-def get_salary_stats(limit: tuple = None,
-    **departments: dict[str, dict[str, float]]) -> tuple[list, float]:
+def get_salary_stats(limit: tuple = None, **departments: dict[str, dict[str, float]]) -> tuple:
     """
     Calculate the top 3 salaries and their ratio to the total salary.
 
