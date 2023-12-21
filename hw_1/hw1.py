2178dc02b6cd55f16e2edb3e1e209c0f0b64c5c5
Fenya <frsafronova@gmail.com>
2023-12-21 18:59:12 +0300

    fixing errors that the linter shows on Github

--- hw_1/hw1.py
+++ hw_1/hw1.py
@@ -10,13 +10,13 @@ def company_salary_stats(
     """Calculate statistics of salaries in the company.
 
         Args:
-        exclude_departments: optional argument to exclude specific departments from the statistics.
-        departments: keyword arguments where department names are keys and values are dictionaries
-        with employee names (str) as keys and salaries (float) as values.
+            exclude_departments: optional argument to exclude specific departments from the statistics.
+            departments: keyword arguments where department names are keys and values are dictionaries
+            with employee names (str) as keys and salaries (float) as values.
 
         Returns:
-        dictionary: containing the average, maximum, and median salaries in the company.
-        All numbers are rounded to two decimal places.
+            dictionary: containing the average, maximum, and median salaries in the company.
+            All numbers are rounded to two decimal places.
     """
     all_salaries = []
     for department, employees in departments.items():
