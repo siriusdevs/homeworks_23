d5ab61445d0beda46c383a00feb6465a60d39e55
fghh64 <fghh64.46hhgf@yandex.ru>
2023-12-21 16:12:20 +0300

    test 6 hw1

--- test_hw1.py
+++ test_hw1.py
@@ -9,7 +9,7 @@ def test_get_salary_stats_no_limit():
         IT={'Alice': 1000.0, 'Bob': 2000.0, 'Charlie': 3000.0},
         HR={'Dave': 4000.0, 'Eve': 5000.0, 'Frank': 6000.0},
     )
-    assert stats == ([1000.0, 2000.0, 3000.0], 30.0)
+    assert stats == ([1000.0, 2000.0, 3000.0], 28.57)
 
 
 def test_get_salary_stats_with_limit():
