1004848872b52d6f15ec8cb2456dacf51ed47d78
VikaPaz <vikaqse@gmail.com>
2023-12-22 12:55:17 +0300

    add docstrings

--- test_hw3.py
+++ test_hw3.py
@@ -2,12 +2,14 @@ from hw3 import Teacher, Student, Course, Group, EducationalInstitution
 
 
 def test_teacher_create_course():
+    """The test for the funck create_course of the class Teacher"""
     teacher = Teacher("Иванов")
     course = teacher.create_course("Программирование")
     assert course.course_name == "Программирование"
     assert course.teacher == teacher
 
 def test_teacher_assign_student():
+    """The test for the funck assign_student of the class Teacher"""
     teacher = Teacher("Зайцев")
     course = Course("ОС", teacher)
     student = Student("Петров")
@@ -15,12 +17,14 @@ def test_teacher_assign_student():
     assert student in course.students
 
 def test_student_enroll():
+    """The test for the funck student_enroll of the class Student"""
     student = Student("Сидоров")
     course = Course("ПЦУ", Teacher("Кузнецов"))
     student.enroll(course)
     assert student in course.students
 
 def test_student_drop_course():
+    """The test for the funck drop_course of the class Student"""
     student = Student("Васнецов")
     course = Course("ОФП", Teacher("Смирнов"))
     student.enroll(course)
@@ -28,12 +32,14 @@ def test_student_drop_course():
     assert student not in course.students
 
 def test_group_add_student():
+    """The test for the funck add_student of the class Group"""
     group = Group("К0709")
     student = Student("Попов")
     group.add_student(student)
     assert student in group.students
 
 def test_group_remove_student():
+    """The test for the funck remove_student of the class Group"""
     group = Group("К0111")
     student = Student("Петров")
     group.add_student(student)
@@ -41,12 +47,14 @@ def test_group_remove_student():
     assert student not in group.students
 
 def test_educational_institution_add_course():
+    """The test for the funck add_course of the class EducationalInstitution"""
     institution = EducationalInstitution()
     course = Course("Сети", Teacher("Головин"))
     institution.add_course(course)
     assert course in institution.courses
 
 def test_educational_institution_remove_course():
+    """The test for the funck remove_course of the class EducationalInstitution"""
     institution = EducationalInstitution()
     course = Course("Вышмат", Teacher("Иванов"))
     institution.add_course(course)
@@ -54,12 +62,14 @@ def test_educational_institution_remove_course():
     assert course not in institution.courses
 
 def test_educational_institution_add_group():
+    """The test for the funck add_group of the class EducationalInstitution"""
     institution = EducationalInstitution()
     group = Group("К0609")
     institution.add_group(group)
     assert group in institution.groups
 
 def test_educational_institution_remove_group():
+    """The test for the funck remove_group of the class EducationalInstitution"""
     institution = EducationalInstitution()
     group = Group("К0409")
     institution.add_group(group)
