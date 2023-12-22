e62253660c411388b73b31e2a927aeb0a82656ee
ilya <135736769+agent-ilyusha@users.noreply.github.com>
2023-12-22 17:41:06 +0300

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -5,15 +5,15 @@
 def three_max_selaries(
     list_salary: list[float],
     cap_salary: float,
-) -> tuple[list[float], list[float]]:
+) -> tuple[list[float]]:
     """Search three max salaries and all salaries are less than cap_salary.
 
     Parameters:
-        list_salary: list[float] - all salaries
+        list_salary: List[float] - all salaries
         cap_salary: None | float - maximum salary
 
     Returns:
-        tuple[list[float], list[float]] - top three salaries and all salaries less than cap_salary
+        Tuple[List[float], List[float]] - top three salaries and all salaries less than cap_salary
     """
     list_max_salaries: list[float] = []
 
@@ -24,7 +24,7 @@ def three_max_selaries(
             list_salary = list_salary[-list_salary.index(salary)-1:]
             break
 
-    return list_max_salaries, list_salary
+    return list_max_salaries
 
 
 def check_sum_salary(
@@ -34,17 +34,17 @@ def check_sum_salary(
     """Return three max salaries.
 
     Parameters:
-        departments:tuple[str, list[float]] - takes two arguments: name company and all salaries
+        departments: Tuple[str, List[float]] - takes two arguments: name company and all salaries
         salary_cap: None | float - maximum salary
 
     Returns:
-        tuple[list[float], float] - top three salaries in company and percentage of salary
+        Tuple[List[float], float] - top three salaries in company and percentage of salary
 
     Raises:
         TypeError: if not isinstance(departments[ind][0], str).
     """
-    if salary_cap is not None and salary_cap < 0:
-        salary_cap = 0
+    if salary_cap is None or salary_cap < 0:
+        salary_cap = float('inf')
 
     list_all_salaries: list[float] = []
 
@@ -55,19 +55,27 @@ def check_sum_salary(
         list_all_salaries.extend(departments[ind][1])
 
     list_all_salaries.sort(reverse=True)
-    list_all_salaries = [salary for salary in list_all_salaries if salary >= 0]
+    list_all_salaries = [round(salary, 2)
+                        for salary in list_all_salaries
+                        if salary >= 0
+                        ]
+    
 
-    if salary_cap is not None:
-        list_three_max_salaries, list_all_salaries = three_max_selaries(
-            list_all_salaries,
-            salary_cap,
-        )
-    else:
-        list_three_max_salaries = list_all_salaries[:3]
+
+    list_three_max_salaries= three_max_selaries(
+        list_all_salaries,
+        salary_cap,
+            )
+
+    
 
     sum_all_sal = sum(list_all_salaries)
+    print(list_three_max_salaries, sum_all_sal)
 
     return (
         list_three_max_salaries,
         round((sum(list_three_max_salaries) / sum_all_sal)*100, 2),
         ) if sum_all_sal > 0 else 0
+
+
+print(check_sum_salary(('b', [1, 0.1, 3]), ('n', [0, 5, 1.1])))
