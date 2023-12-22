1d93c14af97a1efa0fcc3289f09bb9ae1c22e719
VikaPaz <100610578+VikaPaz@users.noreply.github.com>
2023-12-22 17:46:08 +0300

    Update by flake8 test_hw2.py

--- test_hw2.py
+++ test_hw2.py
@@ -1,33 +1,37 @@
-import pytest
+"""test for hw2.py."""
 import json
+
+import pytest
+
 from hw2 import process_data
 
-
 test_data = [('data_hw2.json', 'result_test_hw2.py', {
-    "hosts_count": {
-        "yandex.ru": 50.0,
-        "gmail.com": 50.0
+    'hosts_count': {
+        'yandex.ru': 50.0,
+        'gmail.com': 50.0,
     },
-    "online_duration": {
-        "Less than 2 days": 0.0,
-        "Less than 1 week": 0.0,
-        "Less than 1 month": 0.0,
-        "Less than 6 months": 50.0,
-        "More than 6 months": 50.0
-    }
-})]
+    'online_duration': {
+        'Less than 2 days': 0,
+        'Less than 1 week': 0,
+        'Less than 1 month': 0,
+        'Less than 6 months': 50.0,
+        'More than 6 months': 50.0,
+    },
+},
+),
+]
 
 
-@pytest.mark.parametrize("input_file, output_file, data", test_data)
-def test_process_data(input_file, output_file, data):
-    """The test for funck process_data. 
+@pytest.mark.parametrize('input_file, output_file, output_data', test_data)
+def test_process_data(input_file, output_file, output_data):
+    """Test for funck process_data.
 
     Args:
-        input_file (str): The name of file input.
-        output_file (str): The name of file output.
-        data (dict): TThe test date result of the funck. 
-    """    
+        input_file (str): _description_
+        output_file (str): _description_
+        output_data (dict): _description_
+    """
     process_data(input_file_path=input_file, output_file_path=output_file)
-    with open(output_file, 'r') as output_data:
-        result = json.load(output_data)
-    assert result == data
+    with open(output_file, 'r') as output:
+        result_data = json.load(output)
+    assert result_data == output_data
