fdbdd5de0b5340e47d444abfd5f8f49f0f9dc0d1
VikaPaz <100610578+VikaPaz@users.noreply.github.com>
2023-12-22 17:46:51 +0300

    Update by flake8 test_hw1.py

--- test_hw1.py
+++ test_hw1.py
@@ -1,35 +1,39 @@
 """test for hw1.py."""
 import pytest
+
 from hw1 import top3
 
-data = [
+test_data = [
+    (
         (
-            (
-                ("Department 1", {"John Smith": 50000, "Jane Doe": 60000}),
-                ("Department 2", {"Alice Johnson": 70000, "Bob Smith": 80000}),
-                ("Department 3", {"Mike Brown": 90000, "Emily Davis": 100000}),
-            ),
-            [['Department 3', 'Department 2', 'Department 1'], 
-             ['Department 1', 'Department 2', 'Department 3']],
+            ('Department 1', {'John Smith': 50000, 'Jane Doe': 60000}),
+            ('Department 2', {'Alice Johnson': 70000, 'Bob Smith': 80000}),
+            ('Department 3', {'Mike Brown': 90000, 'Emily Davis': 100000}),
         ),
+        [['Department 3', 'Department 2', 'Department 1'],
+         ['Department 1', 'Department 2', 'Department 3'],
+         ],
+    ),
+    (
         (
-            (
-                ("Department A", {"John Doe": 60000, "Jane Smith": 50000}),
-                ("Department B", {"Alice Davis": 80000, "Bob Johnson": 70000}),
-                ("Department C", {"Mike Smith": 100000, "Emily Brown": 90000}),
-            ),
-            [['Department C', 'Department B', 'Department A'], 
-             ['Department A', 'Department B', 'Department C']],
+            ('Department A', {'John Doe': 60000, 'Jane Smith': 50000}),
+            ('Department B', {'Alice Davis': 80000, 'Bob Johnson': 70000}),
+            ('Department C', {'Mike Smith': 100000, 'Emily Brown': 90000}),
         ),
-    ]
+        [['Department C', 'Department B', 'Department A'],
+         ['Department A', 'Department B', 'Department C'],
+         ],
+    ),
+]
 
-@pytest.mark.parametrize("departments, expected_result", data)
+
+@pytest.mark.parametrize('departments, expected_result', test_data)
 def test_top3(departments, expected_result):
     """Test for funck top3.
 
     Args:
         departments (tuple): The departments for the test data.
         expected_result (list): The test date result of the funck.
-    """    
-    result = top3(*departments)
-    assert result == expected_result
+    """
+    result_data = top3(*departments)
+    assert result_data == expected_result
