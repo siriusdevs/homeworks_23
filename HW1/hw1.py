cb5538ec24519b6915dafa5f6bd32a2492006c6e
anonim767bn <anonim2462@yandex.ru>
2023-12-22 17:19:51 +0300

    fixing module import in hw1

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -1,7 +1,29 @@
 """Function calculates top 3 salaries and their sum percentage of all salaries."""
-from typing import Optional
+from typing import Any, Optional
 
-from hw3 import check_type
+
+def check_type(
+    input_value: Any, types: tuple[type, ...] | type,
+    check_positive: Optional[bool] = False,
+) -> None:
+    """
+    Check if value is of type `types`.
+
+    Args:
+        input_value (Any): The value to check.
+        types (tuple[type, ...]): The type to check against.
+        check_positive (Optional[bool], optional): Check if value is positive. Defaults to False.
+
+    Raises:
+        TypeError: If value is not of type `types`.
+        ValueError: If value is not positive.
+    """
+    if not isinstance(input_value, types):
+        value_type = type(input_value)
+        error_msg = f'Expected type {types}, got {value_type}'
+        raise TypeError(error_msg)
+    if check_positive and input_value < 0:
+        raise ValueError(f'Expected positive value, but got {input_value}')
 
 
 def salary_stat(
