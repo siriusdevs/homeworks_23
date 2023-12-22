730f44c349a3acedba943fe85ba47e075ea52fbb
LifelongCat <lifelongcat@yandex.ru>
2023-12-22 11:32:39 +0300

    complete hw3

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -12,7 +12,7 @@ Task3: Управление учебными курсами
 Класс "Студент":
 Поля:
 Имя студента
-Группа
+Возраст
 
 Класс "Курс":
 Поля:
@@ -35,6 +35,8 @@ Task3: Управление учебными курсами
 
 Класс "Учебное заведение":
 Поля:
+Название
+Список групп - массив или список объектов класса "Группа"
 Список курсов - массив или список объектов класса "Курс"
 Методы:
 Добавить курс в учебное заведение
@@ -44,3 +46,147 @@ Task3: Управление учебными курсами
 Удалить группу из учебного заведения
 Получить список всех групп в учебном заведении
 """
+
+from add_decorators import add_simple_list_operations, add_simple_property
+
+
+@add_simple_property('teacher_name', str)
+@add_simple_property('faculty', str)
+class Teacher:
+    """Teacher of instituion class."""
+
+    def __init__(self, teacher_name: str, faculty: str) -> None:
+        """Teacher initialization.
+
+        Args:
+            teacher_name: teacher's name
+            faculty: teacher's faculty
+        """
+        self.teacher_name, self.faculty = teacher_name, faculty
+
+    def __str__(self) -> str:
+        """Get string representation of the teacher object.
+
+        Returns:
+            string representation
+        """
+        return f'Teacher "{self.teacher_name}" on faculty "{self.faculty}"'
+
+
+@add_simple_property('student_name', str)
+@add_simple_property('age', int)
+class Student:
+    """Student of instituion class."""
+
+    def __init__(self, student_name: str, age: int) -> None:
+        """Student initialization.
+
+        Args:
+            student_name: student's name
+            age: student's age
+        """
+        self.student_name, self.age = student_name, age
+
+    def __str__(self) -> str:
+        """Get string representation of the student object.
+
+        Returns:
+            string representation
+        """
+        return f'Student "{self.student_name}" {self.age} y.o.'
+
+
+@add_simple_property('course_name', str)
+@add_simple_property('course_teacher', Teacher)
+@add_simple_property('course_students', Student, is_list=True)
+@add_simple_list_operations('course_students', 'course_student', Student)
+class Course:
+    """Course of institution class."""
+
+    def __init__(
+        self,
+        course_name: str,
+        course_teacher: Teacher,
+        course_students: list[Student],
+    ) -> None:
+        """Course initialization.
+
+        Args:
+            course_name: course's name
+            course_teacher: course's teacher
+            course_students: students who study on the course
+        """
+        self.course_name = course_name
+        self.course_teacher = course_teacher
+        self.course_students = course_students
+
+    def __str__(self) -> str:
+        """Get string representation of the course object.
+
+        Returns:
+            string representation
+        """
+        name, teacher, students = self.course_name, self.course_teacher, len(self.course_students)
+        return f'Course "{name}" with teacher "{teacher}" and {students} students'
+
+
+@add_simple_property('group_name', str)
+@add_simple_property('group_students', Student, is_list=True)
+@add_simple_list_operations('group_students', 'group_student', Student)
+class Group:
+    """Group of instituion class."""
+
+    def __init__(self, group_name: str, group_students: list[Student]) -> None:
+        """Group initialization.
+
+        Args:
+            group_name: group's name
+            group_students: students who are in the group
+        """
+        self.group_name, self.group_students = group_name, group_students
+
+    def __str__(self) -> str:
+        """Get string representation of the group object.
+
+        Returns:
+            string representation
+        """
+        name, students = self.group_name, len(self.group_students)
+        return f'Group "{name}" with {students} students'
+
+
+@add_simple_property('institution_name', str)
+@add_simple_property('institution_courses', Course, is_list=True)
+@add_simple_property('institution_groups', Group, is_list=True)
+@add_simple_list_operations('institution_courses', 'institution_course', Course)
+@add_simple_list_operations('institution_groups', 'institution_group', Group)
+class Institution:
+    """Institution class."""
+
+    def __init__(
+        self,
+        institution_name: str,
+        institution_courses: list[Course],
+        institution_groups: list[Group],
+    ) -> None:
+        """Institution initialization.
+
+        Args:
+            institution_name: institutuion's name
+            institution_courses: courses that teach in the institution
+            institution_groups: groups that are in the institution
+        """
+        self.institution_name = institution_name
+        self.institution_courses = institution_courses
+        self.institution_groups = institution_groups
+
+    def __str__(self) -> str:
+        """Get string representation of the institution object.
+
+        Returns:
+            string representation
+        """
+        name = self.institution_name
+        courses = len(self.institution_courses)
+        groups = len(self.institution_groups)
+        return f'Institution "{name}" with {courses} courses and {groups} groups'
--- hw3/test_hw3.py
+++ hw3/test_hw3.py
@@ -1,11 +1,92 @@
 """Tests for hw3.py."""
 
+from typing import Any
 
-def test():
-    """Test workflows.
+import pytest
+
+import hw3
+
+# testing objects
+teacher = hw3.Teacher('Trebla', 'Programming')
+teacher2 = hw3.Teacher('Valsehcayv', 'Networks')
+student = hw3.Student('Nikita', 17)
+student2 = hw3.Student('Egor', 16)
+student3 = hw3.Student('Yaroslav', 15)
+course = hw3.Course('Python', teacher, [student, student2])
+course2 = hw3.Course('JavaScript', teacher, [student2, student3])
+group = hw3.Group('К0709-23/2', [student, student3])
+group2 = hw3.Group('К0709-23/4', [student2, student3])
+institution = hw3.Institution('Better', [course], [group])
+
+TEST_OPERATIONS_DATA = (
+    (course, 'course_student', [str(elem) for elem in course.course_students], student3),
+    (group, 'group_student', [str(elem) for elem in group.group_students], student2),
+    (
+        institution,
+        'institution_course',
+        [str(elem) for elem in institution.institution_courses],
+        course2,
+    ),
+    (
+        institution,
+        'institution_group',
+        [str(elem) for elem in institution.institution_groups],
+        group2,
+    ),
+)
+
+TEST_GET_SET_DATA = (
+    (teacher, 'teacher_name', teacher.teacher_name, 'Sirob'),
+    (teacher, 'faculty', teacher.faculty, 'Art'),
+    (student, 'student_name', student.student_name, 'Daniel'),
+    (student, 'age', student.age, 18),
+    (course, 'course_name', course.course_name, 'IP'),
+    (course, 'course_teacher', course.course_teacher, teacher2),
+    (course, 'course_students', course.course_students, [student2, student3]),
+    (group, 'group_name', group.group_name, 'К0709-25/5'),
+    (group, 'group_students', group.group_students, [student, student2]),
+    (institution, 'institution_name', institution.institution_name, 'Best'),
+    (institution, 'institution_courses', institution.institution_courses, [course2]),
+    (institution, 'institution_groups', institution.institution_groups, [group2]),
+)
+
+
+@pytest.mark.parametrize('test_obj, attribute, curr_values, new_value', TEST_OPERATIONS_DATA)
+def test_operations(test_obj: type, attribute: str, curr_values: Any, new_value: Any) -> None:
+    """Test all methods with attributes: add, remove and get_all.
+
+    Args:
+        test_obj: object that will be tested
+        attribute: attribute that will be tested (e.g. student, not students!)
+        curr_values: current values of the object attribute (for testing get all)
+        new_value: new value that need to append and then remove
 
     Asserts:
-        Always True
+        True if function with current test data returns right value.
     """
-    test_value = 1
-    assert test_value
+    assert getattr(test_obj, f'get_all_{attribute}s')() == curr_values, \
+        "Get all method don't work"
+    getattr(test_obj, f'add_{attribute}')(new_value)
+    assert getattr(test_obj, f'get_all_{attribute}s')() == curr_values + [str(new_value)], \
+        "Add method don't work"
+    getattr(test_obj, f'remove_{attribute}')(new_value)
+    assert getattr(test_obj, f'get_all_{attribute}s')() == curr_values, \
+        "Remove method don't work"
+
+
+@pytest.mark.parametrize('test_obj, attribute, curr_value, new_value', TEST_GET_SET_DATA)
+def test_getters_setters(test_obj: type, attribute: str, curr_value: Any, new_value: Any) -> None:
+    """Test all getters and setters.
+
+    Args:
+        test_obj: object that will be tested
+        attribute: attribute that will be tested
+        curr_value: current value of the object attribute (for testing getter)
+        new_value: new value of the object attribute (for testing setter)
+
+    Asserts:
+        True if function with current test data returns right value.
+    """
+    assert getattr(test_obj, attribute) == curr_value, "Getter don't work"
+    setattr(test_obj, attribute, new_value)
+    assert getattr(test_obj, attribute) == new_value, "Setter don't work"
