a48b791b742e65ce60bcd58f4dc3d7dd99c62002
viktoriatyapkova <tyapkova06@bk.ru>
2023-12-22 23:10:18 +0300

    Fixed bug in the function call

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -19,52 +19,6 @@ LAP_TO_HAP_AGE_STRING = '46-60'
 HIGHEST_AND_ABOVE_AP_AGE_STRING = '60+'
 
 
-def create_file(output_file: str) -> None:
-    """Create an output file if it is missing.
-
-    Args:
-        output_file: str - the file with the result.
-    """
-    if os.path.dirname(output_file) and not os.path.exists(output_file):
-        os.makedirs(os.path(output_file))
-
-
-def check_is_file(input_file: str, output_file: str) -> bool:
-    """Check inputing file.
-
-    Args:
-        input_file: str - input data file.
-        output_file: str - the file with the result.
-
-    Returns:
-        bool: if the file contains an error.
-    """
-    try:
-        os.path.isfile(input_file)
-    except FileNotFoundError as invalid_info:
-        message = 'Is not a file'
-        error_info(output_file, type(invalid_info).__name__, message)
-        return False
-
-
-def check_input_file(input_file: str, output_file: str) -> bool:
-    """Check inputing file.
-
-    Args:
-        input_file: str - input data file.
-        output_file: str - the file with the result.
-
-    Returns:
-        bool: if the file contains an error.
-    """
-    try:
-        load_input_data(input_file)
-    except json.decoder.JSONDecodeError as invalid_info:
-        message = 'Input file is not a valid JSON string or is empty'
-        error_info(output_file, type(invalid_info).__name__, message)
-        return False
-
-
 def error_info(output_file: str, invalid_file: str, message: str) -> None:
     """
     Add text in outputing file if there is an error.
@@ -74,7 +28,6 @@ def error_info(output_file: str, invalid_file: str, message: str) -> None:
         invalid_file: str - input data file, which does not meet the conditions.
         message: str - text, that occurs in an error.
     """
-    create_file(output_file)
     with open(output_file, 'w') as out_file:
         json.dump(
             obj={'error': invalid_file, 'information': message},
@@ -83,17 +36,30 @@ def error_info(output_file: str, invalid_file: str, message: str) -> None:
         )
 
 
-def load_input_data(input_file: str) -> dict[str, dict]:
+def load_input_data(input_file: str, output_file: str) -> dict[str, dict]:
     """Take data from the input file.
 
     Args:
         input_file: str - input data file.
+        output_file: str - the file with the result.
 
     Returns:
         dict[str, dict]: data from the input file.
     """
+    try:
+        with open(input_file, 'r') as test_json:
+            json.load(test_json)
+    except FileNotFoundError as invalid_info:
+        message = 'Is not a file'
+        error_info(output_file, type(invalid_info).__name__, message)
+        return False
+    except json.decoder.JSONDecodeError as invalid_info:
+        message = 'Input file is not a valid JSON string or is empty'
+        error_info(output_file, type(invalid_info).__name__, message)
+        return False
+
     with open(input_file, 'r') as json_file_int:
-        input_data: dict[str, dict] = json.load(json_file_int)
+        input_data = json.load(json_file_int)
     return input_data
 
 
@@ -119,11 +85,11 @@ def record_age(information: dict, age_category: dict) -> None:
         age_category['60+'] += 1
 
 
-def calculate_age(input_file: str) -> dict:
+def calculate_age(input_data: dict[str, dict]) -> dict:
     """Calculate people by age.
 
     Args:
-        input_file: str - input data file.
+        input_data: dict[str, dict] - data from input file.
 
     Returns:
         dict: statistics on the ages of all people being entered.
@@ -135,24 +101,22 @@ def calculate_age(input_file: str) -> dict:
         LAP_TO_HAP_AGE_STRING: 0,
         HIGHEST_AND_ABOVE_AP_AGE_STRING: 0,
         }
-    input_data: dict[str, dict] = load_input_data(input_file)
     for _, information in input_data.items():
         record_age(information, age_category)
 
     return age_category
 
 
-def calculate_year(input_file: str) -> dict:
+def calculate_year(input_data: dict[str, dict]) -> dict:
     """Calculate people by year of registration.
 
     Args:
-        input_file: str - input data file.
+        input_data: dict[str, dict] - data from input file.
 
     Returns:
         dict: statistics of the years of registration of all entered people.
     """
     regist_y = {}
-    input_data: dict[str, dict] = load_input_data(input_file)
     for _, information in input_data.items():
         registered_year = information.get(REGISTERED)
         if registered_year:
@@ -170,7 +134,6 @@ def save_output_data(output_file: str, output_data: dict[str, dict]) -> None:
         output_file: str - the file with the result.
         output_data: str - the resulting data with statistics.
     """
-    create_file(output_file)
     with open(output_file, 'w') as json_file_out:
         json.dump(output_data, json_file_out, indent=4)
 
@@ -182,13 +145,9 @@ def process_data(input_file, output_file) -> None:
         input_file: str - input data file.
         output_file: str - the file with the result.
     """
-    if not check_is_file(input_file, output_file):
-        return
-    if not check_input_file(input_file, output_file):
-        return
-    input_data: dict[str, dict] = load_input_data(input_file)
-    age_category = calculate_age(input_file)
-    regist_y = calculate_year(input_file)
+    input_data = load_input_data(input_file, output_file)
+    age_category = calculate_age(input_data)
+    regist_y = calculate_year(input_data)
     if input_data:
         output_data = {
             AGE: {ctg: (cnt / len(input_data)) * 100 for ctg, cnt in age_category.items()},
@@ -199,4 +158,6 @@ def process_data(input_file, output_file) -> None:
             AGE: 0,
             REGISTERED: 0,
         }
+    if os.path.dirname(output_file) and not os.path.exists(output_file):
+        os.makedirs(os.path.dirname(output_file))
     save_output_data(output_file, output_data)
--- hw2/test_hw2.py
+++ hw2/test_hw2.py
@@ -5,12 +5,13 @@ import pytest
 
 from hw2 import process_data
 
+OUTPUT_FILE = 'result.json'
 VALID_TEST_DATA = (
-    ('input/data_1.json', 'output/result_1.json', 'expected/expected_1.json'),
-    ('input/data_2.json', 'output/result_2.json', 'expected/expected_2.json'),
-    ('input/data_3.json', 'output/result_3.json', 'expected/expected_3.json'),
-    ('input/data_4.json', 'output/result_4.json', 'expected/expected_4.json'),
-    ('input/data_5.json', 'output/result_5.json', 'expected/expected_5.json'),
+    ('input/data_1.json', OUTPUT_FILE, 'expected/expected_1.json'),
+    ('input/data_2.json', OUTPUT_FILE, 'expected/expected_2.json'),
+    ('input/data_3.json', OUTPUT_FILE, 'expected/expected_3.json'),
+    ('input/data_4.json', OUTPUT_FILE, 'expected/expected_4.json'),
+    ('input/data_5.json', OUTPUT_FILE, 'expected/expected_5.json'),
 )
 
 
@@ -31,25 +32,13 @@ def test_process_data(input_file: str, output_file: str, expected: str) -> None:
     assert result_data == expected_data
 
 
-INVALID_TEST_DATA = (
-    ('input/data_6.json', 'error/result_1.json', 'expected/expected_6.json'),
-    ('input/data_.json', 'error/result_2.json', 'expected/expected_7.json'),
-    ('input/data_7.txt', 'error/result_3.json', 'expected/expected_8.json'),
-)
+@pytest.mark.xfail
+def test_process_data_failed_first() -> None:
+    """First negative test."""
+    process_data('input/data_6.json', 'result_1.json')
 
 
-@pytest.mark.parametrize('input_file, output_file, expected', INVALID_TEST_DATA)
-def test_process_data_failed(input_file: str, output_file: str, expected: dict) -> None:
-    """Tests with error output with process_data.
-
-    Args:
-        input_file: str - input data file.
-        output_file: str - the file with the result.
-        expected: str - the file with the expected data.
-    """
-    process_data(input_file, output_file)
-    with open(expected) as expected_file:
-        expected_data = json.load(expected_file)
-    with open(output_file, 'r') as file_result:
-        result_data = json.load(file_result)
-    assert result_data == expected_data
+@pytest.mark.xfail
+def test_process_data_failed_second() -> None:
+    """Second negative test."""
+    process_data('input/data_.json', 'result_2.json')
