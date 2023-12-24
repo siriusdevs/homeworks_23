aafa03baed5f23ee0c5007f12e66a78206957edc
FGun4ik <evstifeev.egor@bk.ru>
2023-12-24 21:36:00 +0300

    main file added

---
+++ first_hw/main.py
@@ -0,0 +1,19 @@
+"""sdf"""
+import statistics
+from typing import Dict, Optional
+
+
+def salary_stats(
+    departments: Dict[str, Dict[str, float]], limit: Optional[float] = None
+) -> Dict[str, float]:
+    salaries = []
+    for department in departments.values():
+        for salary in department.values():
+            if limit is None or salary >= limit:
+                salaries.append(salary)
+    salaries = sorted([round(salary, 2) for salary in salaries])
+    return {
+        'average': round(sum(salaries) / len(salaries), 2),
+        'maximum': max(salaries) if salaries else None,
+        'median': round(statistics.median(salaries), 2) if salaries else None,
+    }
