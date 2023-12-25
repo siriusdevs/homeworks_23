5c8b3e8fbd8ca3f7c0bc894035f737e0187e9a74
hape30 <sasha2007.ivanov@mail.ru>
2023-12-25 12:21:27 +0300

    hw3 init

---
+++ hw3/hw3.py
@@ -0,0 +1,235 @@
+"""Архитектура классов для учета товаров в ресторане."""
+from typing import List
+
+
+class Product:
+    """Класс, представляющий товар."""
+
+    def __init__(self, name: str, price: float):
+        """
+        Инициализирует новый экземпляр класса Product.
+
+        Args:
+            name (str): название продукта
+            price (float): цена продукта
+        """
+        self._name = name
+        self._price = price
+
+    @property
+    def name(self) -> str:
+        """
+        Возвращает название товара.
+
+        Returns:
+            _name (str): название продукта
+        """
+        return self._name
+
+    @name.setter
+    def name(self, value: str):
+        """
+        Устанавливает название товара.
+
+        Args:
+            value (str): значения имени
+
+        Raises:
+            TypeError: если значение не является str
+        """
+        if not isinstance(value, str):
+            raise TypeError(f"Имя {value} должно быть str, а не {type(value).name}")
+        self._name = value
+
+    @property
+    def price(self) -> float:
+        """
+        Возвращает цену товара.
+
+        Returns:
+            _price (float): цена продукта
+        """
+        return self._price
+
+    @price.setter
+    def price(self, value: float):
+        """
+        Устанавливает цену товара.
+
+        Args:
+            value (str)
+
+        Raises:
+            TypeError: если значение не является str
+            ValueError: если цена меньше 0
+        """
+        if not isinstance(value, float):
+            raise TypeError(f"Цена {value} должно быть float, а не {type(value).price}")
+        if value <= 0:
+            raise ValueError("Цена должна быть положительным числом")
+        self._price = value
+
+
+class Dish:
+    """Класс, представляющий блюдо."""
+
+    def __init__(self, name: str):
+        """
+        Инициализирует новый экземпляр класса Dish.
+
+        Args:
+            name (str): имя блюда
+        """
+        self._name = name
+        self._products = []
+
+    @property
+    def name(self) -> str:
+        """
+        Возвращает название блюда.
+
+        Returns:
+            _name (str): значение имени
+        """
+        return self._name
+
+    @name.setter
+    def name(self, value: str):
+        """
+        Устанавливает название блюда.
+
+        Args:
+            value (str): название блюда
+
+        Raises:
+            TypeError: если значение не является str
+        """
+        if not isinstance(value, str):
+            raise TypeError(f"Имя {value} должно быть str, а не {type(value).name}")
+        self._name = value
+
+    @property
+    def products(self) -> List[Product]:
+        """
+        Возвращает список продуктов в блюде.
+
+        Returns:
+            List: список продуктов в блюде.
+        """
+        return self._products
+
+    def add_product(self, product: Product):
+        """
+        Добавляет продукт в блюдо.
+
+        Args:
+            product (Product): объект продукта.
+        """
+        self._products.append(product)
+
+    def remove_product(self, product: Product):
+        """
+        Удаляет продукт из блюда.
+
+        Args:
+            product (Product): объект продукта
+        """
+        self._products.remove(product)
+
+
+class Restaurant:
+    """Класс, представляющий ресторан."""
+
+    def __init__(self, name: str, dishes: List[Dish], inventory: List[Product]):
+        """
+        Инициализирует новый экземпляр класса Restaurant.
+
+        Args:
+            name (str): название ресторана
+            dishes (List): список блюд
+            inventory (List): список продуктов, из которых приготовлено блюдо
+        """
+        self._name = name
+        self._dishes = dishes
+        self._inventory = inventory
+
+    @property
+    def name(self) -> str:
+        """
+        Возвращает название ресторана.
+
+        Returns:
+            _name (str): название ресторана
+        """
+        return self._name
+
+    @name.setter
+    def name(self, value: str):
+        """
+        Устанавливает название ресторана.
+
+        Args:
+            value (str): значение названия ресторана
+
+        Raises:
+            TypeError: если значение не является str
+        """
+        if not isinstance(value, str):
+            raise TypeError(f"Имя {value} должно быть str, а не {type(value).name}")
+        self._name = value
+
+    @property
+    def dishes(self) -> List[Dish]:
+        """
+        Возвращает список блюд, предлагаемых рестораном.
+
+        Returns:
+            List: список блюд
+        """
+        return self._dishes
+
+    def add_dish(self, dish: Dish):
+        """
+        Добавляет блюдо в меню.
+
+        Args:
+            dish (Dish): объект продукта
+        """
+        self._dishes.append(dish)
+
+    def remove_dish(self, dish: Dish):
+        """
+        Удаляет блюдо из меню.
+
+        Args:
+            dish: объект продукта
+        """
+        self._dishes.remove(dish)
+
+    @property
+    def inventory(self) -> List[Product]:
+        """
+        Возвращает инвентарь ресторана.
+
+        Returns:
+            List (list): список продуктов для блюда.
+        """
+        return self._inventory
+
+    def order_dish(self, dish: Dish) -> str:
+        """
+        Заказывает блюдо, и обновляет инвентарь соответственно.
+
+        Args:
+            dish: объект продукта
+
+        Returns:
+            str: уведомление о статусе заказа
+        """
+        dish_products = dish.products
+        for product in dish_products:
+            if product not in self._inventory:
+                return "Не хватает товаров на складе"
+        for product in dish_products:
+            self._inventory.remove(product)
+        return "Блюдо заказано успешно"
---
+++ hw3/test_hw3.py
@@ -0,0 +1,70 @@
+"""Файл тестирования классов."""
+import unittest
+
+from hw3 import Dish, Product, Restaurant
+
+
+class TestRestaurantClasses(unittest.TestCase):
+    """Тесты для классов Product, Dish и Restaurant."""
+
+    def setUp(self):
+        """Настройка перед выполнением каждого теста."""
+        self.product1 = Product("Яблоко", 1.0)
+        self.product2 = Product("Банан", 2.0)
+
+        self.dish = Dish("Фруктовый салат")
+        self.dish.add_product(self.product1)
+        self.dish.add_product(self.product2)
+
+        self.restaurant = Restaurant(
+            "Наш ресторан", [self.dish], [self.product1, self.product2]
+        )
+
+    def test_product_attributes(self):
+        """Проверка атрибутов продукта."""
+        self.assertEqual(self.product1.name, "Яблоко")
+        self.assertEqual(self.product1.price, 1.0)
+
+    def test_dish_attributes(self):
+        """Проверка атрибутов блюда."""
+        self.assertEqual(self.dish.name, "Фруктовый салат")
+        self.assertEqual(len(self.dish.products), 2)
+
+    def test_restaurant_attributes(self):
+        """Проверка атрибутов ресторана."""
+        self.assertEqual(self.restaurant.name, "Наш ресторан")
+        self.assertEqual(len(self.restaurant.dishes), 1)
+        self.assertEqual(len(self.restaurant.inventory), 2)
+
+    def test_add_and_remove_dish_from_menu(self):
+        """Проверка добавления и удаления блюда из меню ресторана."""
+        new_dish = Dish("Суп")
+        self.restaurant.add_dish(new_dish)
+        self.assertIn(new_dish, self.restaurant.dishes)
+        self.restaurant.remove_dish(new_dish)
+        self.assertNotIn(new_dish, self.restaurant.dishes)
+
+    def test_add_and_remove_product_from_dish(self):
+        """Проверка добавления и удаления продукта из блюда."""
+        new_product = Product("Апельсин", 1.5)
+        self.dish.add_product(new_product)
+        self.assertIn(new_product, self.dish.products)
+        self.dish.remove_product(new_product)
+        self.assertNotIn(new_product, self.dish.products)
+
+    def test_order_dish_with_available_inventory(self):
+        """Проверка успешного заказа блюда с доступным инвентарем."""
+        result = self.restaurant.order_dish(self.dish)
+        self.assertEqual(result, "Блюдо заказано успешно")
+        self.assertEqual(len(self.restaurant.inventory), 0)
+
+    def test_order_dish_without_enough_inventory(self):
+        """Проверка заказа блюда без достаточного инвентаря."""
+        self.restaurant.order_dish(self.dish)
+        result = self.restaurant.order_dish(self.dish)
+        self.assertEqual(result, "Не хватает товаров на складе")
+        self.assertEqual(len(self.restaurant.inventory), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
