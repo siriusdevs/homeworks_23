def40ee70df668b8f14240c6b3e1acb17522ca9a
hape30 <sasha2007.ivanov@mail.ru>
2023-12-25 12:16:40 +0300

    added hw1

---
+++ hw1/hw1.py
@@ -0,0 +1,47 @@
+"""hw1.py."""
+
+
+def calculate_salary(
+    limit: float | None = None,
+    **departments: dict[str, float],
+) -> tuple[float, list[float]]:
+    """
+    Calculate the percentage of the top 3 salaries
+    compared to the total sum of all salaries.
+
+    Args:
+        departments: keyword arguments, where values
+            containing employees names and salaries.
+        limit: Salary limit to consider. Defaults to None.
+
+    Returns:
+        tuple: Percentage of the top 3 salaries and
+            a list of rounded top 3 salaries.
+    """
+    all_salaries = []
+    for _, salaries in departments.items():
+        all_salaries.extend(salaries.values())
+
+    if limit is not None:
+        all_salaries = [salary for salary in all_salaries if salary >= limit]
+
+    all_salaries.sort()
+
+    top_salaries = all_salaries[:3]
+
+    if sum(all_salaries) == 0:
+        return ()
+
+    percentage = round((sum(top_salaries) / sum(all_salaries)) * 100, 2)
+
+    rounded_salaries = [round(salary, 2) for salary in top_salaries]
+
+    return percentage, rounded_salaries
+
+
+(
+    calculate_salary(
+        IT={'John': 5000.0, 'Jane': 5000.0, 'Bob': 5000.0},
+        Phone={'Alex': 2500.66, 'Alice': 2512.1428},
+    )
+)
---
+++ hw1/test_hw1.py
@@ -0,0 +1,67 @@
+"""Testing module hw1.py."""
+import pytest
+
+from hw1 import calculate_salary
+
+TEST_CASES = (
+    (
+        {'IT': {'John': 5000.0, 'Jane': 6000.0, 'Bob': 4500.0}},
+        None,
+        (100.0, [4500.0, 5000.0, 6000.0]),
+    ),
+    (
+        {},
+        None,
+        (),
+    ),
+    (
+        {'IT': {'John': 5000.0, 'Jane': 6000.0, 'Bob': 4500.0}},
+        6000.0,
+        (100.0, [6000.0]),
+    ),
+    (
+        {'IT': {'John': 5000.0, 'Jane': 6000.0, 'Bob': 4500.0}},
+        4500.0,
+        (100.0, [4500.0, 5000.0, 6000.0]),
+    ),
+    (
+        {'IT': {'John': 5000.0, 'Jane': 6000.0, 'Bob': 4500.0}},
+        5000.0,
+        (100.0, [5000.0, 6000.0]),
+    ),
+    (
+        {'IT': {'John': -5000.0, 'Jane': -6000.0, 'Bob': -4500.0}},
+        None,
+        (100.0, [-6000.0, -5000.0, -4500.0]),
+    ),
+    (
+        {'IT': {'John': 5000.0, 'Jane': 5000.0, 'Bob': 5000.0}},
+        None,
+        (100.0, [5000.0, 5000.0, 5000.0]),
+    ),
+    (
+        {
+            'IT': {'John': 5000.0, 'Jane': 5000.0, 'Bob': 5000.0},
+            'Phones': {'Alex': 2500.66, 'Alice': 2512.1428},
+        },
+        None,
+        (50.03, [2500.66, 2512.14, 5000.0]),
+    ),
+)
+
+
+@pytest.mark.parametrize('departments, limit, expected_result', TEST_CASES)
+def test_calculate_salary(
+    departments: dict,
+    limit: float | None,
+    expected_result: tuple[float, list[float]],
+) -> None:
+    """
+    Test the calculate_salary function with different scenarios.
+
+    Args:
+        departments: keyword arguments, where values containing employees' names and salaries.
+        limit: Salary limit to consider.
+        expected_result: Expected result tuple containing the percentage and rounded top salaries.
+    """
+    assert calculate_salary(limit, **departments) == expected_result
