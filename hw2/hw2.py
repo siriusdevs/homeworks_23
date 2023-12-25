b6df228ae10b9657c85aabb4e9dd73976485d342
hape30 <sasha2007.ivanov@mail.ru>
2023-12-25 12:28:29 +0300

    flake error

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -4,19 +4,16 @@ from datetime import datetime
 
 
 def process_data(
-    input_path: str = "data_hw2.json", output_path: str = "data_result.json"
-) -> None:
+    input_path: str = 'data_hw2.json', output_path: str = 'data_result.json'
+):
     """
     Функция реализующая анализ существующего json и соберет из него новую статистику.
 
     Args:
         input_path (str): путь, где находится .json файл.
         output_path (str): путь для записи итогового .json файла.
-
-    Returns:
-        None
     """
-    with open(input_path, "r") as input_file:
+    with open(input_path, 'r') as input_file:
         data = json.load(input_file)
 
     regions = {}
@@ -24,9 +21,9 @@ def process_data(
     last_login_dates = []
 
     for user, user_info in data.items():
-        region = user_info["region"]
-        age = user_info["age"]
-        last_login = datetime.strptime(user_info["last_login"], "%Y-%m-%d")
+        region = user_info['region']
+        age = user_info['age']
+        last_login = datetime.strptime(user_info['last_login'], '%Y-%m-%d')
 
         if region not in regions:
             regions[region] = 0
@@ -47,18 +44,18 @@ def process_data(
     online_days = [time.total_seconds() / (60 * 60 * 24) for time in online_times]
 
     stats = {
-        "region_distribution": region_distribution,
-        "average_age": average_age,
-        "online_times": {
-            "<2 days": sum(day < 2 for day in online_days) / total_users,
-            "<1 week": sum(day < 7 for day in online_days) / total_users,
-            "<1 month": sum(day < 30 for day in online_days) / total_users,
-            "<6 months": sum(day < 180 for day in online_days) / total_users,
-            ">6 months": sum(day > 180 for day in online_days) / total_users,
+        'region_distribution': region_distribution,
+        'average_age': average_age,
+        'online_times': {
+            '<2 days': sum(day < 2 for day in online_days) / total_users,
+            '<1 week': sum(day < 7 for day in online_days) / total_users,
+            '<1 month': sum(day < 30 for day in online_days) / total_users,
+            '<6 months': sum(day < 180 for day in online_days) / total_users,
+            '>6 months': sum(day > 180 for day in online_days) / total_users,
         },
     }
 
-    with open(output_path, "w") as output_file:
+    with open(output_path, 'w') as output_file:
         json.dump(stats, output_file)
 
 
--- hw2/test_hw2.py
+++ hw2/test_hw2.py
@@ -11,18 +11,18 @@ class TestProcessData(unittest.TestCase):
 
     def test_process_data(self):
         """Метод тестирования возвращаемого json файла."""
-        input_path = os.path.join("data_hw2.json")
-        output_path = os.path.join("data_result.json")
+        input_path = os.path.join('data_hw2.json')
+        output_path = os.path.join('data_result.json')
 
         process_data(input_path, output_path)
 
-        with open(output_path, "r") as output_file:
+        with open(output_path, 'r') as output_file:
             stats = json.load(output_file)
 
         self.assertIsInstance(stats, dict)
-        self.assertIn("region_distribution", stats)
-        self.assertIn("average_age", stats)
-        self.assertIn("online_times", stats)
+        self.assertIn('region_distribution', stats)
+        self.assertIn('average_age', stats)
+        self.assertIn('online_times', stats)
 
 
 if __name__ == "__main__":
