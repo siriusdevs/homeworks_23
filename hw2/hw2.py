e49d981b63b40206c10c154f4913c655d175e390
hape30 <sasha2007.ivanov@mail.ru>
2023-12-25 23:40:52 +0300

    fixed flake errors

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -2,12 +2,26 @@
 import json
 from datetime import datetime
 
-MONTH = 30
-HALF_YEAR = 180
+from init_constants import Constants
+
+
+def load_json_data(input_path: str = 'data_hw2.json'):
+    """
+    Функция подгрузки json информации.
+
+    Args:
+        input_path (str): путь к файлу json.
+
+    Returns:
+        dict: загруженный json.
+    """
+    with open(input_path, 'r') as input_file:
+        return json.load(input_file)
 
 
 def process_data(
-    input_path: str = 'data_hw2.json', output_path: str = 'data_result.json',
+    input_path: str = 'data_hw2.json',
+    output_path: str = 'data_result.json',
 ):
     """
     Функция реализующая анализ существующего json и соберет из него новую статистику.
@@ -16,47 +30,53 @@ def process_data(
         input_path (str): путь, где находится .json файл.
         output_path (str): путь для записи итогового .json файла.
     """
-    with open(input_path, 'r') as input_file:
-        json_data = json.load(input_file)
-
+    json_data = load_json_data()
     result_data = {
         'regions': {},
         'ages': [],
         'last_login_dates': [],
-        'total': len(json_data),
+        f'{Constants.total_list[0]}': len(json_data),
     }
 
     for _user, user_info in json_data.items():
         last_login = datetime.strptime(user_info['last_login'], '%Y-%m-%d')
 
-        if user_info['region'] not in result_data['regions']:
-            result_data['regions'][user_info['region']] = 0
-        result_data['regions'][user_info['region']] += 1
+        if user_info['region'] not in result_data[Constants.regions_list[0]]:
+            result_data[Constants.regions_list[0]][user_info['region']] = 0
+        result_data[Constants.regions_list[0]][user_info['region']] += 1
 
         result_data['ages'].append(user_info['age'])
         result_data['last_login_dates'].append(last_login)
 
     result_data['region_distribution'] = {
-        region: (count / result_data['total']) * 100
-        for region, count in result_data['regions'].items()
+        region: (count / result_data[Constants.total_list[0]]) * 100
+        for region, count in result_data[Constants.regions_list[0]].items()
     }
 
-    result_data['online_times'] = [datetime.now() - date
-                                   for date in result_data['last_login_dates']]
-    result_data['online_days'] = [time.total_seconds() / (60 * 60 * 24) 
-                                  for time in result_data['online_times']]
+    result_data['online_times'] = [
+        datetime.now() - date for date in result_data['last_login_dates']
+    ]
+    result_data['online_days'] = [
+        time.total_seconds() / (60 * 60 * 24) for time in result_data['online_times']
+    ]
 
-    const = result_data['total']
-    const2 = result_data['online_days']
     result_data['stats'] = {
         'region_distribution': result_data['region_distribution'],
-        'average_age': sum(result_data['ages']) / result_data['total'],
+        'average_age': sum(result_data['ages']) / result_data[Constants.total_list[0]],
         'online_times': {
-            '<2 days': sum(day < 2 for day in const2) / const,
-            '<1 week': sum(day < 7 for day in const2) / const,
-            '<1 month': sum(day < MONTH for day in const2) / const,
-            '<6 months': sum(day < HALF_YEAR for day in const2) / const,
-            '>6 months': sum(day > HALF_YEAR for day in const2) / const,
+            '<2 days': sum(day < 2 for day in result_data[Constants.online_days[0]])
+            / result_data[Constants.total_list[0]],
+            '<1 week': sum(day < 7 for day in result_data[Constants.online_days[0]])
+            / result_data[Constants.total_list_two[0]],
+            '<1 month': sum(
+                day < Constants.month for day in result_data[Constants.online_days[0]]
+            )
+            / result_data[Constants.total_list_two[0]],
+            '>6 months': sum(
+                day > Constants.half_year
+                for day in result_data[Constants.online_days[0]]
+            )
+            / result_data['total'],
         },
     }
 
---
+++ hw2/init_constants.py
@@ -0,0 +1,11 @@
+"""Константы для файла hw2."""
+
+
+class Constants:
+    """Класс инициализации глобальных переменных."""
+    regions_list = ['regions']
+    total_list = ['total']
+    online_days = ['online_days']
+    total_list_two = ['total']
+    MONTH = 30
+    HALF_YEAR = 180
