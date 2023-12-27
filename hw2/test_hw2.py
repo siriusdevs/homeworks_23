c994c8e266a705ab84dff668e9e85000a1234741
ilya-danilov <ilya.danilov.dev@gmail.com>
2023-12-27 14:18:05 +0300

    change several functions to check for invalid data to one function

--- hw2/test_hw2.py
+++ hw2/test_hw2.py
@@ -36,56 +36,6 @@ valid_test_data = (
     ),
 )
 
-invalid_test_data = (
-    (123, './hw2/test_data/IO_data/valid_data/output_data/output_1.json'),
-    (
-        './hw2/test_data/IO_data/invalid_data/not_json.txt',
-        './hw2/test_data/IO_data/valid_data/output_data/output_1.json',
-    ),
-    (
-        'non_existent_file.json',
-        './hw2/test_data/IO_data/valid_data/output_data/output_1.json',
-    ),
-    (
-        './hw2/test_data/IO_data/invalid_data/empty.json',
-        './hw2/test_data/IO_data/valid_data/output_data/output_1.json',
-    ),
-    (
-        './hw2/test_data/IO_data/invalid_data/input_1.json',
-        './hw2/test_data/IO_data/valid_data/output_data/output_1.json',
-    ),
-    (
-        './hw2/test_data/IO_data/invalid_data/input_2.json',
-        './hw2/test_data/IO_data/valid_data/output_data/output_1.json',
-    ),
-    (
-        './hw2/test_data/IO_data/invalid_data/input_3.json',
-        './hw2/test_data/IO_data/valid_data/output_data/output_1.json',
-    ),
-    (
-        './hw2/test_data/IO_data/invalid_data/input_4.json',
-        './hw2/test_data/IO_data/valid_data/output_data/output_1.json',
-    ),
-    (
-        './hw2/test_data/IO_data/invalid_data/input_5.json',
-        './hw2/test_data/IO_data/valid_data/output_data/output_1.json',
-    ),
-    (
-        './hw2/test_data/IO_data/invalid_data/input_6.json',
-        './hw2/test_data/IO_data/valid_data/output_data/output_1.json',
-    ),
-
-    ('./hw2/test_data/IO_data/valid_data/input_data/input_1.json', 123),
-    (
-        './hw2/test_data/IO_data/valid_data/input_data/input_1.json',
-        './hw2/test_data/IO_data/invalid_data/not_json.txt',
-    ),
-    (
-        './hw2/test_data/IO_data/valid_data/input_data/input_1.json',
-        'non_existent_file.json',
-    ),
-)
-
 
 @pytest.mark.parametrize('input_file_path, output_file_path, expected', valid_test_data)
 def test_process_data(input_file_path: str, output_file_path: str, expected: tuple):
@@ -104,80 +54,70 @@ def test_process_data(input_file_path: str, output_file_path: str, expected: tup
     assert output_data == expected_data
 
 
-@pytest.mark.xfail(raises=TypeError)
-def test_input_file_path_not_str():
-    """Test the error for the fact that the input file path is not str."""
-    process_data(*invalid_test_data[0])
+invalid_test_data = (
+    (123, './hw2/test_data/IO_data/valid_data/output_data/output_1.json', TypeError),
+    (
+        './hw2/test_data/IO_data/invalid_data/not_json.txt',
+        './hw2/test_data/IO_data/valid_data/output_data/output_1.json',
+        exceptions.FileIsNotJsonError,
+    ),
+    (
+        'non_existent_file.json',
+        './hw2/test_data/IO_data/valid_data/output_data/output_1.json',
+        FileNotFoundError,
+    ),
+    (
+        './hw2/test_data/IO_data/invalid_data/empty.json',
+        './hw2/test_data/IO_data/valid_data/output_data/output_1.json',
+        exceptions.FileIsEmptyError,
+    ),
+    (
+        './hw2/test_data/IO_data/invalid_data/input_1.json',
+        './hw2/test_data/IO_data/valid_data/output_data/output_1.json',
+        TypeError,
+    ),
+    (
+        './hw2/test_data/IO_data/invalid_data/input_2.json',
+        './hw2/test_data/IO_data/valid_data/output_data/output_1.json',
+        TypeError,
+    ),
+    (
+        './hw2/test_data/IO_data/invalid_data/input_3.json',
+        './hw2/test_data/IO_data/valid_data/output_data/output_1.json',
+        IndexError,
+    ),
+    (
+        './hw2/test_data/IO_data/invalid_data/input_4.json',
+        './hw2/test_data/IO_data/valid_data/output_data/output_1.json',
+        ValueError,
+    ),
+    (
+        './hw2/test_data/IO_data/invalid_data/input_5.json',
+        './hw2/test_data/IO_data/valid_data/output_data/output_1.json',
+        TypeError,
+    ),
+    (
+        './hw2/test_data/IO_data/invalid_data/input_6.json',
+        './hw2/test_data/IO_data/valid_data/output_data/output_1.json',
+        TypeError,
+    ),
+
+    ('./hw2/test_data/IO_data/valid_data/input_data/input_1.json', 123, TypeError),
+    (
+        './hw2/test_data/IO_data/valid_data/input_data/input_1.json',
+        './hw2/test_data/IO_data/invalid_data/not_json.txt',
+        exceptions.FileIsNotJsonError,
+    ),
+    (
+        './hw2/test_data/IO_data/valid_data/input_data/input_1.json',
+        'non_existent_file.json',
+        FileNotFoundError,
+    ),
+)
 
 
-@pytest.mark.xfail(raises=exceptions.FileIsNotJsonError)
-def test_input_file_extension_other_than_json():
-    """Test the error for the fact that the input file has a extension other than .json."""
-    process_data(*invalid_test_data[1])
-
-
-@pytest.mark.xfail(raises=FileNotFoundError)
-def test_input_file_not_found():
-    """Test the error for the fact that the input file was not found."""
-    process_data(*invalid_test_data[2])
-
-
-@pytest.mark.xfail(raises=exceptions.FileIsEmptyError)
-def test_input_file_empty():
-    """Test the error for the fact that the input file is empty."""
-    process_data(*invalid_test_data[3])
-
-
-@pytest.mark.xfail(raises=TypeError)
-def test_user_data_not_dict():
-    """Test the error for the fact that the user_data is not a dict."""
-    process_data(*invalid_test_data[4])
-
-
-@pytest.mark.xfail(raises=TypeError)
-def test_user_char_not_int_or_str():
-    """Test the error for the fact that the user_char is not an int or str."""
-    process_data(*invalid_test_data[5])
-
-
-@pytest.mark.xfail(raises=IndexError)
-def test_number_user_chars_not_equal():
-    """Test the error for the fact that the number of user characteristics \
-        is not equal to len_valid_chars."""
-    process_data(*invalid_test_data[6])
-
-
-@pytest.mark.xfail(raises=ValueError)
-def test_user_char_invalid_name():
-    """Test the error for the fact that user characteristic name has an invalid name."""
-    process_data(*invalid_test_data[7])
-
-
-@pytest.mark.xfail(raises=TypeError)
-def test_user_char_not_str():
-    """Test the error for the fact that the user_char is not a str."""
-    process_data(*invalid_test_data[8])
-
-
-@pytest.mark.xfail(raises=TypeError)
-def test_user_char_not_int():
-    """Test the error for the fact that the user_chars[-1] is not a int."""
-    process_data(*invalid_test_data[9])
-
-
-@pytest.mark.xfail(raises=TypeError)
-def test_output_file_path_not_str():
-    """Test the error for the fact that the output file path is not str."""
-    process_data(*invalid_test_data[10])
-
-
-@pytest.mark.xfail(raises=exceptions.FileIsNotJsonError)
-def test_output_file_extension_other_than_json():
-    """Test the error for the fact that the output file has a extension other than .json."""
-    process_data(*invalid_test_data[11])
-
-
-@pytest.mark.xfail(raises=FileNotFoundError)
-def test_output_file_not_found():
-    """Test the error for the fact that the output file was not found."""
-    process_data(*invalid_test_data[12])
+def test_invalid_test_data():
+    """Test invalid test data."""
+    for input_file_path, output_file_path, error in invalid_test_data:
+        with pytest.raises(error):
+            process_data(input_file_path, output_file_path)
