9adca7c83c148314dbe0d774ea68ab036e509e5f
iamelizabethsheina <lizonyass@icloud.com>
2023-12-27 17:34:45 +0300

    Completed HW1. Added files: hw1/hw1.py, hw1/test_hw1.py. Changed file: .github/workflows/pylint.yml.

---
+++ hw1/hw1.py
@@ -0,0 +1,27 @@
+"""Top 3 salaries module."""
+
+
+def top3_salaries(
+    *departments,
+    including: tuple[str, ...] = None,
+) -> tuple[tuple[str, float], tuple[str, float]]:
+    """Find 3 most- and least-paid departments in a given tuple by average value.
+
+    Args:
+        departments: a tuple with departments names their values.
+        including: tuple with names of departments to be excluded from stats, defaults to None.
+
+    Returns:
+        stats: tuple of top 3 most and least paid departments with their average salaries.
+    """
+    avg_salaries = {}
+    for department, workers in departments:
+        if including is None or department in including:
+            salaries = workers.values()
+            avg_salary = round(sum(salaries) / len(salaries), 2) if salaries else 0
+            avg_salaries[department] = avg_salary
+
+    salaries_sorted = sorted(avg_salaries.items(), key=lambda srt: srt[1], reverse=True)
+    top3_most_paid = salaries_sorted[:3]
+    top3_least_paid = salaries_sorted[-3:][::-1]
+    return tuple(top3_most_paid), tuple(top3_least_paid)
