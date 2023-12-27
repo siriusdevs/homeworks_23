01a9b991d5aa57e4e635d2f23e74af8acc38762a
isaeva20 <avelisavayrad@yandex.ru>
2023-12-27 10:03:20 +0300

    design edited

--- hw1.py
+++ hw1.py
@@ -1,25 +1,25 @@
-"""Module for calculating statistics."""
+"""Function for calculate statistic."""
 
 
-def salary_departments(required_departments: tuple[str] = None,
-                       **department_name: dict[str, float]
-                       ) -> tuple[list, float]:
-    """Displaying the top 3 salaries in the company
-    and the percentage of this top to all salaries.
+def salary_departments(
+    required_departments: tuple[str] = None,
+    **department_name: dict[str, float],
+) -> tuple[list, float]:
+    """Display the top 3 salaries in the company and the percentage of this top to all salaries.
 
     Args:
         required_departments (tuple[str]): tuple of department names(str)
             to be included in statistics. Default is None.
-                department_name (dict[str, float]): dictionary where
-                    the names of the department employees are keys (str),
-                        and salaries are the value (float).
+        department_name (dict[str, float]): dictionary where
+            the names of the department employees are keys (str),
+            and salaries are the value (float).
 
     Returns:
-        Tuple[list, float]: Statistics the top 3 salaries in the company
-        and the percentage of this top to all salaries.
+        Tuple[list, float]: statistics the top 3 salaries in the company
+            and the percentage of this top to all salaries.
 
     Raises:
-        ValueError: gives an error message.
+        ValueError: if list of the salary is empty.
     """
     list_salary = []
 
@@ -32,5 +32,4 @@ def salary_departments(required_departments: tuple[str] = None,
         raise ValueError('List salary cannot be a empty')
     top_salaries = list_salary[:3]
     total_salaries = round(sum(top_salaries) / sum(list_salary), 2) * 100
-
     return top_salaries, total_salaries
--- test_hw1.py
+++ test_hw1.py
@@ -1,37 +1,38 @@
-"""Module test for hw1"""
+"""Module test for hw1."""
 
 
 import pytest
+
 from hw1 import salary_departments
 
 all_departments = {
     'financy_department': {
-        'Alex': 77000.90,
-        'Victoria': 34000.20,
+        'Alex': 77000.9,
+        'Victoria': 34000.2,
         'Platon': 45000.87,
         'Oleg': 68000.89,
-        'Irina': 72000.74
+        'Irina': 72000.74,
     },
     'hr_department': {
-        'Svetlana': 46529.80,
+        'Svetlana': 46529.8,
         'Fiona': 47890.76,
-        'Eleonora': 60780.50,
+        'Eleonora': 60780.5,
         'Efrem': 51000.56,
     },
     'administrative_depatment': {
-        'Pavel': 33900.90,
-        'Margarita': 54760.90,
-        'Evgeny': 33900.90,
+        'Pavel': 33900.9,
+        'Margarita': 54760.9,
+        'Evgeny': 33900.9,
     },
     'technical_department': {
-        'Alexander': 156000.80,
-        'Roman': 136590.70,
+        'Alexander': 156000.8,
+        'Roman': 136590.7,
     },
 }
-top_salaries = [156000.80, 136590.70, 77000.90]
-top_salaries_2 = [77000.90, 72000.74, 68000.89]
+top_salaries = [156000.8, 136590.7, 77000.9]
+top_salaries2 = [77000.9, 72000.74, 68000.89]
 test_data = [(all_departments, (top_salaries, 40.0))]
-test_data2 = [(all_departments, ('financy_department', 'hr_department'), (top_salaries_2, 43.0))]
+test_data2 = [(all_departments, ('financy_department', 'hr_department'), (top_salaries2, 43.0))]
 test_data_invalid = {
     'financy_department': {
     },
@@ -44,9 +45,8 @@ test_data_invalid = {
 }
 
 
-@pytest.mark.parametrize("department_name, expected", test_data)
-def test_salary_departments(department_name: dict[str, float],
-                            expected: tuple[list, float]):
+@pytest.mark.parametrize('department_name, expected', test_data)
+def test_salary_departments(department_name: dict[str, float], expected: tuple[list, float]):
     """Function test salary_departments.
 
     Args:
@@ -62,10 +62,12 @@ def test_invalid_salary_departments():
     salary_departments(test_data_invalid)
 
 
-@pytest.mark.parametrize("department_name, required_departments, expected", test_data2)
-def test_salary_departments_with_req(department_name: dict[str, float],
-                                     required_departments: tuple[str],
-                                     expected: tuple[list, float]):
+@pytest.mark.parametrize('department_name, required_departments, expected', test_data2)
+def test_salary_departments_with_req(
+    department_name: dict[str, float],
+    required_departments: tuple[str],
+    expected: tuple[list, float],
+):
     """Function test salary_departments with required departments.
 
     Args:
