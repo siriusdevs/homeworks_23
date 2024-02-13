e26498809f6353cd202df1178b392f297b9bbfee
hape30 <sasha2007.ivanov@mail.ru>
2024-02-13 19:34:55 +0300

    quotes fixed

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from init_constants import Constants
 
 
-def load_json_data(input_path: str = "data_hw2.json"):
+def load_json_data(input_path: str = 'data_hw2.json'):
     """
     Функция подгрузки json информации.
 
@@ -16,12 +16,12 @@ def load_json_data(input_path: str = "data_hw2.json"):
     Returns:
         dict: загруженный json.
     """
-    with open(input_path, "r") as input_file:
+    with open(input_path, 'r') as input_file:
         return json.load(input_file)
 
 
 def process_data(
-    output_path: str = "data_result.json",
+    output_path: str = 'data_result.json',
 ):
     """
     Функция реализующая анализ существующего json и соберет из него новую статистику.
@@ -34,76 +34,75 @@ def process_data(
     """
     json_data = load_json_data()
     result_data = {
-        "regions": {},
-        "ages": [],
-        "last_login_dates": [],
-        f"{Constants.total_list[0]}": len(json_data),
+        'regions': {},
+        'ages': [],
+        'last_login_dates': [],
+        f'{Constants.total_list[0]}': len(json_data),
     }
 
     for _user, user_info in json_data.items():
-        last_login = datetime.strptime(user_info["last_login"], "%Y-%m-%d")
+        last_login = datetime.strptime(user_info['last_login'], '%Y-%m-%d')
 
-        if user_info["region"] not in result_data[Constants.regions_list[0]]:
-            result_data[Constants.regions_list[0]][user_info["region"]] = 0
-        result_data[Constants.regions_list[0]][user_info["region"]] += 1
+        if user_info['region'] not in result_data[Constants.regions_list[0]]:
+            result_data[Constants.regions_list[0]][user_info['region']] = 0
+        result_data[Constants.regions_list[0]][user_info['region']] += 1
 
-        result_data["ages"].append(user_info["age"])
-        result_data["last_login_dates"].append(last_login)
+        result_data['ages'].append(user_info['age'])
+        result_data['last_login_dates'].append(last_login)
 
     if Constants.total_list[0] != 0:
-        result_data["region_distribution"] = {
+        result_data['region_distribution'] = {
             region: (count / result_data[Constants.total_list[0]]) * 100
             for region, count in result_data[Constants.regions_list[0]].items()
         }
     else:
-        print("На ноль делить нельзя!")
+        print('На ноль делить нельзя!')
         sys.exit(1)
 
-    result_data["online_times"] = [
-        datetime.now() - date for date in result_data["last_login_dates"]
+    result_data['online_times'] = [
+        datetime.now() - date for date in result_data['last_login_dates']
     ]
-    result_data["online_days"] = [
-        time.total_seconds() / (60 * 60 * 24) for time in result_data["online_times"]
+    result_data['online_days'] = [
+        time.total_seconds() / (60 * 60 * 24) for time in result_data['online_times']
     ]
     if (
         result_data[Constants.total_list[0]] != 0
         and result_data[Constants.total_list_two[0]] != 0
-        and result_data["total"] != 0
+        and result_data['total'] != 0
     ):
         try:
-            result_data["stats"] = {
-                "region_distribution": result_data["region_distribution"],
-                "average_age": sum(result_data["ages"])
+            result_data['stats'] = {
+                'region_distribution': result_data['region_distribution'],
+                'average_age': sum(result_data['ages'])
                 / result_data[Constants.total_list[0]],
-                "online_times": {
-                    "<2 days": sum(
+                'online_times': {
+                    '<2 days': sum(
                         day < 2 for day in result_data[Constants.online_days[0]]
                     )
                     / result_data[Constants.total_list[0]],
-                    "<1 week": sum(
+                    '<1 week': sum(
                         day < 7 for day in result_data[Constants.online_days[0]]
                     )
                     / result_data[Constants.total_list_two[0]],
-                    "<1 month": sum(
+                    '<1 month': sum(
                         day < Constants.month
                         for day in result_data[Constants.online_days[0]]
                     )
                     / result_data[Constants.total_list_two[0]],
-                    ">6 months": sum(
+                    '>6 months': sum(
                         day > Constants.half_year
                         for day in result_data[Constants.online_days[0]]
                     )
-                    / result_data["total"],
+                    / result_data['total'],
                 },
             }
         except ZeroDivisionError as error:
-            return f"На ноль делить нельзя!\n{error}"
+            return f'На ноль делить нельзя!\n{error}'
     else:
-        print("На ноль делить нельзя!")
-        sys.exit(1)
+        return 'На ноль делить нельзя!'
 
-    with open(output_path, "w") as output_file:
-        json.dump(result_data["stats"], output_file)
+    with open(output_path, 'w') as output_file:
+        json.dump(result_data['stats'], output_file)
 
 
 process_data()
