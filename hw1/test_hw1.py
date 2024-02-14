d14d86ab3b7e23d02c0b01bf901c1439e9c767db
0m0ra <102877699+0m0ra@users.noreply.github.com>
2024-02-14 17:14:58 +0300

    Update test_hw1.py

--- hw1/test_hw1.py
+++ hw1/test_hw1.py
@@ -1,45 +1,36 @@
-"""Test for calculating salary statistics module."""
 import pytest
-
 from hw1 import salary_statistics
+
 
 company = {
     'first': {
-        'worker1.1': 124.4,
-        'worker2.1': 244,
+        'worker1': 124.4,
+        'worker2': 244
     },
     'second': {
-        'worker1.2': 125.2422,
-        'worker2.2': 44,
-        'worker3.2': 1234,
+        'worker1': 125.2422,
+        'worker2': 44,
+        'worker3': 1234
     },
     'third': {
-        'worker1.3': 100,
+        'worker1': 100
     },
     'forth': {
-        'worker1.4': 232,
-        'worker2.4': 150.245,
-    },
+        'worker1': 232,
+        'worker2': 150.245
+    }
 }
 
 
-@pytest.mark.parametrize('company, min_salary, excected_value', ((company, None,
-                                                                  {
-                                                                      'top': [1234, 244, 232],
-                                                                      'ratio': 75.87,
-                                                                  }),
-                                                                 (company,
-                                                                  150,
-                                                                  {
-                                                                      'top': [1234, 244, 232],
-                                                                      'ratio': 91.92,
-                                                                  })))
+@pytest.mark.parametrize('company, min_salary, excected_value', (
+    (company, None, {
+        'top': [1234, 244, 232],
+        'ratio': 75.87
+    }),
+    (company, 150, {
+        'top': [1234, 244, 232],
+        'ratio': 91.92
+    })
+))
 def test(company, min_salary, excected_value):
-    """Final test.
-
-    Args:
-        company: company
-        min_salary: min salary
-        excected_value: excected value
-    """
     assert salary_statistics(company, min_salary) == excected_value
