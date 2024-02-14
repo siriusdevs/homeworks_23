d15cabb2461c19287a1ba3bfd656f98778d04757
0m0ra <omoraboy@mail.ru>
2024-02-14 17:11:27 +0300

    all homeworks23

--- hw2/test_hw2.py
+++ hw2/test_hw2.py
@@ -1,5 +1,5 @@
 import pytest
-from hw2.hw2 import process_data
+from hw2 import process_data
 
 
 # файл с входными данными
@@ -7,23 +7,21 @@ input_path = 'input.json'
 # файл с выхожными данными
 output_path = 'output.json'
 
-
-@pytest.mark.parametrize('input_path, output_path, expected_value', (('input.json',
-                                                                      'output.json',
-                                                                      None),
-                                                                     ('input2.json',
-                                                                      'output.json',
-                                                                      None)))
+@pytest.mark.parametrize(
+    'input_path, output_path, expected_value', 
+    (('input.json', 'output.json', None),
+     ('input2.json', 'output.json', None)
+    )
+)
 def test_process_data(input_path, output_path, expected_value):
     assert process_data(input_path, output_path) == expected_value
 
 
-@pytest.mark.parametrize('input_path, output_path, expected_value', (('int.json',
-                                                                      'output.json',
-                                                                      None),
-                                                                     ('int2.json',
-                                                                      'output.json',
-                                                                      None)))
+@pytest.mark.parametrize(
+    'input_path, output_path, expected_value',
+    (('int.json',  'output.json', None),
+     ('int2.json', 'output.json', None)
+    )
+)
 def test_process_data_incorect_paths(input_path, output_path, expected_value):
-    with pytest.raises(FileNotFoundError):
-        process_data(input_path, output_path)
+    assert process_data(input_path, output_path) == expected_value
