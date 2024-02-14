b1c3721820e77fec28946b2cd03fa675a5225b69
hape30 <sasha2007.ivanov@mail.ru>
2024-02-14 23:44:09 +0300

    hw3 init

--- hw3/test_hw3.py
+++ hw3/test_hw3.py
@@ -16,10 +16,14 @@ class TestRestaurantClasses(unittest.TestCase):
         """Настройка перед выполнением каждого теста."""
         self.product1 = Product('Яблоко', 1.0)
         self.product2 = Product('Банан', BANANA_PRICE)
+        self.product_non_exist = Product(1, BANANA_PRICE)
 
         self.dish = Dish('Фруктовый салат')
         self.dish.add_product(self.product1)
         self.dish.add_product(self.product2)
+
+        self.dish_non_exist = Dish(None)
+        self.dish_non_exist.add_product(self.product_non_exist)
 
         self.restaurant_details = RestaurantDetails('Test Restaurant', [PIZZA, PASTA])
 
@@ -28,18 +32,37 @@ class TestRestaurantClasses(unittest.TestCase):
         self.assertEqual(self.product1.name, 'Яблоко')
         self.assertEqual(self.product1.price, 1.0)
 
+    def test_product_name_none(self):
+        """Тест на проверку None имени."""
+        self.assertEqual(None, None)
+
+    def test_dish_with_none_product(self):
+        """Тест на проверку блюда с None атрибутами."""
+        self.assertEqual(self.dish_non_exist.name, None)
+        self.assertEqual(len(self.dish_non_exist.products), 1)
+
     def test_dish_attributes(self):
         """Проверка атрибутов блюда."""
         self.assertEqual(self.dish.name, 'Фруктовый салат')
         self.assertEqual(len(self.dish.products), 2)
 
-    def test_add_and_remove_product_from_dish(self):
-        """Проверка добавления и удаления продукта из блюда."""
-        new_product = Product('Апельсин', ORANGE_PRICE)
-        self.dish.add_product(new_product)
-        self.assertIn(new_product, self.dish.products)
-        self.dish.remove_product(new_product)
-        self.assertNotIn(new_product, self.dish.products)
+    def test_add_none_product(self):
+        """Тест на добавление None продукта."""
+        try:
+            self.dish_non_exist.add_product(None)
+        except ValueError:
+            self.assertTrue(True)
+        else:
+            self.fail('Нельзя добавить в блюдо несуществующий продукт!')
+
+    def test_remove_none_product(self):
+        """Тест на удаление None продукта."""
+        try:
+            self.dish_non_exist.remove_product(None)
+        except ValueError:
+            self.assertTrue(True)
+        else:
+            self.fail('Нельзя удалить то, что не существует!')
 
 
 class TestRestaurantDetails(unittest.TestCase):
@@ -70,6 +93,10 @@ class TestMenuManager(unittest.TestCase):
         """Метод инициализации перед тестами."""
         self.menu_manager = MenuManager([PIZZA, PASTA])
 
+        self.dish = Dish('Яблочный салат.')
+        self.apple = Product('Яблоко', 1.0)
+        self.dish.add_product(self.apple)
+
     def test_dishes(self):
         """Тестирование блюд."""
         self.assertEqual(self.menu_manager.dishes, [PIZZA, PASTA])
@@ -83,6 +110,32 @@ class TestMenuManager(unittest.TestCase):
         """Тест удаления блюда."""
         self.menu_manager.remove_dish(PIZZA)
         self.assertEqual(self.menu_manager.dishes, [PASTA])
+
+    def test_non_exist_remove_dish(self):
+        """Тест на удаление None блюда."""
+        try:
+            self.menu_manager.remove_dish(None)
+        except ValueError:
+            self.assertTrue(True)
+        else:
+            self.fail('Блюдо должно существовать!!!')
+
+    def test_non_exist_append_dish(self):
+        """Тест на добавление None блюда."""
+        try:
+            self.menu_manager.add_dish(None)
+        except ValueError:
+            self.assertTrue(True)
+        else:
+            self.fail('Блюдо должно существовать!!!')
+
+    def test_add_and_remove_product_from_dish(self):
+        """Проверка добавления и удаления продукта из блюда."""
+        new_product = Product('Апельсин', ORANGE_PRICE)
+        self.dish.add_product(new_product)
+        self.assertIn(new_product, self.dish.products)
+        self.dish.remove_product(new_product)
+        self.assertNotIn(new_product, self.dish.products)
 
 
 if __name__ == '__main__':
