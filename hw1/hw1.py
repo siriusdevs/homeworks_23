c6e94c890b5318ce70612e837240a1a8ff57ed1f
0m0ra <102877699+0m0ra@users.noreply.github.com>
2024-02-14 17:13:29 +0300

    Update hw1.py

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,34 +1,34 @@
-"""Module for calculating salary statistics."""
-
-
-def salary_statistics(company_dict, min_salary=None):
-    """Salary statictics function.
+def salary_statistics(company_dict: dict, min_salary: int =None) -> dict:
+    """salary statictics function.
 
     Args:
         company_dict: dict - a dictionary with company departments and workers.
-        min_salary: int|float optional - the minimum threshold for salaries. Defaults to None.
+        min_salary: float optional - the minimum threshold for salaries. Defaults to None.
 
     Returns:
         dict: - output data.
     """
     # создаём список со всеми зп
     all_salaries = []
-    for workers in company_dict.values():
+    for key in company_dict:
+        workers = company_dict[key]
+
         all_salaries.extend(workers.values())
 
     if min_salary is not None:
         # если есть аргумент min_salary отсеиваем всё, что меньше его
-        all_salaries = list(filter(lambda fltr: fltr >= min_salary, all_salaries))
+        all_salaries = list(filter(lambda x: x >= min_salary, all_salaries))
 
     # топ 3 зп
-    top_salaries = sorted(all_salaries, reverse=True)[:3]
+    top_salaries = sorted(all_salaries, reverse=True)[0:3]
 
-    for top in range(3):
-        top_salaries[top] = round(top_salaries[top], 2)
+    for i in range(3):
+        top_salaries[i] = round(top_salaries[i], 2)
 
     # отношение топа зарплат ко всем
-    if sum(all_salaries) > 0:
+    ratio = 0
+    
+    if len(all_salaries) > 0:
         ratio = round(sum(top_salaries) / sum(all_salaries) * 100, 2)
-    else:
-        ratio = 0
+
     return {'top': top_salaries, 'ratio': ratio}
