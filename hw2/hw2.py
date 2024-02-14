f882f2bbb31989338172b146e5dc234383e4fcf6
hape30 <sasha2007.ivanov@mail.ru>
2024-02-14 22:50:15 +0300

    hw2 init

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -19,26 +19,74 @@ def load_json_data(input_path: str = 'data_hw2.json'):
         return json.load(input_file)
 
 
+def calculate_statistics(result_data, key):
+    """
+    Функция подсчета среднего возраста.
+
+    Args:
+        result_data: dict, содержащий в себе данные.
+        key: dict, ключ поиска по словарю.
+
+    Returns:
+         Union[int: средний возраст, dict: ошибка о делении на 0]
+    """
+    total = result_data[key]
+    try:
+        return sum(result_data['ages']) / total
+    except ZeroDivisionError:
+        return {'error': 'Деление на 0 запрещено.'}
+
+
+def calculate_online_times_statistics(result_data, online_days_key, total_key):
+    """
+    Вычисляет статистику онлайн-времени пользователей.
+
+    Args:
+        result_data: dict, словарь с данными.
+        online_days_key: str, ключ для словаря result_data.
+        total_key: str, ключ для словаря result_data.
+
+    Returns:
+        dict: Словарь с данными после анализа/Ошибка о делении на 0.
+    """
+    total = result_data[total_key]
+    try:
+        return {
+            '<2 days': sum(day < 2 for day in result_data[online_days_key]) / total,
+            '<1 week': sum(day < 7 for day in result_data[online_days_key]) / total,
+            '<1 month': sum(
+                day < Constants.month for day in result_data[online_days_key]
+            )
+            / total,
+            '>6 months': sum(
+                day > Constants.half_year for day in result_data[online_days_key]
+            )
+            / total,
+        }
+    except ZeroDivisionError:
+        return {'error': 'Деление на 0 запрещено.'}
+
+
 def process_data(
-    input_path: str = 'data_hw2.json',
     output_path: str = 'data_result.json',
 ):
     """
     Функция реализующая анализ существующего json и соберет из него новую статистику.
 
     Args:
-        input_path (str): путь, где находится .json файл.
         output_path (str): путь для записи итогового .json файла.
+
+    Returns:
+        str: dict, предупреждение о делении на 0.
     """
-    json_data = load_json_data()
     result_data = {
         'regions': {},
         'ages': [],
         'last_login_dates': [],
-        f'{Constants.total_list[0]}': len(json_data),
+        f'{Constants.total_list[0]}': len(load_json_data()),
     }
 
-    for _user, user_info in json_data.items():
+    for _user, user_info in (load_json_data()).items():
         last_login = datetime.strptime(user_info['last_login'], '%Y-%m-%d')
 
         if user_info['region'] not in result_data[Constants.regions_list[0]]:
@@ -48,10 +96,13 @@ def process_data(
         result_data['ages'].append(user_info['age'])
         result_data['last_login_dates'].append(last_login)
 
-    result_data['region_distribution'] = {
-        region: (count / result_data[Constants.total_list[0]]) * 100
-        for region, count in result_data[Constants.regions_list[0]].items()
-    }
+    if Constants.total_list[0] == 0:
+        return {'error': 'Деление на 0 запрещено.'}
+    else:
+        result_data['region_distribution'] = {
+            region: (count / result_data[Constants.total_list[0]]) * 100
+            for region, count in result_data[Constants.regions_list[0]].items()
+        }
 
     result_data['online_times'] = [
         datetime.now() - date for date in result_data['last_login_dates']
@@ -59,26 +110,29 @@ def process_data(
     result_data['online_days'] = [
         time.total_seconds() / (60 * 60 * 24) for time in result_data['online_times']
     ]
+    be_true = (
+        result_data[Constants.total_list[0]] != 0
+        and result_data[Constants.total_list_two[0]] != 0
+        and result_data['total'] != 0
+    )
 
-    result_data['stats'] = {
-        'region_distribution': result_data['region_distribution'],
-        'average_age': sum(result_data['ages']) / result_data[Constants.total_list[0]],
-        'online_times': {
-            '<2 days': sum(day < 2 for day in result_data[Constants.online_days[0]])
-            / result_data[Constants.total_list[0]],
-            '<1 week': sum(day < 7 for day in result_data[Constants.online_days[0]])
-            / result_data[Constants.total_list_two[0]],
-            '<1 month': sum(
-                day < Constants.month for day in result_data[Constants.online_days[0]]
-            )
-            / result_data[Constants.total_list_two[0]],
-            '>6 months': sum(
-                day > Constants.half_year
-                for day in result_data[Constants.online_days[0]]
-            )
-            / result_data['total'],
-        },
-    }
+    if be_true:
+        average_age = calculate_statistics(result_data, Constants.total_key[0])
+        online_times = calculate_online_times_statistics(
+            result_data, Constants.online_days[0], Constants.total_key[0]
+        )
+
+        if average_age is None or online_times is None:
+            return {'error': 'Деление на  0 запрещено.'}
+
+
+        result_data['stats'] = {
+            'region_distribution': result_data['region_distribution'],
+            'average_age': average_age,
+            'online_times': online_times,
+        }
+    else:
+        return {'error': 'Деление на  0 запрещено.'}
 
     with open(output_path, 'w') as output_file:
         json.dump(result_data['stats'], output_file)
