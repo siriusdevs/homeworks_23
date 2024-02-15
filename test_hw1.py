247c4f198d85a228bafde12bd9976c4ad697ee2c
FGun4ik <74475113+FGun4ik@users.noreply.github.com>
2024-02-16 02:51:38 +0300

    Update test_hw1.py

--- test_hw1.py
+++ test_hw1.py
@@ -9,22 +9,31 @@ MAXIMUM_KEY = 'maximum'
 MEDIAN_KEY = 'median'
 
 
+DEP_SALES = 'Sales'
+DEP_HR = 'HR'
+
+
 @pytest.mark.parametrize('departments, limit, expected', [
     ({}, None, {AVERAGE_KEY: None, MAXIMUM_KEY: None, MEDIAN_KEY: None}),
     (
-        {'Sales': {}, 'HR': {}},
+        {DEP_SALES: {}, DEP_HR: {}},
         None,
         {AVERAGE_KEY: None, MAXIMUM_KEY: None, MEDIAN_KEY: None},
     ),
     (
-        {'Sales': {'John': 2000.0, 'Jane': 3000.0}, 'HR': {'Bob': 1500.0, 'Alice': 1800.0}},
+        {DEP_SALES: {'Artem': 2000.0, 'Egor': 3000.0}, DEP_HR: {'Dima': 1500.0, 'Sonya': 1800.0}},
         5000.0,
         {AVERAGE_KEY: None, MAXIMUM_KEY: None, MEDIAN_KEY: None},
     ),
     (
-        {'Sales': {'John': 5000.0, 'Jane': 5000.0}, 'HR': {'Bob': 5000.0, 'Alice': 5000.0}},
+        {DEP_SALES: {'Artem': 5000.0, 'Egor': 5000.0}, DEP_HR: {'Dima': 5000.0, 'Sonya': 5000.0}},
         None,
         {AVERAGE_KEY: 5000.0, MAXIMUM_KEY: 5000.0, MEDIAN_KEY: 5000.0},
+    ),
+    (
+        {DEP_SALES: {'Artem': 6000.0, 'Egor': 7000.0}, DEP_HR: {'Dima': 8000.0, 'Sonya': 9000.0}},
+        5000.0,
+        {AVERAGE_KEY: 7500.0, MAXIMUM_KEY: 9000.0, MEDIAN_KEY: 7500.0},
     ),
 ])
 def test_salary_stats(departments, limit, expected):
