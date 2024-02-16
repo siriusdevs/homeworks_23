ac55cf918f7729a4bb3dcb479ed6fd1a47fbc23d
fghh64 <fghh64.46hhgf@yandex.ru>
2024-02-16 13:59:03 +0300

    test2

--- hw2/test_hw2.py
+++ hw2/test_hw2.py
@@ -20,7 +20,7 @@ ERROR_CHECKING = (
     (
         './hw2/test_error.json',
         'test_output.json',
-        'file ./hw2/test_error.json invalid test data format!',
+        'file ./hw2/test_error.json is not in JSON format!',
     ),
     (
         './hw2/data_hw2.json',
@@ -29,8 +29,8 @@ ERROR_CHECKING = (
     ),
     (
         './hw2/data_hw2.json',
-        './hw2/test_success.json',
-        'file The prograp was completed without errors.',
+        './hw2/test_output.json',
+        'The prograp was completed without errors.',
     ),
 )
 
