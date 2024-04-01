5b502ffd4784ed786e5cec77c187b3a18a930366
siriusdevs <tenigin.aa@talantiuspeh.ru>
2024-04-02 00:46:19 +0300

    1st, 2nd, 3rd hw's added with tests

---
+++ task2/test_main_hw2.py
@@ -0,0 +1,52 @@
+"""Module for testing the main file."""
+
+import json
+
+import pytest
+
+from main_hw2 import process_data
+
+test_data = {
+
+    (
+        './data_hw2.json',
+        './task2/test_result/test_1.json',
+        './task2/test_answer/answer_test_1.json',
+    ),
+    (
+        './task2/data_file/data_file_1.json',
+        './task2/test_result/test_2.json',
+        './task2/test_answer/answer_test_2.json',
+    ),
+    (
+        './task2/data_file/data_file_2.json',
+        './task2/test_result/test_3.json',
+        './task2/test_answer/answer_test_3.json',
+    ),
+    (
+        './task2/data_file/data_file_3.json',
+        './task2/test_result/test_4.json',
+        './task2/test_answer/answer_test_4.json',
+    ),
+}
+
+
+@pytest.mark.parametrize('data_file_for_stats, output_file, expected', test_data)
+def test_process_data(data_file_for_stats: str, output_file: str, expected: tuple):
+
+    """test function for main modul procces_data function.
+
+    Args:
+        data_file_for_stats (str): _description_
+        output_file (str): _description_
+        expected (tuple): _description_"""
+
+    process_data(data_file_for_stats, output_file)
+
+    with open(output_file, 'r') as data_file_res:
+        data_file_result = json.loads(data_file_res.read())
+
+    with open(expected, 'r') as data_file_ans:
+        data_file_answer = json.loads(data_file_ans.read())
+
+    assert data_file_result == data_file_answer
