94a0df05409813dc14bc3c3ace4ce0a0f3496160
Vadim_Sannikov <brastars85@gmail.com>
2024-04-03 17:10:26 +0300

    prov enter3

--- hw3/test_passenger_car.py
+++ hw3/test_passenger_car.py
@@ -1,14 +1,11 @@
 import pytest
-
 from hw3 import PassengerCar
 
+"""Tests for PassengerCar class."""
 
 TOYOTA_MODEL = 'Toyota'
 MODEL_YEAR = 2024
 CAR_COST = 35000.0
-
-
-"""Tests for PassengerCar class."""
 
 def test_init():
     """Test initialization of PassengerCar object."""
@@ -18,18 +15,15 @@ def test_init():
     assert passenger_car.cost == CAR_COST
     assert passenger_car.passenger_seats == 5
 
-
 def test_invalid_passenger_seats_type():
     """Test invalid passenger seats type."""
     with pytest.raises(TypeError):
         PassengerCar(TOYOTA_MODEL, MODEL_YEAR, CAR_COST, '5')
 
-
 def test_empty_passenger_seats():
     """Test empty passenger seats."""
     with pytest.raises(TypeError):
         PassengerCar(TOYOTA_MODEL, MODEL_YEAR, CAR_COST, None)
-
 
 def test_extra_argument():
     """Test extra argument passed to PassengerCar."""
