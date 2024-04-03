b84d5fabe43cf8ea33069c4e423705a85f5f255d
Vadim_Sannikov <brastars85@gmail.com>
2024-04-03 16:57:16 +0300

    fix tests pasenger and car for flake8

--- hw3/test_passenger_car.py
+++ hw3/test_passenger_car.py
@@ -4,30 +4,32 @@ from hw3 import PassengerCar
 
 
 TOYOTA_MODEL = 'Toyota'
+MODEL_YEAR = 2024
+CAR_COST = 35000.0
 
 
 def test_init():
     """Test initialization of PassengerCar object."""
-    passenger_car = PassengerCar(TOYOTA_MODEL, 2022, 30000.0, 5)
+    passenger_car = PassengerCar(TOYOTA_MODEL, MODEL_YEAR, CAR_COST, 5)
     assert passenger_car.model == TOYOTA_MODEL
-    assert passenger_car.year == 2022
-    assert passenger_car.cost == 30000.0
+    assert passenger_car.year == MODEL_YEAR
+    assert passenger_car.cost == CAR_COST
     assert passenger_car.passenger_seats == 5
 
 
 def test_invalid_passenger_seats_type():
     """Test invalid passenger seats type."""
     with pytest.raises(TypeError):
-        PassengerCar(TOYOTA_MODEL, 2022, 30000.0, '5')
+        PassengerCar(TOYOTA_MODEL, MODEL_YEAR, CAR_COST, '5')
 
 
 def test_empty_passenger_seats():
     """Test empty passenger seats."""
     with pytest.raises(TypeError):
-        PassengerCar(TOYOTA_MODEL, 2022, 30000.0, None)
+        PassengerCar(TOYOTA_MODEL, MODEL_YEAR, CAR_COST, None)
 
 
 def test_extra_argument():
     """Test extra argument passed to PassengerCar."""
     with pytest.raises(TypeError):
-        PassengerCar(TOYOTA_MODEL, 2022, 30000.0, 5, 'extra')
+        PassengerCar(TOYOTA_MODEL, MODEL_YEAR, CAR_COST, 5, 'extra')
