91325fac276842e628df068fe0a5cae620d654a0
Vadim_Sannikov <brastars85@gmail.com>
2024-04-03 16:19:46 +0300

    fix test flake8 WPS226

--- hw3/test_passenger_car.py
+++ hw3/test_passenger_car.py
@@ -1,10 +1,12 @@
 import pytest
 from hw3 import PassengerCar
 
+TOYOTA_MODEL = 'Toyota'
+
 
 def test_init():
-    passenger_car = PassengerCar('Toyota', 2022, 30000.0, 5)
-    assert passenger_car.model == 'Toyota'
+    passenger_car = PassengerCar(TOYOTA_MODEL, 2022, 30000.0, 5)
+    assert passenger_car.model == TOYOTA_MODEL
     assert passenger_car.year == 2022
     assert passenger_car.cost == 30000.0
     assert passenger_car.passenger_seats == 5
@@ -12,14 +14,14 @@ def test_init():
 
 def test_invalid_passenger_seats_type():
     with pytest.raises(TypeError):
-        PassengerCar('Toyota', 2022, 30000.0, '5')
+        PassengerCar(TOYOTA_MODEL, 2022, 30000.0, '5')
 
 
 def test_empty_passenger_seats():
     with pytest.raises(TypeError):
-        PassengerCar('Toyota', 2022, 30000.0, None)
+        PassengerCar(TOYOTA_MODEL, 2022, 30000.0, None)
 
 
 def test_extra_argument():
     with pytest.raises(TypeError):
-        PassengerCar('Toyota', 2022, 30000.0, 5, 'extra')
+        PassengerCar(TOYOTA_MODEL, 2022, 30000.0, 5, 'extra')
