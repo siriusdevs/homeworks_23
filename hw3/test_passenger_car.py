f26f43d540b1927831ec6eeb8c1dc83e0c513779
Vadim_Sannikov <brastars85@gmail.com>
2024-04-03 17:23:56 +0300

    fix dockstring on pasenger

--- hw3/test_passenger_car.py
+++ hw3/test_passenger_car.py
@@ -1,12 +1,12 @@
+"""
+Tests for PassengerCar class.
+"""
 import pytest
-
 from hw3 import PassengerCar
-
 
 TOYOTA_MODEL = 'Toyota'
 MODEL_YEAR = 2024
 CAR_COST = 35000.0
-
 
 def test_init():
     """Test initialization of PassengerCar object."""
@@ -16,18 +16,15 @@ def test_init():
     assert passenger_car.cost == CAR_COST
     assert passenger_car.passenger_seats == 5
 
-
 def test_invalid_passenger_seats_type():
     """Test invalid passenger seats type."""
     with pytest.raises(TypeError):
         PassengerCar(TOYOTA_MODEL, MODEL_YEAR, CAR_COST, '5')
 
-
 def test_empty_passenger_seats():
     """Test empty passenger seats."""
     with pytest.raises(TypeError):
         PassengerCar(TOYOTA_MODEL, MODEL_YEAR, CAR_COST, None)
-
 
 def test_extra_argument():
     """Test extra argument passed to PassengerCar."""
